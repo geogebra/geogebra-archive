f1e9a7e240653086ce22f5af2a8011e8b1e6252b
materthron <materthron@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2007-04-06 14:23:26 +0000

    Changed input for date to a JFormattedTextField, thus now it's only possible to enter correct dates. Also, old values are treated in a downward compatible manner.
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@123 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/TitlePanel.java
+++ geogebra/gui/TitlePanel.java
@@ -1,14 +1,14 @@
 /* 
-GeoGebra - Dynamic Geometry and Algebra
-Copyright Markus Hohenwarter, http://www.geogebra.at
+ GeoGebra - Dynamic Geometry and Algebra
+ Copyright Markus Hohenwarter, http://www.geogebra.at
 
-This file is part of GeoGebra.
+ This file is part of GeoGebra.
 
-This program is free software; you can redistribute it and/or modify it 
-under the terms of the GNU General Public License as published by 
-the Free Software Foundation; either version 2 of the License, or 
-(at your option) any later version.
-*/
+ This program is free software; you can redistribute it and/or modify it 
+ under the terms of the GNU General Public License as published by 
+ the Free Software Foundation; either version 2 of the License, or 
+ (at your option) any later version.
+ */
 
 package geogebra.gui;
 
@@ -27,124 +27,178 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import geogebra.Application;
+import geogebra.kernel.Construction;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
 /**
- * Panel with title, author and date of construction. Handles all updates
- * to kernel and notifies attached ActionListeners about kernel changes.
+ * Panel with title, author and date of construction. Forwards all updates to
+ * kernel and notifies attached ActionListeners about kernel changes. Thus, it
+ * can be used to edit the aforementioned values in the kernel.
  * 
  * @author Markus Hohenwarter
+ * @author Philipp Weissenbacher (materthron@users.sourceforge.net)
  */
 public class TitlePanel extends JPanel {
 
-	/**
-	 * 
+    private static final long serialVersionUID = 1L;
+
+    private JTextField titleField, authorField;
+
+    private JFormattedTextField dateField;
+
+    private ArrayList listeners = new ArrayList();
+
+    private Construction cons;
+
+    public TitlePanel(Application app) {
+	cons = app.getKernel().getConstruction();
+
+	setLayout(new BorderLayout(5, 5));
+	titleField = new JTextField();
+	authorField = new JTextField();
+	dateField = new JFormattedTextField(SimpleDateFormat
+		.getDateInstance(SimpleDateFormat.MEDIUM));
+	dateField.setColumns(12);
+	dateField.setFocusLostBehavior(JFormattedTextField.PERSIST);
+
+	titleField.setText(cons.getTitle());
+	authorField.setText(cons.getAuthor());
+	dateField.setText(configureDate(cons.getDate()));
+
+	JPanel p = new JPanel(new BorderLayout(5, 5));
+	p.add(new JLabel(app.getPlain("Title") + ": "), BorderLayout.WEST);
+	p.add(titleField, BorderLayout.CENTER);
+	add(p, BorderLayout.NORTH);
+
+	p = new JPanel(new BorderLayout(5, 5));
+	JPanel p1 = new JPanel(new BorderLayout());
+	p1.add(new JLabel(app.getPlain("Author") + ": "), BorderLayout.WEST);
+	p1.add(authorField, BorderLayout.CENTER);
+	p.add(p1, BorderLayout.CENTER);
+
+	p1 = new JPanel(new BorderLayout());
+	p1.add(new JLabel(app.getPlain("Date") + ": "), BorderLayout.WEST);
+	p1.add(dateField, BorderLayout.CENTER);
+
+	p.add(p1, BorderLayout.EAST);
+	add(p, BorderLayout.CENTER);
+	/*
+	 * setBorder(BorderFactory.createCompoundBorder(BorderFactory
+	 * .createEtchedBorder(), BorderFactory.createEmptyBorder(5, 5, 5, 5)));
 	 */
-	private static final long serialVersionUID = 1L;
-	private JTextField tfTitle, tfAuthor, tfDate;
-	private Vector listeners = new Vector();
-	//private Application app;
-	private Construction cons;
+	setBorder(BorderFactory.createTitledBorder(app
+		.getPlain("Document info")));
 
-	public TitlePanel(Application app) {
-		//this.app = app;
-		cons = app.getKernel().getConstruction();
-		
-		setLayout(new BorderLayout(5, 5));
-		tfTitle = new JTextField();
-		tfAuthor = new JTextField();
-		tfDate = new JTextField();
-		tfDate.setColumns(12);		
-		
-		tfTitle.setText(cons.getTitle());
-		tfAuthor.setText(cons.getAuthor());
-		tfDate.setText(cons.getDate());
-		
-	
+	ActionListener lst = new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		fireTextFieldUpdate((JTextField) e.getSource());
+	    }
+	};
+	titleField.addActionListener(lst);
+	authorField.addActionListener(lst);
+	dateField.addActionListener(lst);
 
-		JPanel p = new JPanel(new BorderLayout(5, 5));
-		p.add(
-			new JLabel(app.getPlain("Title") + ": "),
-			BorderLayout.WEST);
-		p.add(tfTitle, BorderLayout.CENTER);
-		add(p, BorderLayout.NORTH);
-		p = new JPanel(new BorderLayout(5, 5));
-		JPanel p1 = new JPanel(new BorderLayout());
-		p1.add(
-			new JLabel(app.getPlain("Author") + ": "),
-			BorderLayout.WEST);
-		p1.add(tfAuthor, BorderLayout.CENTER);
-		p.add(p1, BorderLayout.CENTER);
-		p1 = new JPanel(new BorderLayout());
-		p1.add(
-			new JLabel(app.getPlain("Date") + ": "),
-			BorderLayout.WEST);
-		p1.add(tfDate, BorderLayout.CENTER);
-		
-		p.add(p1, BorderLayout.EAST);
-		add(p, BorderLayout.CENTER);
-		setBorder(
-			BorderFactory.createCompoundBorder(
-				BorderFactory.createEtchedBorder(),
-				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+	FocusAdapter focusListener = new FocusAdapter() {
+	    public void focusLost(FocusEvent e) {
+		fireTextFieldUpdate((JTextField) e.getSource());
+	    }
+	};
+	titleField.addFocusListener(focusListener);
+	authorField.addFocusListener(focusListener);
+	dateField.addFocusListener(focusListener);
+    }
 
-		ActionListener lst = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				fireTextFieldUpdate((JTextField) e.getSource());
-			}
-		};
-		tfTitle.addActionListener(lst);
-		tfAuthor.addActionListener(lst);
-		tfDate.addActionListener(lst);
+    private String configureDate(String src) {
+	Calendar cal = Calendar.getInstance();
+	Date date = cal.getTime();
 
-		FocusListener flst = new FocusListener() {
-			public void focusLost(FocusEvent e) {
-				fireTextFieldUpdate((JTextField) e.getSource());
-			}
-			public void focusGained(FocusEvent e) {
-			}
-		};
-		tfTitle.addFocusListener(flst);
-		tfAuthor.addFocusListener(flst);
-		tfDate.addFocusListener(flst);
+	// If no date specified use current date
+	if (src.equals("")) {
+	    src = SimpleDateFormat.getDateInstance(SimpleDateFormat.MEDIUM)
+		    .format(date);
+	}
+	/*
+	 * Try to parse date with the local date format. If this fails just
+	 * display it and let the user re-edit it. To draw user attention to
+	 * this shortcoming we highlight the text field with red and let it have
+	 * the focus. TODO: Is this sufficient enough without any textual advice
+	 * such as a dialog?
+	 */
+	else {
+	    try {
+		date = DateFormat.getDateInstance().parse(src);
+	    } catch (ParseException e) {
+		dateField.setBackground(new Color(255, 48, 48));
+		dateField.requestFocusInWindow();
+		return src;
+	    }
 	}
 
-	private void fireTextFieldUpdate(JTextField tf) {
-		String text = tf.getText();
-		boolean kernelChanged = false;
-		if (tf == tfTitle) {
-			if (text.equals(cons.getTitle()))
-				return;
-			cons.setTitle(text);
-			kernelChanged = true;
-		} else if (tf == tfAuthor) {
-			if (text.equals(cons.getAuthor()))
-				return;
-			cons.setAuthor(text);
-			kernelChanged = true;
-		} else if (tf == tfDate) {
-			if (text.equals(cons.getDate()))
-				return;
-			cons.setDate(text);
-			kernelChanged = true;
-		}
+	DateFormat df = SimpleDateFormat.getDateInstance(DateFormat.MEDIUM);
 
-		if (kernelChanged) {
-			notifyListeners();
-		}
+	return df.format(date);
+    }
+
+    /**
+     * Updates the kernel if the user makes changes to the text fields.
+     */
+    private void fireTextFieldUpdate(JTextField tf) {
+	String text = tf.getText();
+	boolean kernelChanged = false;
+
+	if (tf == titleField) {
+	    if (text.equals(cons.getTitle()))
+		return;
+	    cons.setTitle(text);
+	    kernelChanged = true;
+	} else if (tf == authorField) {
+	    if (text.equals(cons.getAuthor()))
+		return;
+	    cons.setAuthor(text);
+	    kernelChanged = true;
+	} else if (tf == dateField) {
+	    if (text.equals(cons.getDate()))
+		return;
+	    cons.setDate(text);
+	    kernelChanged = true;
 	}
 
-	public void addActionListener(ActionListener lst) {
-		listeners.add(lst);
+	if (kernelChanged) {
+	    notifyListeners();
 	}
+    }
 
-	private void notifyListeners() {
-		int size = listeners.size();
-		for (int i = 0; i < size; i++) {
-			((ActionListener) listeners.get(i)).actionPerformed(
-				new ActionEvent(
-					this,
-					ActionEvent.ACTION_PERFORMED,
-					"TitleChanged"));
-		}
+    public void addActionListener(ActionListener lst) {
+	listeners.add(lst);
+    }
+
+    private void notifyListeners() {
+	int size = listeners.size();
+	for (int i = 0; i < size; i++) {
+	    ((ActionListener) listeners.get(i))
+		    .actionPerformed(new ActionEvent(this,
+			    ActionEvent.ACTION_PERFORMED, "TitleChanged"));
 	}
+    }
 
 }
