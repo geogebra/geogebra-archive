f8ad63d164cc8aebe2d22926b78f9857e46c06c2
materthron <materthron@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2007-07-12 03:12:31 +0000

    Added support for alpha channel.
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@224 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/ImageResizer.java
+++ geogebra/gui/ImageResizer.java
@@ -6,27 +6,27 @@ import java.awt.image.BufferedImage;
 
 public class ImageResizer {
 
+    /**
+     * Resizes the image to the given size.
+     * 
+     * @param srcImg
+     * 		the image to be resized
+     * @param width
+     * 		width of the resized image
+     * @param height
+     * 		height of the resized image
+     */
+    public static BufferedImage resizeImage(BufferedImage srcImage, int width, int height) {
+	BufferedImage resizedImage = new BufferedImage(width, height,
+		BufferedImage.TYPE_INT_ARGB);
+	Graphics2D g2 = resizedImage.createGraphics();
 
-	/**
-	 * Resizes the image to the given size.
-	 * 
-	 * @param srcImg
-	 *            the image to be resized
-	 * @return resized image
-	 */
-	public static BufferedImage resizeImage(BufferedImage image, 
-			int width,
-			int height) {		
-		
-	    // draw original image to resizedImage object and
-	    // scale it to the new size on-the-fly
-	    BufferedImage resizedImage = new BufferedImage(width, 
-	    			height, BufferedImage.TYPE_INT_RGB);
-	    Graphics2D graphics2D = resizedImage.createGraphics();
-	    graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-	      RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-	    graphics2D.drawImage(image, 0, 0, width, height, null);
-			
-		return resizedImage;
-	}
+	g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+		RenderingHints.VALUE_RENDER_QUALITY);
+	g2.drawImage(srcImage, 0, 0, width, height, null);
+	
+	g2.dispose();
+
+	return resizedImage;
+    }
 }
