d2d7d143baa428689e169e64c3b831f6447f1d83
amyabraham <amyabraham@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2007-09-19 02:06:27 +0000

    Spreadsheet initial upload
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@293 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ geogebra/spreadsheet/SpreadsheetTableModel.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package geogebra.spreadsheet;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTable;
+
+import jspreadsheet.Cell;
+/**
+ * @author brisk1
+ *
+ */
+public class SpreadsheetTableModel extends DefaultTableModel
+{
+    /** Stores file name of current document */
+    private JTable table;
+    /**
+     * Stores modified state of document
+     */
+    private boolean modified;
+
+
+    public SpreadsheetTableModel(JTable table)
+    {
+        super();
+        this.table=table;
+        modified=false;
+    }
+    
+    public SpreadsheetTableModel(JTable table, int numRows, int numColumns)
+    {
+        super(numRows, numColumns);
+
+        for (int row = 0; row < numRows; row++)
+           for (int col = 0; col < numColumns; col++)
+
+              // we initialize it here
+//              super.setValueAt(new Cell(""), row, col);
+
+        // initialize state to unmodified and file to untitled
+        modified = false;
+        this.table = table;
+    }
+    
+}
---
+++ geogebra/spreadsheet/SpreadsheetView.java
@@ -0,0 +1,213 @@
+package geogebra.spreadsheet;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.TableModel;
+
+import jspreadsheet.JSpreadsheet;
+
+import geogebra.Application;
+import geogebra.View;
+import geogebra.kernel.GeoElement;
+import geogebra.kernel.Kernel;
+
+/**
+ * A class which will give the view of the Spreadsheet
+ */
+
+/**
+ * @author Amy Mathew Varkey
+ *
+ */
+public class SpreadsheetView extends JComponent implements View
+{
+    
+    private JTable table;
+    private Kernel kernel;
+    private SpreadsheetTableModel tableModel;
+    
+    public SpreadsheetView(Application app,int rows, int columns)
+    {
+       table = createTable();
+       kernel =app.getKernel();
+       setLayout(new BorderLayout());
+       newTableModel(rows, columns);
+
+       // clobber resizing of all columns
+       table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+       // set selection mode for contiguous  intervals
+       table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+       table.setCellSelectionEnabled(true);
+       
+       // we don't allow reordering
+       table.getTableHeader().setReorderingAllowed(false);
+ 
+       JScrollPane scrollPane = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+       add(scrollPane, BorderLayout.CENTER);
+
+       JTable rowHeader = new JTable(new RowModel(table.getModel()));
+
+       rowHeader.setIntercellSpacing(new Dimension(0, 0));
+       table.setRequestFocusEnabled(true);
+       table.requestFocus();
+    
+     }
+    
+
+    
+        /** Can be used by subclasses to customize the underlying JTable
+     * @return The JTable to be used by the spreadsheet
+     */
+    protected JTable createTable()
+    {
+       return new JTable();
+    }
+    
+    private static class RowModel implements TableModel
+    {
+       private TableModel source;
+
+       RowModel(TableModel source)
+       {
+          this.source = source;
+       }
+
+       public boolean isCellEditable(int rowIndex, int columnIndex)
+       {
+          return false;
+       }
+
+       public Class getColumnClass(int columnIndex)
+       {
+          return Object.class;
+       }
+
+       public int getColumnCount()
+       {
+          return 1;
+       }
+
+       public String getColumnName(int columnIndex)
+       {
+          return null;
+       }
+
+       public int getRowCount()
+       {
+          return source.getRowCount();
+       }
+
+       public void setValueAt(Object aValue, int rowIndex, int columnIndex)
+       {
+       }
+
+       public Object getValueAt(int rowIndex, int columnIndex)
+       {
+          return null;
+       }
+
+       public void addTableModelListener(javax.swing.event.TableModelListener l)
+       {
+       }
+
+       public void removeTableModelListener(javax.swing.event.TableModelListener l)
+       {
+       }
+    }
+    
+    /**
+     * Creates new blank SharpTableModel object with specified number of
+     * rows and columns.  table is set to this table model to update screen.
+     *
+     * @param rows number of rows in new table model
+     * @param cols number of columns in new table model
+     */
+    private void newTableModel(int rows, int cols)
+    {
+        tableModel = new SpreadsheetTableModel(table , rows, cols);
+        table.setModel(tableModel);
+        
+    }
+    /* (non-Javadoc)
+     * @see geogebra.View#add(geogebra.kernel.GeoElement)
+     */
+    public void add(GeoElement geo)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    /* (non-Javadoc)
+     * @see geogebra.View#clearView()
+     */
+    public void clearView()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    /* (non-Javadoc)
+     * @see geogebra.View#remove(geogebra.kernel.GeoElement)
+     */
+    public void remove(GeoElement geo)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    /* (non-Javadoc)
+     * @see geogebra.View#rename(geogebra.kernel.GeoElement)
+     */
+    public void rename(GeoElement geo)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    /* (non-Javadoc)
+     * @see geogebra.View#repaintView()
+     */
+    public void repaintView()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    /* (non-Javadoc)
+     * @see geogebra.View#reset()
+     */
+    public void reset()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    /* (non-Javadoc)
+     * @see geogebra.View#update(geogebra.kernel.GeoElement)
+     */
+    public void update(GeoElement geo)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    /* (non-Javadoc)
+     * @see geogebra.View#updateAuxiliaryObject(geogebra.kernel.GeoElement)
+     */
+    public void updateAuxiliaryObject(GeoElement geo)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    public static void main(String args[])
+    {        
+        JFrame frame = new JFrame();
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        contentPane.add(new JSpreadsheet(10,10), BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
+}
