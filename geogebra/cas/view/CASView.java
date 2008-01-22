e215f8fef52b32ea79fb0192f305e04af98129e8
dantesyuan <dantesyuan@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-01-22 03:03:05 +0000

    *** empty log message ***
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@487 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/cas/view/CASView.java
+++ geogebra/cas/view/CASView.java
@@ -36,128 +36,126 @@ import javax.swing.table.TableColumn;
  * @author James King
  *
  */
-public class CASView extends JComponent
-{
-    
-    private Kernel kernel;
-    private JTextField input, output;
-    private GeoGebraCAS cas;
-    private CASTableRenderer renderer;
-    private CASSession session;
-    private boolean showMenuBar = true;
-    
-    public CASView (Application app)
-    {
-       kernel =app.getKernel();
-       cas = new GeoGebraCAS();
-       session = new CASSession();
-       setLayout(new BorderLayout());
-       
-      // input = new JTextField();
-       //JTable table = new JTable(data, columnNames);
-       //JTable table = new JTable(new CASTableModel(session));
-       
-       /*
-       	//table.getModel().addTableModelListener(this);
-       	TableColumn console = table.getColumnModel().getColumn(0);
-       	console.setMinWidth(384);
-       	console.setCellRenderer(renderer);
-       	//JTable table = new JTable()
-		//table.setFillsViewportHeight(true); <-- I think this is only for > 1.4.2
-     	//JScrollPane sp= new JScrollPane(table);
-       	JScrollPane sp= new JScrollPane(table, 
-    		   JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
-    		   JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-       
-       	//table.
-     	//  output = new JTextField();
-       	//add(sp, BorderLayout.NORTH);
-      	// add(input, BorderLayout.NORTH);
-       	add(sp, BorderLayout.CENTER);
-   		//    sp.add(table);
-    	//   table.setValueAt(new String("test"), 0, 0);
-      
-       	//add(output, BorderLayout.SOUTH);
-       
-//       input.addActionListener(new ActionListener() {
-//
-//		public void actionPerformed(ActionEvent arg0) {
-//			//String inputText = input.getText();
-//			//String evalStr = cas.evaluateYACAS(inputText);
-//			//String evalStr = 
-//			//output.setText(evalStr);
-//		}
-//    	   
-//       });
- * */
-       
-       // init scroll pane
-       setLayout(new BorderLayout());
-       JPanel panel = new JPanel();
-       panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-       JScrollPane scrollPane = new JScrollPane(panel);
-       //setPreferredSize(new Dimension(450, 110));
-       add(scrollPane, BorderLayout.CENTER);
-       
-	
-       // focus listenerr
-       FocusListener fl = new FocusListener() {
+public class CASView extends JComponent {
+
+	private Kernel kernel;
+
+	private JTextField input, output;
+
+	private GeoGebraCAS cas;
+
+	private CASTableRenderer renderer;
+
+	private CASSession session;
+
+	private boolean showMenuBar = true;
+
+	public CASView(Application app) {
+		kernel = app.getKernel();
+		cas = new GeoGebraCAS();
+		session = new CASSession();
+		setLayout(new BorderLayout());
+
+		// input = new JTextField();
+		//JTable table = new JTable(data, columnNames);
+		//JTable table = new JTable(new CASTableModel(session));
+
+		/*
+		 //table.getModel().addTableModelListener(this);
+		 TableColumn console = table.getColumnModel().getColumn(0);
+		 console.setMinWidth(384);
+		 console.setCellRenderer(renderer);
+		 //JTable table = new JTable()
+		 //table.setFillsViewportHeight(true); <-- I think this is only for > 1.4.2
+		 //JScrollPane sp= new JScrollPane(table);
+		 JScrollPane sp= new JScrollPane(table, 
+		 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
+		 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		 
+		 //table.
+		 //  output = new JTextField();
+		 //add(sp, BorderLayout.NORTH);
+		 // add(input, BorderLayout.NORTH);
+		 add(sp, BorderLayout.CENTER);
+		 //    sp.add(table);
+		 //   table.setValueAt(new String("test"), 0, 0);
+		 
+		 //add(output, BorderLayout.SOUTH);
+		 
+		 //       input.addActionListener(new ActionListener() {
+		 //
+		 //		public void actionPerformed(ActionEvent arg0) {
+		 //			//String inputText = input.getText();
+		 //			//String evalStr = cas.evaluateYACAS(inputText);
+		 //			//String evalStr = 
+		 //			//output.setText(evalStr);
+		 //		}
+		 //    	   
+		 //       });
+		 * */
+
+		// init scroll pane
+		setLayout(new BorderLayout());
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		JScrollPane scrollPane = new JScrollPane(panel);
+		//setPreferredSize(new Dimension(450, 110));
+		add(scrollPane, BorderLayout.CENTER);
+
+		// focus listenerr
+		FocusListener fl = new FocusListener() {
 			public void focusGained(FocusEvent e) {
 				Object src = e.getSource();
-				
+
 				if (src instanceof JTextArea) {
-					JTextArea ta = (JTextArea) src;					
+					JTextArea ta = (JTextArea) src;
 					ta.setBorder(BorderFactory.createLineBorder(Color.red));
 				}
 			}
 
 			public void focusLost(FocusEvent e) {
 				Object src = e.getSource();
-				
+
 				if (src instanceof JTextArea) {
-					JTextArea ta = (JTextArea) src;					
+					JTextArea ta = (JTextArea) src;
 					ta.setBorder(null);
 				}
-			}        	
-        };
-        
-       
+			}
+		};
+
 		// CAScontroller
-        CASController casCtrl = new CASController(this, session);
-        
+		CASController casCtrl = new CASController(this, session);
+
 		// create array of text fields
 		ArrayList jcomponents = new ArrayList();
 		for (int i = 0; i < 4; i++) {
-			JTextArea ta = new JTextArea(1,1);			
+			JTextArea ta = new JTextArea(1, 1);
 			ta.setLineWrap(true);
-			
-			 // register focus listener with ta
+
+			// register focus listener with ta
 			ta.addFocusListener(fl);
-			
+
 			// register key listener
 			ta.addKeyListener(casCtrl);
-			
+
 			jcomponents.add(ta);
-		}		 		
-        initSessionPanel(panel, jcomponents);
-        
-       
-        
-        
-     }
-    
-    public GeoGebraCAS getCAS() {
-    	return cas;
-    }
-    
-    /**
-     * Inits a panel to hold all components given in the list jcomponents.
-     * @param panel
-     * @param jcomponents
-     */
-    private void initSessionPanel(JPanel panel, ArrayList jcomponents) {
-    	 panel.removeAll();		
-    	 
+		}
+		initSessionPanel(panel, jcomponents);
+
+	}
+
+	public GeoGebraCAS getCAS() {
+		return cas;
+	}
+
+	/**
+	 * Inits a panel to hold all components given in the list jcomponents.
+	 * @param panel
+	 * @param jcomponents
+	 */
+	private void initSessionPanel(JPanel panel, ArrayList jcomponents) {
+		panel.removeAll();
+
 		// create grid with one column
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
@@ -165,19 +163,17 @@ public class CASView extends JComponent
 		c.anchor = GridBagConstraints.NORTHWEST;
 		c.weightx = 1.0;
 		c.weighty = 0;
-		
-		
+
 		for (int i = 0; i < jcomponents.size(); i++) {
 			JComponent p = (JComponent) jcomponents.get(i);
 			c.gridx = 0;
 			c.gridy = i;
-								
-			panel.add(p, c);			
-		}			
-		
+
+			panel.add(p, c);
+		}
+
 		c.weighty = 1.0;
 		panel.add(Box.createVerticalGlue(), c);
-    }
-  
-   
+	}
+
 }
