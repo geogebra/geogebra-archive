d0990f024361a92ddddf4ae28a86cc37b5211820
espavilat <espavilat@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-02-15 18:58:33 +0000

    added new methods
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@570 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- tutor/persistence/dao/http/HttpStrategyDao.java
+++ tutor/persistence/dao/http/HttpStrategyDao.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import tutor.model.Strategy;
 import tutor.persistence.dao.http.impl.HttpDaoImpl;
 import tutor.persistence.dao.http.mapper.StrategyMapper;
 import tutor.persistence.dao.iface.StrategyDao;
@@ -11,7 +12,6 @@ import tutor.persistence.dao.iface.StrategyDao;
 public class HttpStrategyDao extends HttpDaoImpl implements StrategyDao {
 
 	private static String SERVICE_NAME = "estrategies";
-
 	
 	public List findStrategiesByProblemId(Long id) {
 
@@ -27,6 +27,53 @@ public class HttpStrategyDao extends HttpDaoImpl implements StrategyDao {
 		return strategies;
 	}
 
+	public List getStrategiesList() {
+		
+		String serviceName = getDataSource();
+		
+		Map params = new HashMap();
+		params.put("service", SERVICE_NAME);
+		params.put("call", "findEstrategiesList");
+		
+		List strategies = queryForList(serviceName, (Map)params, StrategyMapper.class);
+		
+		return strategies;
+	}
+
+	public Strategy getStrategyById(Long id) {
+		
+		String serviceName = getDataSource();
+		
+		Map params = new HashMap();
+		params.put("service", SERVICE_NAME);
+		params.put("call", "findEstrategiyById");
+		params.put("id", id);
+		
+		Strategy strategy = (Strategy) queryForObject(serviceName, (Map)params, StrategyMapper.class);
+		
+		return strategy;
+	}
+
+	public void removeStrategy(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeStrategy(Strategy strategy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void saveStrategy(Strategy strategy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateStrategy(Strategy strategy) {
+		// TODO Auto-generated method stub
+	
+	}
+	
 	public static void main(String[] args) throws Throwable {
 		
 		HttpStrategyDao dao = new HttpStrategyDao();
