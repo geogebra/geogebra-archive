d951853c8b4108c5d94c4d68823b7b20376c91c7
espavilat <espavilat@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-02-15 19:05:04 +0000

    Menu bar interface
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@580 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ tutor/Menubar.java
@@ -0,0 +1,9 @@
+package tutor;
+
+public interface Menubar {
+	public void initMenubar();
+	public void updateMenubar();
+	public void updateMenuFile();
+	public void updateMenuWindow();
+	public void updateSelection();
+}
