534dd291cbedb7c98ddf4883fceb29940f25505e
espavilat <espavilat@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-02-15 18:56:50 +0000

    added new methods to Strategy dao interface
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@566 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- tutor/persistence/dao/iface/StrategyDao.java
+++ tutor/persistence/dao/iface/StrategyDao.java
@@ -2,7 +2,16 @@ package tutor.persistence.dao.iface;
 
 import java.util.List;
 
+import tutor.model.Strategy;
+
 public interface StrategyDao {
 
+	public Strategy getStrategyById(Long id);
+	public List getStrategiesList();
 	public List findStrategiesByProblemId(Long id);
+	
+	public void saveStrategy(Strategy strategy);
+	public void removeStrategy(Strategy strategy);
+	public void removeStrategy(Long id);
+	public void updateStrategy(Strategy strategy);
 }
