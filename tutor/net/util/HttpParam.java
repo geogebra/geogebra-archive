44dcb3b16a621c6c5dae72079366d4250d40878e
espavilat <espavilat@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-02-15 19:00:24 +0000

    Bean for http post/get tasks
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@572 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ tutor/net/util/HttpParam.java
@@ -0,0 +1,20 @@
+package tutor.net.util;
+
+public class HttpParam {
+	
+	private String name;
+	private Object value;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Object getValue() {
+		return value;
+	}
+	public void setValue(Object value) {
+		this.value = value;
+	}
+}
