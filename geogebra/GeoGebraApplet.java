4a6b63699542dc533ee5f7f2eb979f990e3f3f19
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-04-20 20:11:19 +0000

    bugfix: "null" -> "args" (param filename ignored)
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@828 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebraApplet.java
+++ geogebra/GeoGebraApplet.java
@@ -3,7 +3,7 @@ package geogebra;
 public class GeoGebraApplet extends GeoGebraAppletBase {
 
 	protected Application buildApplication(String[] args, boolean ua) {
-		return new GeoGebraApplication(null, this, undoActive);
+		return new GeoGebraApplication(args, this, undoActive);
 	}
 
 }
