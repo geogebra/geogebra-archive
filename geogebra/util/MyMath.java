3693a3a97699e15c06db43dbcbb2058276648ec2
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-04-26 06:25:13 +0000

    bugfix: 1/3 should be 1d / 3d
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@858 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/util/MyMath.java
+++ geogebra/util/MyMath.java
@@ -23,7 +23,7 @@ public final class MyMath {
 	
 	public static final double LOG10 = Math.log(10);
 	public static final double LOG2 = Math.log(2);
-	public static final double ONE_THIRD = 1/3;
+	public static final double ONE_THIRD = 1d / 3d;
 
 
 	/**
