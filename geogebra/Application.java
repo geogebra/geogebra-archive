4999ba469d78d2e48040908aa292560c22267de8
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-09-16 17:47:16 +0000

    3.1.39.0
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@1626 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/Application.java
+++ geogebra/Application.java
@@ -144,9 +144,9 @@ public abstract class Application implements	KeyEventDispatcher {
 	public static final boolean disableI2G = false;
 	// <-- Added for Intergeo File Format (Yves Kreis)
 	public static final boolean disableSpreadsheet = false;
-    public static final String buildDate = "September 15, 2008";
+    public static final String buildDate = "September 16, 2008";
 	
-    public static final String versionString = "3.1.38.0";    
+    public static final String versionString = "3.1.39.0";    
     public static final String XML_FILE_FORMAT = "3.02";    
 	// Added for Intergeo File Format (Yves Kreis) -->
     public static final String I2G_FILE_FORMAT = "1.00.20080731";    
