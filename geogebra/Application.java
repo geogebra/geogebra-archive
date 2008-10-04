d87a0e5907dbf0cab976f681c85c6bb34c6eab23
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-10-04 21:39:59 +0000

    3.1.49.0
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@1756 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/Application.java
+++ geogebra/Application.java
@@ -90,8 +90,8 @@ import javax.swing.plaf.FontUIResource;
 public abstract class Application implements KeyEventDispatcher {
 
 	// version
-	public static final String buildDate = "October 3, 2008";
-	public static final String versionString = "3.1.48.0";
+	public static final String buildDate = "October 4, 2008";
+	public static final String versionString = "3.1.49.0";
 	public static final String XML_FILE_FORMAT = "3.02";
 	public static final String I2G_FILE_FORMAT = "1.00.20080731";
 
