4386cbb5a4362ac6ae4e5332fe3134725cf720f5
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-10-13 21:04:44 +0000

    3.1.61.0
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@1825 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/Application.java
+++ geogebra/Application.java
@@ -99,7 +99,7 @@ public abstract class Application implements KeyEventDispatcher {
 
 	// version
 	public static final String buildDate = "October 12, 2008";
-	public static final String versionString = "3.1.60.0";
+	public static final String versionString = "3.1.61.0";
 	public static final String XML_FILE_FORMAT = "3.02";
 	public static final String I2G_FILE_FORMAT = "1.00.20080731";
 
