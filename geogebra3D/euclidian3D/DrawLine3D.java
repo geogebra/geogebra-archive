4cc1e5fa171af921cf528b5afb258b131ab44a95
mathieublossier <mathieublossier@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-10-15 06:06:57 +0000

    New object.
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@1839 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra3D/euclidian3D/DrawLine3D.java
+++ geogebra3D/euclidian3D/DrawLine3D.java
@@ -1,20 +1,47 @@
 package geogebra3D.euclidian3D;
 
+import geogebra.kernel.linalg.GgbMatrix;
 import geogebra.kernel.linalg.GgbVector;
+import geogebra3D.kernel3D.GeoLine3D;
 
 public class DrawLine3D extends Drawable3D {
 
+	GeoLine3D L;
 	
+	public DrawLine3D(EuclidianView3D view, GeoLine3D l){
+		this.L=l;
+		this.view3D=view;
+		setGeoElement(l);
+		
+		update();
+	}	
 	
 	public void update() {
-		// TODO Raccord de méthode auto-généré
+		
+        isVisible = geo.isEuclidianVisible();       				 
+        if (!isVisible) return;
+		labelVisible = geo.isLabelVisible();    	
+		
+		
+		
+		GgbMatrix mc = L.getSegmentMatrix(-2,3); 
+		view3D.toScreenCoords3D(mc);
+		
+		matrix = mc.copy();
 
 	}
 	
 	
 	
 	public void draw(EuclidianRenderer3D renderer) {
-		// TODO Raccord de méthode auto-généré
+		if(!geo.isEuclidianVisible())
+			return;
+		
+		renderer.setMaterial(geo.getObjectColor(),1.0f);//TODO geo.getAlphaValue());
+		renderer.setMatrix(getMatrixGL());
+		renderer.drawCylinder(0.02f); 
+		renderer.resetMatrix();
+		
 
 	}
 
