41908ea6a7ab665577d07c850c8b9aeba17de24d
mathieublossier <mathieublossier@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-10-18 16:40:09 +0000

    Beginning with view frustum.
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@1860 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra3D/euclidian3D/GL/GLDisplay.java
+++ geogebra3D/euclidian3D/GL/GLDisplay.java
@@ -14,8 +14,7 @@ import java.util.ArrayList;
  * @author Pepijn Van Eeckhoudt
  */
 public class GLDisplay {
-    public static final int DEFAULT_WIDTH = 640;
-    public static final int DEFAULT_HEIGHT = 480;
+
 
     private static final int DONT_CARE = -1;
 
