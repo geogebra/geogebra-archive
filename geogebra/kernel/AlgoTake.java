0fb3a0dca770cca9f3fddd5d30adb001ce082ee6
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2008-10-26 07:46:38 +0000

    Take[list,k,0] returns empty list
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@1943 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/AlgoTake.java
+++ geogebra/kernel/AlgoTake.java
@@ -63,7 +63,15 @@ public class AlgoTake extends AlgoElement {
     	
     	size = inputList.size();
     	int start=(int)m.getDouble();
-    	int end= start -1 + (int)n.getDouble();
+    	double nVal = n.getDouble();
+    	int end= start -1 + (int)nVal;
+    	
+    	if (nVal == 0 && inputList.isDefined() && start >= 0 && start < size) {
+    		// return empty list
+        	outputList.setDefined(true);
+        	outputList.clear();
+        	return;
+    	}
     	
     	if (!inputList.isDefined() ||  size == 0 || start < 0 || end >= size || start > end) {
     		outputList.setUndefined();
