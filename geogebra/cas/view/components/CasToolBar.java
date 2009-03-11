03a134fbd53745d002e79b7be86b28410aa52e12
hpulven <hpulven@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2009-03-11 21:46:16 +0000

    11.03.09 Ulven: Methods for cas toolbar
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@3196 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/cas/view/components/CasToolBar.java
+++ geogebra/cas/view/components/CasToolBar.java
@@ -7,29 +7,34 @@ package geogebra.cas.view.components;
  * Special variants of needed methods:
  * 		getModeText(int mode)
  * 		getToolBarDefinition();
+ * Factory for commands:
+ * 		getCommand(int mode,CASView casview)
  * 
  * </pre>
  * @author      Hans-Petter Ulven 
  * @version     11.03.09
  */
+import geogebra.cas.view.CASView;
 
 public class CasToolBar{
 
-    /// --- Properties --- ///
 	/** Menu/Mode id's */
 	public static final int		MODE_SIMPLIFY	=	1;
-	public static final int		MODE_NUMERIC	=	2;
-	
+	public static final int		MODE_EXPAND		=	2;
+	public static final int		MODE_NUMERIC	=	3;
+
+    /// --- Properties --- ///
     
     /// --- Interface --- ///
+	
+	/** Cas getModeText */
 	public static String getModeText(int mode) {
 		switch(mode) {
 		case MODE_SIMPLIFY:				return		"Simplify";
+		case MODE_EXPAND:				return		"Expand";
 		case MODE_NUMERIC:				return		"Numeric";
 		default:						return		"";
 		}//switch(mode)
-		
-		
 	}//getModeText(mode)
 	
 	/**
@@ -42,9 +47,23 @@ public class CasToolBar{
 		StringBuffer sb	=	new StringBuffer();
 		sb.append(MODE_SIMPLIFY);
 		sb.append(" | ");
+		sb.append(MODE_EXPAND);
+		sb.append(" | ");
 		sb.append(MODE_NUMERIC);
 		//...
 		return sb.toString();
 	}//getToolbarDefinition()
 	
+	/** Factory for commands */
+	public static Command_ABS getCommand(int mode,CASView casview) {
+		switch(mode) {
+		case(MODE_SIMPLIFY):
+				return CmdEval.getInstance(casview);
+		case (MODE_EXPAND):
+				return CmdExpand.getInstance(casview);
+			default:
+				return null;
+		}//switch
+	}//getCommand(mode)
+
 }//class CasToolBar
