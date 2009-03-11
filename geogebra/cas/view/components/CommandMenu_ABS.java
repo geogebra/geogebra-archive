c0744deff129ac84597c77720daf6abc0eae25bb
hpulven <hpulven@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2009-03-11 21:05:18 +0000

    11.03.09 Ulven Abstract JMenuItem with command
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@3194 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ geogebra/cas/view/components/CommandMenu_ABS.java
@@ -0,0 +1,45 @@
+package geogebra.cas.view.components;
+
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import geogebra.main.Application;
+
+/**
+ * Abstract CommandMenu_ABS
+ * <pre>
+ * Parent for JMenuItems who
+ *      -listens to themselves, implementing ActionListener
+ *      -implements Command_IF
+ *      -should be singletons
+ * Saves a long case-statement.
+ * Can adjust all visual properties in this class.
+ * </pre>
+ * @author      Hans-Petter Ulven
+ * @version     11.03.09
+ */
+public abstract class CommandMenu_ABS extends JMenuItem implements ActionListener{
+
+    /// --- Properties --- ///
+    private Command_ABS  command =   null;
+    
+    /// --- Interface --- ///
+    
+    /** Constructor with command setting */
+    
+    /** To set the command for the JMenuItem. */
+    public void setCommand(Command_ABS command){
+        this.command=command;
+        this.addActionListener(this);
+    }//setCommand(Command_IF)
+    
+    /** Implementing ActionListener */
+    public void actionPerformed(ActionEvent ae){
+        if(command!=null){
+            command.execute();
+        }else{
+            Application.debug("No command installed!");
+        }//if command installed
+    }//actionPerformed(ActionEvent)
+    
+}//abstract class CommandMenu_ABS
