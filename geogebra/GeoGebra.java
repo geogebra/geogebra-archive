782661462862f10fb80f27692a4f3a37a12b6114
hohenwarter <hohenwarter@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2009-06-22 02:32:42 +0000

    ggb3-3-14-0
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@3760 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebra.java
+++ geogebra/GeoGebra.java
@@ -19,8 +19,8 @@ import java.net.URL;
 public class GeoGebra extends Object {
 	
 	// GeoGebra version
-	public static final String BUILD_DATE = "June 19, 2009";
-	public static final String VERSION_STRING = "3.3.13.0";
+	public static final String BUILD_DATE = "June 21, 2009";
+	public static final String VERSION_STRING = "3.3.14.0";
 		
 	// File format versions
 	public static final String XML_FILE_FORMAT = "3.3";
