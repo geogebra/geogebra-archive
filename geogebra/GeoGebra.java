ab6d5da73cee201ee3ed35ba4a65f06712cde81b
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2009-10-04 19:02:06 +0000

    virtual keyboard improvements
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@4246 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebra.java
+++ geogebra/GeoGebra.java
@@ -19,8 +19,8 @@ import java.net.URL;
 public class GeoGebra extends Object {
 	
 	// GeoGebra version
-	public static final String BUILD_DATE = "October 03, 2009";
-	public static final String VERSION_STRING = "3.3.27.0";
+	public static final String BUILD_DATE = "October 04, 2009";
+	public static final String VERSION_STRING = "3.3.28.0";
 	//public static final String VERSION_STRING = "3D alpha";
 		
 	// File format versions
