1b9cd41d9340a396e628f2bd31fe95ccb41fda81
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2009-10-05 19:30:55 +0000

    3.3.30.0
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@4257 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebra.java
+++ geogebra/GeoGebra.java
@@ -20,7 +20,7 @@ public class GeoGebra extends Object {
 	
 	// GeoGebra version
 	public static final String BUILD_DATE = "October 05, 2009";
-	public static final String VERSION_STRING = "3.3.29.0";
+	public static final String VERSION_STRING = "3.3.30.0";
 	//public static final String VERSION_STRING = "3D alpha";
 		
 	// File format versions
