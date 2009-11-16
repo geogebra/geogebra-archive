0bcbae407a4e4e316c6bba2de9f061879253cbb4
murkle <murkle@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2009-11-16 19:14:56 +0000

    3.3.39.0
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@4488 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebra.java
+++ geogebra/GeoGebra.java
@@ -19,8 +19,8 @@ import java.net.URL;
 public class GeoGebra extends Object {
 	
 	// GeoGebra version
-	public static final String  BUILD_DATE = "November 14, 2009";
-	public static final String  VERSION_STRING = "3.3.38.0";
+	public static final String  BUILD_DATE = "November 16, 2009";
+	public static final String  VERSION_STRING = "3.3.39.0";
 	public static final boolean IS_PRE_RELEASE = !VERSION_STRING.endsWith(".0");
 		
 	// enable I2G on all pre-releases
