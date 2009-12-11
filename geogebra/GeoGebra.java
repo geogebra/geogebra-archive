887bcbdef0d3fd01d4ac2c564767a6d23b373f4c
hohenwarter <hohenwarter@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2009-12-11 18:41:43 +0000

    version number
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@4580 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebra.java
+++ geogebra/GeoGebra.java
@@ -19,8 +19,8 @@ import java.net.URL;
 public class GeoGebra extends Object {
 	
 	// GeoGebra version
-	public static final String  BUILD_DATE = "December 01, 2009";
-	public static final String  VERSION_STRING = "3.3.42.0";
+	public static final String  BUILD_DATE = "December 11, 2009";
+	public static final String  VERSION_STRING = "3.3.43.0";
 	public static final boolean IS_PRE_RELEASE = !VERSION_STRING.endsWith(".0");
 		
 	// enable I2G on all pre-releases
