1553bad59e11d6b2bcbc5b82d80351017a00466a
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-02-07 16:53:21 +0000

    more efficient switches
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@4899 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/arithmetic/ExpressionNodeConstants.java
+++ geogebra/kernel/arithmetic/ExpressionNodeConstants.java
@@ -8,7 +8,7 @@ public interface ExpressionNodeConstants {
 	public static final int STRING_TYPE_GEOGEBRA = 1;
 	public static final int STRING_TYPE_JASYMCA = 2;
 	public static final int STRING_TYPE_MATH_PIPER = 3;
-	public static final int STRING_TYPE_LATEX = 100;
+	public static final int STRING_TYPE_LATEX = 4;
 	
 	public static final String UNICODE_PREFIX = "uNiCoDe";
 	public static final String UNICODE_DELIMITER = "U";  
@@ -16,20 +16,20 @@ public interface ExpressionNodeConstants {
 	public static final int NO_OPERATION = Integer.MIN_VALUE; 
     
 	// boolean
-	public static final int NOT_EQUAL = -100;
-	public static final int NOT = -99;
-	public static final int OR = -98;
-    public static final int AND = -97;
-    public static final int EQUAL_BOOLEAN = -96;
-    public static final int LESS = -95;
-    public static final int GREATER = -94;
-    public static final int LESS_EQUAL = -93;
-    public static final int GREATER_EQUAL = -92;    
-    public static final int PARALLEL = -91;  
-    public static final int PERPENDICULAR = -90;
-    public static final int IS_ELEMENT_OF = -89;
-    public static final int CONTAINS = -88;
-    public static final int CONTAINS_STRICT = -87;
+	public static final int NOT_EQUAL = -14;
+	public static final int NOT = -13;
+	public static final int OR = -12;
+    public static final int AND = -11;
+    public static final int EQUAL_BOOLEAN = -10;
+    public static final int LESS = -9;
+    public static final int GREATER = -8;
+    public static final int LESS_EQUAL = -7;
+    public static final int GREATER_EQUAL = -6;    
+    public static final int PARALLEL = -5;  
+    public static final int PERPENDICULAR = -4;
+    public static final int IS_ELEMENT_OF = -3;
+    public static final int CONTAINS = -2;
+    public static final int CONTAINS_STRICT = -1;
     
     public static final String strNOT = "\u00ac";
     public static final String strAND = "\u2227";
@@ -89,14 +89,14 @@ public interface ExpressionNodeConstants {
     public static final int VECTORPRODUCT = 40;
     public static final int CONJUGATE = 41;
      
-    public static final int FUNCTION = 100;
-    public static final int VEC_FUNCTION = 101;
-    public static final int DERIVATIVE = 110;  
+    public static final int FUNCTION = 42;
+    public static final int VEC_FUNCTION = 43;
+    public static final int DERIVATIVE = 44;  
     
     
     // spreadsheet absolute reference using $ signs
-    public static final int $VAR_ROW = 501;
-    public static final int $VAR_COL = 502;
-    public static final int $VAR_ROW_COL = 503;
+    public static final int $VAR_ROW = 45;
+    public static final int $VAR_COL = 46;
+    public static final int $VAR_ROW_COL = 47;
 	
 }
