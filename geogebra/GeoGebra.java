7b932c9be1114d04b32a301f0e0f4eff2862eede
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-02-19 20:05:37 +0000

    3.3.71
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5008 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebra.java
+++ geogebra/GeoGebra.java
@@ -20,7 +20,7 @@ public class GeoGebra extends Object {
 	
 	// GeoGebra version
 	public static final String BUILD_DATE = "February 19, 2010";
-	public static final String VERSION_STRING = "3.3.70.0";
+	public static final String VERSION_STRING = "3.3.71.0";
 	public static final String SHORT_VERSION_STRING = "4.0"; // used for online archive
 
 	// File format versions
