08adf2c8cc64c6a5f4503cd192a0636de0b1a00b
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-02-21 22:00:35 +0000

    more Maxima support
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5036 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/AlgoFactor.java
+++ geogebra/kernel/AlgoFactor.java
@@ -12,117 +12,36 @@ the Free Software Foundation.
 
 package geogebra.kernel;
 
-import geogebra.kernel.arithmetic.ExpressionNode;
-import geogebra.main.Application;
 /**
- * Try to expand the given function 
+ * Find a limit
  * 
  * @author Michael Borcherds
  */
-public class AlgoFactor extends AlgoElement {
-
-	private static final long serialVersionUID = 1L;
-	private GeoFunction f; // input
-    private GeoFunction g; // output        
-    
-    private StringBuilder sb = new StringBuilder();
+public class AlgoFactor extends AlgoSimplify {
    
     public AlgoFactor(Construction cons, String label, GeoFunction f) {
-    	super(cons);
-        this.f = f;            	
-    	
-        g = new GeoFunction(cons);                
-        setInputOutput(); // for AlgoElement        
-        compute();
-        g.setLabel(label);
+    	super(cons, label, f);
     }
     
     protected String getClassName() {
         return "AlgoFactor";
     }
-    
-    // for AlgoElement
-    protected void setInputOutput() {
-        input = new GeoElement[1];
-        input[0] = f;
+     
+    protected String getCASString(String functionIn) {
+    	
 
-        output = new GeoElement[1];
-        output[0] = g;
-        setDependencies(); // done by AlgoElement
-    }
 
-    public GeoFunction getResult() {
-        return g;
-    }
 
-    protected final void compute() {       
-        if (!f.isDefined()) {
-        	g.setUndefined();
-        	return;
-        }   
-        
-	    /* Experimental Maxima code
-
-        // *************************** TODO: change to STRING_TYPE_MAXIMA
-	    String functionIn = f.getFormulaString(ExpressionNode.STRING_TYPE_MATH_PIPER, true);
-
+    	
 	    sb.setLength(0);
-        sb.append("factor(");
+        sb.append("Factor(");
         sb.append(functionIn);
-        sb.append(");");
-		String functionOut = kernel.evaluateMaxima(sb.toString());
-		
-		Application.debug("Factorize input:"+functionIn);
-		Application.debug("Factorize output:"+functionOut);
-		
-		if (functionOut == null || functionOut.length()==0) 
-		{
-			g.set(f); // set to input ie leave unchanged
-		}
-		else
-		{
-			g.set(kernel.getAlgebraProcessor().evaluateToFunction(functionOut));					
-		} */
-                
+        sb.append(')');
+
         
-	    String functionIn = f.getFormulaString(ExpressionNode.STRING_TYPE_MATH_PIPER, true);
+        return sb.toString();
+        
 
-	    
-	    sb.setLength(0);
-	    // added Rationalize to cope with eg Factor[9.1 x^7 - 32 x^6 + 48 x^5 - 40 x^4 + 20 x³ - 6 x² + x ]
-        sb.append("Factor(Rationalize(");
-        sb.append(functionIn);
-        sb.append("))");
-		String functionOut = kernel.evaluateMathPiper(sb.toString());
-		
-		//Application.debug("Factorize input:"+functionIn);
-		//Application.debug("Factorize output:"+functionOut);
-		
-		boolean yacasError=false;
-		
-		if (functionOut == null || functionOut.length()==0) yacasError=true; // Yacas error
-		
-		else if (functionOut.length()>7)
-			if (functionOut.startsWith("Factor(") || // Yacas error
-			    functionOut.startsWith("FWatom(") )  // Yacas oddity??
-				yacasError=true;
-			
-
-		if (yacasError) // Yacas error
-		{
-			g.set(f); // set to input ie leave unchanged
-		}
-		else
-		{
-			g.set(kernel.getAlgebraProcessor().evaluateToFunction(functionOut));					
-		}
-		
-		g.setDefined(true);	
-		
-    }
-    
-    final public String toString() {
-    	return getCommandDescription();
     }
 
 }
