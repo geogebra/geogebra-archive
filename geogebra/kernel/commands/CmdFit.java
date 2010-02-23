96dc1421b3af945295ee5dd40425f2afde163bd9
hans-petter <hans-petter@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-02-23 17:46:32 +0000

    Fit[<list of pts>,<list of funcs>]
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5063 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ geogebra/kernel/commands/CmdFit.java
@@ -0,0 +1,47 @@
+package geogebra.kernel.commands; 
+/* 
+GeoGebra - Dynamic Mathematics for Everyone
+http://www.geogebra.org
+
+This file is part of GeoGebra.
+
+This program is free software; you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by 
+the Free Software Foundation.
+
+*/
+import geogebra.kernel.GeoElement;
+import geogebra.kernel.GeoList;
+import geogebra.kernel.Kernel;
+import geogebra.kernel.arithmetic.Command;
+import geogebra.kernel.arithmetic.NumberValue;
+import geogebra.main.MyError;
+
+/** 
+ * Fit[<List Points>,<List of Functions>]
+ * 
+ * @author Hans-Petter Ulven
+ * @version 2010-02-22
+ */
+public class CmdFit extends CommandProcessor{
+
+    public CmdFit(Kernel kernel) {super(kernel);}
+    
+    public GeoElement[] process(Command c) throws MyError {
+        int n=c.getArgumentNumber();
+        GeoElement[] arg=resArgs(c);;
+        switch(n) {
+            case 2:
+                    if(  (arg[0].isGeoList() )&& (arg[1].isGeoList())  ){ 
+                        GeoElement[] ret={kernel.Fit(c.getLabel(),(GeoList)arg[0],(GeoList) arg[1]) };
+                        return ret;
+                    }else{
+                        throw argErr(app,c.getName(),arg[0]);
+                    }//if arg[0] is GeoList 
+
+            default :
+
+    			throw argNumErr(app, c.getName(), n);
+        }//switch(number of arguments)
+    }//process(Command) 
+}// class CmdFit
