7e00ecfd3d7a9047a06a5d3d4100febb183b8bcf
hans-petter <hans-petter@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-02-23 17:46:06 +0000

    Fit[<list of pts>,<list of funcs>]
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5062 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CommandDispatcher.java
+++ geogebra/kernel/commands/CommandDispatcher.java
@@ -235,6 +235,7 @@ public class CommandDispatcher {
     	cmdTable.put("FitExp", new CmdFitExp(kernel));
     	cmdTable.put("FitLog", new CmdFitLog(kernel));
     	cmdTable.put("FitPow", new CmdFitPow(kernel));
+    	cmdTable.put("Fit",new CmdFit(kernel));
         cmdTable.put("RandomNormal", new CmdRandomNormal(kernel));
         cmdTable.put("ConstructionStep", new CmdConstructionStep(kernel));
         cmdTable.put("Normal", new CmdNormal(kernel));
