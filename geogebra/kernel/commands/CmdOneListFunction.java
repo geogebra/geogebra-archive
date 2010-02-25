5e927d0d658b28cbe702afbc8561d5776152ae9a
hans-petter <hans-petter@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-02-25 03:48:19 +0000

    FitGrowth
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5081 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CmdOneListFunction.java
+++ geogebra/kernel/commands/CmdOneListFunction.java
@@ -183,6 +183,23 @@ class CmdFitLogistic extends CmdOneListFunction{
 	}
 
 }// class CmdFitLogistic
+
+/** 
+ * FitGrowth[<List of points>]
+ * 
+ * @author Hans-Petter Ulven
+ * @version 2010-02-25
+ */
+class CmdFitGrowth extends CmdOneListFunction{
+
+    public CmdFitGrowth(Kernel kernel) {super(kernel);}
+    
+	final protected GeoElement doCommand(String a, GeoList b)
+	{
+		return kernel.FitGrowth(a, b);
+	}
+}//class CmdFitGrowth
+
 /*
  * Mean[ list ]
  *  Michael Borcherds 2008-04-12
