7d6e9594510b00988610e16bb7ea5babf1e5f18c
markus <markus@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-04-18 20:45:33 +0000

    mathpiper update
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5561 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/GeoGebra.java
+++ geogebra/GeoGebra.java
@@ -22,7 +22,7 @@ public class GeoGebra extends Object {
 	
 	// GeoGebra version
 	public static final String BUILD_DATE = "18 April 2010";
-	public static final String VERSION_STRING = "3.3.110.0";
+	public static final String VERSION_STRING = "3.3.111.0";
 	public static final String SHORT_VERSION_STRING = "4.0"; // used for online archive
 
 	// File format versions
