43f38777d4705b66c307cb8877198819a34cb7ce
zbynek <zbynek@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-05-17 19:12:20 +0000

    Error messages for PointIn
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5724 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CmdPointIn.java
+++ geogebra/kernel/commands/CmdPointIn.java
@@ -8,15 +8,24 @@ import geogebra.kernel.arithmetic.Command;
 import geogebra.main.MyError;
 
 
-/*
+/**
  * PointIn[ <Region> ] 
+ * @version 2010-05-17
  */
 public class CmdPointIn extends CommandProcessor {
 
+	/**
+	 * Initiates command processor for PointIn command
+	 * @param kernel Kernel used for computations
+	 */
 	public CmdPointIn (Kernel kernel) {
 		super(kernel);
 	}
 
+	/**
+	 * Checks correctness of inputs and runs the command.
+	 * Last change: correct error messages (2010-05-17), Zbynek Konecny 
+	 */
 	public  GeoElement[] process(Command c) throws MyError {
 		int n = c.getArgumentNumber();
 		boolean[] ok = new boolean[n];
@@ -29,10 +38,9 @@ public class CmdPointIn extends CommandProcessor {
 				{ kernel.PointIn(c.getLabel(), (Region) arg[0])};
 				return ret;
 			} else
-				throw argErr(app, "Point", arg[0]);
+				throw argErr(app, "PointIn", arg[0]);
 		}else
-			throw argNumErr(app, "Point", n);
-
+			throw argNumErr(app, "PointIn", n);
 
 	}
 }
