e72e5976eedfd8c6db229765eef4e1d2e82ebf57
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-05-25 19:05:56 +0000

    new version of maximaconnector (jacomax)
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5756 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ uk/ac/ed/ph/jacomax/internal/DummyLogger.java
@@ -0,0 +1,113 @@
+package uk.ac.ed.ph.jacomax.internal;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+import uk.ac.ed.ph.jacomax.MaximaConfiguration;
+
+public class DummyLogger {
+
+	public void trace(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String string, Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void trace(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isTraceEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void trace(String string, int bytesReadFromCallInput) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String string, Integer valueOf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void trace(String string, boolean maximaStdinFinished, Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void trace(String string, Object[] objects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String string, File programFilesFolder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String string, MaximaConfiguration result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String string, String location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void warn(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String string, String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String string, InputStream propertiesStream) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String string, String callInput, int callTimeout) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String string, String maximaInput,
+			String callTerminatorOutput) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isInfoEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void info(String string, List<String> maximaCommandArray,
+			String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
