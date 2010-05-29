f5a60dff3851cf8c2d464a1f6e3070c303bcabbe
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-05-29 19:12:35 +0000

    Normal[0,1,x]
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5788 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CmdNormal.java
+++ geogebra/kernel/commands/CmdNormal.java
@@ -1,9 +1,12 @@
 package geogebra.kernel.commands;
 
 import geogebra.kernel.GeoElement;
+import geogebra.kernel.GeoFunction;
 import geogebra.kernel.Kernel;
 import geogebra.kernel.arithmetic.Command;
+import geogebra.kernel.arithmetic.ExpressionNode;
 import geogebra.kernel.arithmetic.NumberValue;
+import geogebra.main.Application;
 import geogebra.main.MyError;
 
 /*
@@ -19,23 +22,32 @@ public class CmdNormal extends CommandProcessor {
 
 	public GeoElement[] process(Command c) throws MyError {
 		int n = c.getArgumentNumber();
-		boolean[] ok = new boolean[n];
+		boolean ok;
 		GeoElement[] arg;
 
 		switch (n) {
 		case 3:			
 			arg = resArgs(c);
-			if ((ok[0] = arg[0].isNumberValue()) &&
-				(ok[1] = arg[1].isNumberValue()) &&
-				(ok[2] = arg[2].isNumberValue())) 
-			{
-				GeoElement[] ret = { 
-						kernel.Normal(c.getLabel(),
-						(NumberValue) arg[0], (NumberValue) arg[1], (NumberValue) arg[2]) };
-				return ret;
-				
-			}  else
-				throw argErr(app, c.getName(), arg[0]);
+			if ((ok = arg[0].isNumberValue()) && (arg[1].isNumberValue())) {
+				if (arg[2].isGeoFunction() && ((GeoFunction)arg[2]).toString().equals("x")) {
+									
+					String mean = arg[0].getLabel();
+					String sd = arg[1].getLabel();
+					
+					GeoElement[] ret = kernel.getAlgebraProcessor().processAlgebraCommand( "1/sqrt(2 * pi) / abs("+sd+")*exp(-((x-("+mean+"))/("+sd+"))^2/2)", true );
+					
+					return ret;
+					
+				} else if (arg[2].isNumberValue()) 
+				{
+					GeoElement[] ret = { 
+							kernel.Normal(c.getLabel(),
+							(NumberValue) arg[0], (NumberValue) arg[1], (NumberValue) arg[2]) };
+					return ret;
+					
+				}  else
+					throw argErr(app, c.getName(), arg[2]);
+		} else throw argErr(app, c.getName(), ok ? arg[1] : arg[0]);
 
 		default:
 			throw argNumErr(app, c.getName(), n);
