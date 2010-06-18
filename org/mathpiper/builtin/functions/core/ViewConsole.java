414db3ba5c8323a0f21dd0666bab06aab2a682a6
markus <markus@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-06-18 18:56:33 +0000

    mathpiper update
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@5937 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ org/mathpiper/builtin/functions/core/ViewConsole.java
@@ -0,0 +1,58 @@
+/* {{{ License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ //}}}
+
+// :indentSize=4:lineSeparator=\n:noTabs=false:tabSize=4:folding=explicit:collapseFolds=0:
+
+package org.mathpiper.builtin.functions.core;
+
+import org.mathpiper.builtin.BuiltinFunction;
+import org.mathpiper.lisp.Environment;
+import org.mathpiper.lisp.Utility;
+
+/**
+ *
+ *
+ */
+public class ViewConsole extends BuiltinFunction
+{
+
+    public void evaluate(Environment aEnvironment, int aStackTop) throws Exception
+    {
+         org.mathpiper.ui.gui.consoles.Console.main(null);
+
+         Utility.putTrueInPointer(aEnvironment, getTopOfStackPointer(aEnvironment, aStackTop));
+
+    }//end method.
+
+}//end class.
+
+
+
+
+/*
+%mathpiper_docs,name="ViewConsole",categories="User Functions;Built In"
+*CMD ViewConsole --- show the console window
+*CORE
+*CALL
+    ViewConsole()
+
+*DESC
+
+Shows the console window.
+
+
+%/mathpiper_docs
+*/
