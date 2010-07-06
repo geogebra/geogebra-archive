2f57c2780b07fd4aac80c62ab2f265df56ab1d91
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-07-06 17:19:15 +0000

    GPL v2 -> GPL
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@6103 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/statistics/AlgoRandomNormal.java
+++ geogebra/kernel/statistics/AlgoRandomNormal.java
@@ -5,7 +5,7 @@ http://www.geogebra.org
 This file is part of GeoGebra.
 
 This program is free software; you can redistribute it and/or modify it 
-under the terms of the GNU General Public License v2 as published by 
+under the terms of the GNU General Public License as published by 
 the Free Software Foundation.
 
  */
