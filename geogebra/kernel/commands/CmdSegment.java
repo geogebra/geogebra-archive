02ee7eb8e71cef90db80b26f3d02c691b7b103b4
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-08-15 10:32:54 +0000

    special case for Segment[A,B,poly1] -> do nothing!
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@6459 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CmdSegment.java
+++ geogebra/kernel/commands/CmdSegment.java
@@ -54,6 +54,17 @@ public class CmdSegment extends CommandProcessor {
 	                    throw argErr(app, "Segment", arg[1]);
 	            }
 
+	        case 3 : // special case for Segment[A,B,poly1] -> do nothing!
+	            arg = resArgs(c);
+
+	            if ((ok[0] = (arg[0] .isGeoPoint()))
+	            		&& (ok[1] = (arg[1] .isGeoPoint()))
+	            		&& (ok[2] = (arg[2] .isGeoPolygon()))) {
+	                GeoElement[] ret = {};
+	                return ret;
+	            }
+	            throw argNumErr(app, "Segment", n);
+
 	        default :
 	            throw argNumErr(app, "Segment", n);
 	    }
