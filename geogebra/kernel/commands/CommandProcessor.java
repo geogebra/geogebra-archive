20261630cfc39f9177fdcc467ab3b11c24e7415b
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-08-15 18:20:04 +0000

    ZoomIn[ <Scale Factor>, <Center Point> ] ZoomOut[ <Scale Factor>, <Center Point> ]
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@6468 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CommandProcessor.java
+++ geogebra/kernel/commands/CommandProcessor.java
@@ -6752,31 +6752,48 @@ class CmdOsculatingCircle extends CommandProcessor {
 					
 					EuclidianView ev = app.getEuclidianView();					
 					double px = ev.getWidth() /2 ; //mouseLoc.x;
-					double py = ev.getHeight() / 2 ; //mouseLoc.y;
-					double dx = ev.getXZero() - px;
-					double dy = ev.getYZero() - py;
-					
+					double py = ev.getHeight() / 2 ; //mouseLoc.y;			
 
 				double factor = numGeo.getDouble();
 				if (kernel.isZero(factor))
 					throw argErr(app, c.getName(), arg[0]);
-
-				// make zooming a little bit smoother by having some steps
-
 				
-				ev.setAnimatedCoordSystem(
-						px + dx * factor,
-						py + dy * factor,
-						ev.getXscale() * factor, 4, false);
-				//view.yscale * factor);
+				ev.zoom(px, py, factor, 4, true);
+				
 				app.setUnsaved();
 
 									
-					GeoElement[] ret = { numGeo };
+					GeoElement[] ret = {};
 					return ret;
 
 				} else
 					throw argErr(app, c.getName(), arg[0]);
+			case 2 :
+				arg = resArgs(c);
+				boolean ok0;
+				if ((ok0=arg[0].isGeoNumeric()) && arg[1].isGeoPoint()) {
+					GeoNumeric numGeo = (GeoNumeric)arg[0];
+					GeoPoint p = (GeoPoint)arg[1];
+					int zoom = (int)numGeo.getDouble();
+					
+					EuclidianView ev = app.getEuclidianView();					
+					double px = ev.toScreenCoordXd(p.inhomX); //mouseLoc.x;
+					double py = ev.toScreenCoordYd(p.inhomY); //mouseLoc.y;
+
+				double factor = numGeo.getDouble();
+				if (kernel.isZero(factor))
+					throw argErr(app, c.getName(), arg[0]);
+				
+				ev.zoom(px, py, factor, 4, true);
+
+				app.setUnsaved();
+
+									
+					GeoElement[] ret = {};
+					return ret;
+
+				} else
+					throw argErr(app, c.getName(), ok0 ? arg[1] : arg[0]);
 
 			default :
 				throw argNumErr(app, c.getName(), n);
@@ -6803,34 +6820,51 @@ class CmdOsculatingCircle extends CommandProcessor {
 					int zoom = (int)numGeo.getDouble();
 					
 					EuclidianView ev = app.getEuclidianView();					
-					double px = ev.getWidth() /2 ; //mouseLoc.x;
+					double px = ev.getWidth() / 2 ; //mouseLoc.x;
 					double py = ev.getHeight() / 2 ; //mouseLoc.y;
-					double dx = ev.getXZero() - px;
-					double dy = ev.getYZero() - py;
-					
 
 				double factor = numGeo.getDouble();
 				
 				if (kernel.isZero(factor))
 					throw argErr(app, c.getName(), arg[0]);
 
-				// make zooming a little bit smoother by having some steps
-
-				
-				ev.setAnimatedCoordSystem(
-						px + dx * factor,
-						py + dy * factor,
-						ev.getXscale() / factor, 4, false);
-				//view.yscale * factor);
+				ev.zoom(px, py, 1 / factor, 4, true);
+			
 				app.setUnsaved();
-
 									
-					GeoElement[] ret = { numGeo };
+					GeoElement[] ret = {};
 					return ret;
 
 				} else
 					throw argErr(app, c.getName(), arg[0]);
 				
+			case 2 :
+				arg = resArgs(c);
+				boolean ok0;
+				if ((ok0=arg[0].isGeoNumeric()) && arg[1].isGeoPoint()) {
+					GeoNumeric numGeo = (GeoNumeric)arg[0];
+					GeoPoint p = (GeoPoint)arg[1];
+					int zoom = (int)numGeo.getDouble();
+					
+					EuclidianView ev = app.getEuclidianView();					
+					double px = ev.toScreenCoordXd(p.inhomX); //mouseLoc.x;
+					double py = ev.toScreenCoordYd(p.inhomY); //mouseLoc.y;
+
+				double factor = numGeo.getDouble();
+				if (kernel.isZero(factor))
+					throw argErr(app, c.getName(), arg[0]);
+				
+				ev.zoom(px, py, 1 / factor, 4, true);
+
+				app.setUnsaved();
+
+									
+					GeoElement[] ret = {};
+					return ret;
+
+				} else
+					throw argErr(app, c.getName(), ok0 ? arg[1] : arg[0]);
+
 			default :
 				throw argNumErr(app, c.getName(), n);
 			}
