39c250a4a741d782cd0fdf46fe65f80e56b083b7
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-08-17 09:49:24 +0000

    improved JavaScript error handling
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@6515 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/plugin/GeoGebraGlobal.java
+++ geogebra/plugin/GeoGebraGlobal.java
@@ -1,5 +1,6 @@
 package geogebra.plugin;
 
+import geogebra.kernel.GeoElement;
 import geogebra.main.Application;
 import geogebra.main.MyError;
 
@@ -48,22 +49,40 @@ public class GeoGebraGlobal implements IdFunctionCall {
 			f.exportAsScopeProperty();
 		}
 	}
-
+	
 	public Object execIdCall(IdFunctionObject f, Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
 		if (f.hasTag(FTAG)) {
 			int methodId = f.methodId();
 			switch (methodId) {
 			case Id_alert: {
+				
+				if (args.length != 1) {
+			        String error = argNumError(args.length, "alert( <String> )");
+			        app.showError(error);
+					throw new Error(error);
+				}
+				
 				Object value = (args.length != 0) ? args[0] : Undefined.instance;
 				
 				if (!(value instanceof String)) {
-					int[] linep = new int[1];
-			        Context.getSourcePositionFromStack(linep); // line number of error
-			        app.showError(app.getPlain("ErrorInJavaScript")+"\n"+app.getPlain("SynataxAatLineB", "alert(String)", linep[0]+""));
-			        return "";
+			        //app.showError(app.getPlain("ErrorInJavaScript")+"\n"+app.getPlain("SynataxAatLineB", "alert(String)", linep[0]+""));
+			        String error = argError(value.toString(), "alert( <String> )");
+			        app.showError(error);
+					throw new Error(error);
 				}
-
-				app.showMessage((String) value);
+				
+				Object[] options = {app.getPlain("StopScript"), app.getPlain("OK")};
+				int n = JOptionPane.showOptionDialog(app.getFrame(),
+						(String) value,
+					    "GeoGebra",
+					    JOptionPane.YES_NO_OPTION,
+					    JOptionPane.QUESTION_MESSAGE,
+					    null,     //do not use a custom Icon
+					    options,  //the titles of buttons
+					    options[0]); //default button title
+				
+				if (n == 0) throw new Error("Script stopped by user");
+				
 				return "";
 			}
 			case Id_prompt: {
@@ -82,6 +101,41 @@ public class GeoGebraGlobal implements IdFunctionCall {
 			}
 		}
 		throw f.unknown();
+	}
+	
+	private StringBuilder sb;
+	int[] linep = new int[1];
+	
+	private String argError(Object arg, String syntax) {
+		if (sb == null) sb = new StringBuilder();
+		else sb.setLength(0);
+        Context.getSourcePositionFromStack(linep); // line number of error
+		sb.append(app.getPlain("ErrorInJavaScriptAtLineA", linep[0]+""));
+		sb.append("\n");
+		sb.append(app.getError("IllegalArgument"));
+		sb.append(": ");
+		sb.append(arg.toString());
+		sb.append("\n\n");
+		sb.append(app.getPlain("Syntax"));
+		sb.append(":\n");
+		sb.append(syntax);
+		return sb.toString();
+	}
+
+	private String argNumError(int argNumber, String syntax) {
+		if (sb == null) sb = new StringBuilder();
+		else sb.setLength(0);
+        Context.getSourcePositionFromStack(linep); // line number of error
+		sb.append(app.getPlain("ErrorInJavaScriptAtLineA", linep[0]+""));
+		sb.append("\n");
+		sb.append(app.getError("IllegalArgumentNumber"));
+		sb.append(": ");
+		sb.append(argNumber);
+		sb.append("\n\n");
+		sb.append(app.getPlain("Syntax"));
+		sb.append(":\n");
+		sb.append(syntax);
+		return sb.toString();
 	}
 
 	public static void initStandardObjects(Application app, Scriptable scope, String arg, boolean sealed) {
