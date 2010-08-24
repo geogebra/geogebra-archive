f7553be89854e444d3e9d337052b87b5977ab2f5
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-08-24 21:32:30 +0000

    SlowPlot[ ] improvements
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@6634 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CommandProcessor.java
+++ geogebra/kernel/commands/CommandProcessor.java
@@ -4593,10 +4593,10 @@ class CmdOsculatingCircle extends CommandProcessor {
 		 case 1 :
 			 if ( (ok[0] = (arg[0].isGeoFunctionable()) ) ){
 				 
-				 Application.debug(arg[0].getLabel());
-				 
-				 
 				 GeoNumeric var = new GeoNumeric(cons, 0.0);
+				 
+				 arg[0].setEuclidianVisible(false);
+				 arg[0].update();
 				 
 				 var.setLabel(null); // set label to next available
 				 var.setEuclidianVisible(true);
@@ -4607,10 +4607,18 @@ class CmdOsculatingCircle extends CommandProcessor {
 				 var.setAnimationType(GeoElement.ANIMATION_INCREASING);
 				 var.update();
 				 
-				 String cmd = "Function["+arg[0].getLabel()+",x(Corner[1]), x(Corner[1]) (1-"+var.getLabel()+") + x(Corner(2)) "+var.getLabel()+"]";
+				 StringBuilder sb = new StringBuilder();
+				 sb.append("Function[");
+				 sb.append(arg[0].getLabel());
+				 sb.append(",x(Corner[1]), x(Corner[1]) (1-");
+				 sb.append(var.getLabel());
+				 sb.append(") + x(Corner(2)) ");
+				 sb.append(var.getLabel());
+				 sb.append("]");
+				 
 				 kernel.getAnimatonManager().startAnimation();
 				try {
-					return kernel.getAlgebraProcessor().processAlgebraCommandNoExceptionHandling(cmd, true, false, true);
+					return kernel.getAlgebraProcessor().processAlgebraCommandNoExceptionHandling(sb.toString(), true, false, true);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
