8251dec2b3e35767d63dec1c5f2a8481b44f7891
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-09-03 18:50:19 +0000

    bugfix: wrong label for CurveCartesian in 3D
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@6738 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/GeoCurveCartesianND.java
+++ geogebra/kernel/GeoCurveCartesianND.java
@@ -125,7 +125,7 @@ public abstract class GeoCurveCartesianND extends GeoElement{
 		if (isLabelSet()) {
 			sbToString.append(label);
 			sbToString.append('(');
-			sbToString.append(fun[0].getFunctionVariables().toString());
+			sbToString.append(fun[0].getFunctionVariables()[0].toString());
 			sbToString.append(") = ");					
 		}		
 		sbToString.append(toValueString());
