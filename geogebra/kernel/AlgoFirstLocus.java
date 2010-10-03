944d36e428ad532b3c8789f10aa2149b0059de04
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-10-03 20:51:35 +0000

    improved First[ <Locus> ] to avoid memory problems
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@7012 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/AlgoFirstLocus.java
+++ geogebra/kernel/AlgoFirstLocus.java
@@ -1,5 +1,7 @@
 package geogebra.kernel;
 
+import geogebra.main.Application;
+
 import java.util.ArrayList;
 
 public class AlgoFirstLocus extends AlgoFirst {
@@ -28,18 +30,39 @@ public class AlgoFirstLocus extends AlgoFirst {
     	} 
        
     	outputList.setDefined(true);
-    	outputList.clear();
     	
-    	if (outsize == 0) return; // return empty list
+    	if (outsize == 0) {
+        	outputList.clear();
+    		return; // return empty list
+    	}
+    	
+    	int outputListSize = outputList.size();
+    	
+    	// remove extra elements
+    	if (outputList.size() > outsize) {
+    		for (int i = outputListSize - 1 ; i >= outsize ; i-- ) {
+	    		outputList.remove(i);
+    		}
+    	}
     	
     	// avoid label creation
     	boolean oldSuppressLabels = cons.isSuppressLabelsActive();
     	cons.setSuppressLabelCreation(true);
     	
-    	for (int i=0 ; i<outsize ; i++) {
+    	for (int i=0 ; i < outsize ; i++) {
     		MyPoint mp = points.get(i);
-    		GeoPoint p = new GeoPoint(cons, null, mp.x, mp.y, 1.0);
-    		outputList.add(p);
+   		
+    		if (i < outputList.size()) {
+    			// recycle existing GeoPoint
+    			// important to avoid memory problems
+    			// (and quicker?)
+    			GeoPoint p = (GeoPoint)outputList.get(i); 
+    			p.setCoords(mp.x, mp.y, 1.0);
+    			p.updateRepaint();
+    		} else {
+	    		GeoPoint p = new GeoPoint(cons, null, mp.x, mp.y, 1.0);
+	    		outputList.add(p);
+    		}
     	}
     	
     	cons.setSuppressLabelCreation(oldSuppressLabels);
