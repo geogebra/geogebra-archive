08632be0f2708cfd9b9ba1db936fca5aeba347fb
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-11-02 22:45:57 +0000

    improved alt-behaviour for Polygon Tool
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@7361 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/euclidian/DrawPolygon.java
+++ geogebra/euclidian/DrawPolygon.java
@@ -8,18 +8,21 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by 
 the Free Software Foundation.
 
-*/
+ */
 
 package geogebra.euclidian;
 
 import geogebra.Matrix.GgbVector;
+import geogebra.kernel.Construction;
 import geogebra.kernel.ConstructionDefaults;
 import geogebra.kernel.GeoElement;
+import geogebra.kernel.GeoLine;
 import geogebra.kernel.GeoPoint;
 import geogebra.kernel.GeoPolygon;
+import geogebra.kernel.GeoVec2D;
+import geogebra.kernel.GeoVec3D;
 import geogebra.kernel.Kernel;
 import geogebra.kernel.kernelND.GeoPointND;
-import geogebra.main.Application;
 
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
@@ -27,237 +30,299 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 /**
- *
- * @author  Markus Hohenwarter
- * @version 
+ * 
+ * @author Markus Hohenwarter
+ * @version
  */
-public class DrawPolygon extends Drawable
-implements Previewable {
-   
-    private GeoPolygon poly;            
-    private boolean isVisible, labelVisible;
-    
-    private GeneralPathClipped gp;
-    private double [] coords = new double[2];
-	private ArrayList<GeoPoint> points;              
-     
+public class DrawPolygon extends Drawable implements Previewable {
+
+	private GeoPolygon poly;
+	private boolean isVisible, labelVisible;
+
+	private GeneralPathClipped gp;
+	private double[] coords = new double[2];
+	private ArrayList<GeoPoint> points;
+
 	/**
 	 * Creates new DrawPolygon
-	 * @param view Euclidian view to be used
-	 * @param poly Polygon to be drawn
+	 * 
+	 * @param view
+	 *            Euclidian view to be used
+	 * @param poly
+	 *            Polygon to be drawn
 	 */
-    public DrawPolygon(EuclidianView view, GeoPolygon poly) {
-		this.view = view; 
-		this.poly = poly;		
+	public DrawPolygon(EuclidianView view, GeoPolygon poly) {
+		this.view = view;
+		this.poly = poly;
 		geo = poly;
 
 		update();
-    }
-    
-    /**
-     * Creates a new DrawPolygon for preview.
-     * @param view Euclidian view to be used
-     * @param points vertices     
-     */
+	}
+
+	/**
+	 * Creates a new DrawPolygon for preview.
+	 * 
+	 * @param view
+	 *            Euclidian view to be used
+	 * @param points
+	 *            vertices
+	 */
 	DrawPolygon(EuclidianView view, ArrayList<GeoPoint> points) {
-		this.view = view; 
+		this.view = view;
 		this.points = points;
 
 		updatePreview();
-	} 
+	}
 
 	final public void update() {
-        isVisible = geo.isEuclidianVisible();
-        if (isVisible) { 
-			labelVisible = geo.isLabelVisible();       
+		isVisible = geo.isEuclidianVisible();
+		if (isVisible) {
+			labelVisible = geo.isLabelVisible();
 			updateStrokes(poly);
-			
-            // build general path for this polygon
-			isVisible=addPointsToPath(poly.getPointsND());
+
+			// build general path for this polygon
+			isVisible = addPointsToPath(poly.getPointsND());
 			if (!isVisible)
 				return;
 			gp.closePath();
-        	
-        	 // polygon on screen?		
-    		if (!gp.intersects(0,0, view.width, view.height)) {				
-    			isVisible = false;
-            	// don't return here to make sure that getBounds() works for offscreen points too
-    		}             
-    		// draw trace
-    		if (poly.trace) {
-    			isTracing = true;
-    			Graphics2D g2 = view.getBackgroundGraphics();
-    			if (g2 != null) fill(g2, gp, false);
-    		} else {
-    			if (isTracing) {
-    				isTracing = false;
-    				view.updateBackground();
-    			}
-    		}	
-            
-        }
-    }
-	
-	//return false if a point doesn't lie on the plane
+
+			// polygon on screen?
+			if (!gp.intersects(0, 0, view.width, view.height)) {
+				isVisible = false;
+				// don't return here to make sure that getBounds() works for
+				// offscreen points too
+			}
+			// draw trace
+			if (poly.trace) {
+				isTracing = true;
+				Graphics2D g2 = view.getBackgroundGraphics();
+				if (g2 != null)
+					fill(g2, gp, false);
+			} else {
+				if (isTracing) {
+					isTracing = false;
+					view.updateBackground();
+				}
+			}
+
+		}
+	}
+
+	// return false if a point doesn't lie on the plane
 	private boolean addPointsToPath(GeoPointND[] points) {
 		if (gp == null)
 			gp = new GeneralPathClipped(view);
 		else
 			gp.reset();
-		
+
 		// first point
 		GgbVector v = points[0].getInhomCoordsInD(3);
-		if (!Kernel.isZero(v.getZ())) //TODO generalize
-			return false;		
-		coords[0] = v.getX(); coords[1] = v.getY();
-		view.toScreenCoords(coords);			
-        gp.moveTo(coords[0], coords[1]);   
-		
+		if (!Kernel.isZero(v.getZ())) // TODO generalize
+			return false;
+		coords[0] = v.getX();
+		coords[1] = v.getY();
+		view.toScreenCoords(coords);
+		gp.moveTo(coords[0], coords[1]);
+
 		// for centroid calculation (needed for label pos)
 		double xsum = coords[0];
 		double ysum = coords[1];
-        
-        for (int i=1; i < points.length; i++) {
-        	v = points[i].getInhomCoordsInD(3);
-    		if (!Kernel.isZero(v.getZ())){ //TODO generalize
-    			return false;		
-    		}
-    		coords[0] = v.getX(); coords[1] = v.getY();
-			view.toScreenCoords(coords);	
+
+		for (int i = 1; i < points.length; i++) {
+			v = points[i].getInhomCoordsInD(3);
+			if (!Kernel.isZero(v.getZ())) { // TODO generalize
+				return false;
+			}
+			coords[0] = v.getX();
+			coords[1] = v.getY();
+			view.toScreenCoords(coords);
 			if (labelVisible) {
 				xsum += coords[0];
 				ysum += coords[1];
-			}			
-        	gp.lineTo(coords[0], coords[1]);                  	
-        }
+			}
+			gp.lineTo(coords[0], coords[1]);
+		}
 
 		if (labelVisible) {
-			labelDesc = geo.getLabelDescription();  
+			labelDesc = geo.getLabelDescription();
 			xLabel = (int) (xsum / points.length);
 			yLabel = (int) (ysum / points.length);
-			addLabelOffset();                                       
-		} 
-		
+			addLabelOffset();
+		}
+
 		return true;
 	}
-        
+
 	final public void draw(Graphics2D g2) {
 		if (isVisible) {
-			fill(g2, gp, false); // fill using default/hatching/image as appropriate
+			fill(g2, gp, false); // fill using default/hatching/image as
+									// appropriate
 
 			if (geo.doHighlighting()) {
 				g2.setPaint(poly.getSelColor());
-                g2.setStroke(selStroke);            
-                g2.draw(gp);                
-            }        
-        	
-            // polygons (e.g. in GeoLists) that don't have labeled segments
-            // should also draw their border
-            else if (!poly.wasInitLabelsCalled() && poly.lineThickness > 0) {
-        		 g2.setPaint(poly.getObjectColor());
-                 g2.setStroke(objStroke);            
-                 g2.draw(gp);  
-        	}
-        	
-                                  
-            if (labelVisible) {
+				g2.setStroke(selStroke);
+				g2.draw(gp);
+			}
+
+			// polygons (e.g. in GeoLists) that don't have labeled segments
+			// should also draw their border
+			else if (!poly.wasInitLabelsCalled() && poly.lineThickness > 0) {
+				g2.setPaint(poly.getObjectColor());
+				g2.setStroke(objStroke);
+				g2.draw(gp);
+			}
+
+			if (labelVisible) {
 				g2.setPaint(poly.getLabelColor());
 				g2.setFont(view.fontPoint);
 				drawLabel(g2);
-            }			
-        }
-    }
-    
+			}
+		}
+	}
+
 	final public void updatePreview() {
 		int size = points.size();
 		isVisible = size > 0;
-		
-		if (isVisible) { 		
+
+		if (isVisible) {
 			GeoPointND[] pointsArray = new GeoPointND[size];
-			for (int i=0; i < size; i++) {
+			for (int i = 0; i < size; i++) {
 				pointsArray[i] = (GeoPointND) points.get(i);
 			}
-			addPointsToPath(pointsArray);								              
-		}	
+			addPointsToPath(pointsArray);
+		}
 	}
-	
+
 	private Point2D.Double endPoint = new Point2D.Double();
 
-	final public void updateMousePos(double xRW, double yRW) {	
-		if (isVisible) { 											
-			//double xRW = view.toRealWorldCoordX(mx);
-			//double yRW = view.toRealWorldCoordY(my);
-			
+	final public void updateMousePos(double xRW, double yRW) {
+		if (isVisible) {
+			// double xRW = view.toRealWorldCoordX(mx);
+			// double yRW = view.toRealWorldCoordY(my);
+			// Application.debug(xRW+" "+yRW);
+
 			int mx = view.toScreenCoordX(xRW);
 			int my = view.toScreenCoordY(yRW);
-			
+
 			// round angle to nearest 15 degrees if alt pressed
 			if (view.getEuclidianController().altDown) {
-				GeoPoint p = (GeoPoint)points.get(points.size() - 1);
+
+				GeoPoint p = (GeoPoint) points.get(points.size() - 1);
 				double px = p.inhomX;
 				double py = p.inhomY;
-				double angle = Math.atan2(yRW - py, xRW - px) * 180 / Math.PI;
-				double radius = Math.sqrt((py - yRW) * (py - yRW) + (px - xRW) * (px - xRW));
-				
-				// round angle to nearest 15 degrees
-				angle = Math.round(angle / 15) * 15; 
-				
-				xRW = px + radius * Math.cos(angle * Math.PI / 180);
-				yRW = py + radius * Math.sin(angle * Math.PI / 180);
-				
+
+				if (points.size() > 1) {
+					Construction cons = view.getKernel().getConstruction();
+					GeoPoint intersection = new GeoPoint(cons);
+					GeoLine l = new GeoLine(cons);
+					GeoLine l2 = new GeoLine(cons);
+					GeoPoint p2 = (GeoPoint) points.get(0);
+					double px2 = p2.inhomX;
+					double py2 = p2.inhomY;
+					double nearestX = Double.MAX_VALUE;
+					double nearestY = Double.MAX_VALUE;
+					double dist = Double.MAX_VALUE;
+					for (double angle = 0; angle < 180; angle += 15) {
+
+						if (angle == 90) {
+							l.setCoords(1, 0, -px);
+						} else {
+							double gradient = Math.tan(angle * Math.PI / 180.0);
+							l.setCoords(gradient, -1.0, py - gradient * px);
+						}
+
+						for (double ang2 = 0; ang2 < 180; ang2 += 15) {
+							if (angle == 90) {
+								l2.setCoords(1.0, 0, -px2);
+							} else {
+								double gradient2 = Math.tan(ang2 * Math.PI / 180.0);
+								l2.setCoords(gradient2, -1.0, py2 - gradient2 * px2);
+							}
+
+							// calculate intersection
+							GeoVec3D.cross(l, l2, intersection);
+
+							double x1 = intersection.x / intersection.z;
+							double y1 = intersection.y / intersection.z;
+
+							double d = GeoVec2D.length(x1 - xRW, y1 - yRW);
+							// Application.debug(d+" "+x1+","+y1+" "+xRW+","+yRW);
+							// Application.debug(x1+","+y1);
+							if (d < dist) {
+								nearestX = x1;
+								nearestY = y1;
+								dist = d;
+							}
+
+						}
+					}
+
+					xRW = nearestX;
+					yRW = nearestY;
+				} else {
+					double angle = Math.atan2(yRW - py, xRW - px) * 180 / Math.PI;
+					double radius = Math.sqrt((py - yRW) * (py - yRW) + (px - xRW)
+							* (px - xRW));
+
+					// round angle to nearest 15 degrees
+					angle = Math.round(angle / 15) * 15;
+
+					xRW = px + radius * Math.cos(angle * Math.PI / 180);
+					yRW = py + radius * Math.sin(angle * Math.PI / 180);
+				}
+
 				mx = view.toScreenCoordX(xRW);
 				my = view.toScreenCoordY(yRW);
-				
+
 				endPoint.x = xRW;
 				endPoint.y = yRW;
 				view.getEuclidianController().setLineEndPoint(endPoint);
 				gp.lineTo(mx, my);
-			}
-			else
+			} else
 				view.getEuclidianController().setLineEndPoint(null);
-				gp.lineTo(view.toScreenCoordX(xRW), view.toScreenCoordY(yRW));                                   			                                            
+			gp.lineTo(view.toScreenCoordX(xRW), view.toScreenCoordY(yRW));
 		}
 	}
-    
+
 	final public void drawPreview(Graphics2D g2) {
-    	if (isVisible) {
-			g2.setPaint(ConstructionDefaults.colPreviewFill);                       
-			g2.fill(gp);  			
-		  			            						
-			g2.setPaint(ConstructionDefaults.colPreview);             
-			g2.setStroke(objStroke);            
+		if (isVisible) {
+			g2.setPaint(ConstructionDefaults.colPreviewFill);
+			g2.fill(gp);
+
+			g2.setPaint(ConstructionDefaults.colPreview);
+			g2.setStroke(objStroke);
 			g2.draw(gp);
-    	}		            	
-    }
-	
-	public void disposePreview() {	
-	}
-    
-	final public boolean hit(int x,int y) {		
-       return gp != null && (gp.contains(x, y) || gp.intersects(x-3, y-3, 6, 6));        
-    }
-	
-    final public boolean isInside(Rectangle rect) {
-    	return gp != null && rect.contains(gp.getBounds());  
-    }
-    
-    public GeoElement getGeoElement() {
-        return geo;
-    }    
-    
-    public void setGeoElement(GeoElement geo) {
-        this.geo = geo;
-    }
-    
-    /**
-	 * Returns the bounding box of this Drawable in screen coordinates.	 
-	 */
-	final public Rectangle getBounds() {		
-		if (!geo.isDefined() || !geo.isEuclidianVisible())
-			return null;
-		else 
-			return gp.getBounds();	
+		}
 	}
 
-    
+	public void disposePreview() {
+	}
+
+	final public boolean hit(int x, int y) {
+		return gp != null
+				&& (gp.contains(x, y) || gp.intersects(x - 3, y - 3, 6, 6));
+	}
+
+	final public boolean isInside(Rectangle rect) {
+		return gp != null && rect.contains(gp.getBounds());
+	}
+
+	public GeoElement getGeoElement() {
+		return geo;
+	}
+
+	public void setGeoElement(GeoElement geo) {
+		this.geo = geo;
+	}
+
+	/**
+	 * Returns the bounding box of this Drawable in screen coordinates.
+	 */
+	final public Rectangle getBounds() {
+		if (!geo.isDefined() || !geo.isEuclidianVisible())
+			return null;
+		else
+			return gp.getBounds();
+	}
+
 }
