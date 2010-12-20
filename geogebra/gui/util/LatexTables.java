e08603616696742c9cb46f68064a5e2c6e8ba205
george <george@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-12-20 07:56:37 +0000

    playing with LaTeX symbol table button
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@7836 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ geogebra/gui/util/LatexTables.java
@@ -0,0 +1,45 @@
+package geogebra.gui.util;
+
+public class LatexTables {
+	public final static String [] miscSymbols = {
+		
+		"\\#",
+		"\\&",
+		"\\angle",
+		"\\backprime",
+		"\\bigstar",
+		"\\lozenge",
+		"\\blacklozenge",
+		"\\measuredangle",
+		"\\nabla",
+		"\\neg",
+		"\\nexists",
+		"\\prime",
+		"\\sphericalangle",
+		"\\surd",
+		"\\varnothing",
+		"\\emptyset",
+		"\\exists",
+		"\\forall",
+		"\\infty",
+		"\\square",
+		"\\blacksquare",
+		"\\triangle",
+		"\\triangledown",
+		"\\blacktriangle",
+		"\\blacktriangledown",
+		"\\top",
+		"\\bot",
+		"\\spadesuit",
+		"\\clubsuit",
+		"\\diamondsuit",
+		"\\heartsuit",
+		"\\flat",
+		"\\natural",
+		"\\sharp",
+		"\\diagdown",
+		"\\diagup"
+	};
+	
+	
+}
