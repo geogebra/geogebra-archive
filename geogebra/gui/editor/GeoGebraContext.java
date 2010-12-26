0f38507a5c60af4b8e4ec2d20dca49e0981b7678
calixte <calixte@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-12-26 23:08:29 +0000

    Add a tooltip to give the command's synopsis
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@7883 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/editor/GeoGebraContext.java
+++ geogebra/gui/editor/GeoGebraContext.java
@@ -9,185 +9,191 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by 
 the Free Software Foundation.
 
-*/
+ */
 
 package geogebra.gui.editor;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.awt.Color;
-import java.awt.Font;
-
-import javax.swing.text.ViewFactory;
-import javax.swing.text.View;
-import javax.swing.text.Element;
-
 import geogebra.main.Application;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.text.Element;
+import javax.swing.text.View;
 
 /**
  * 
  * @author Calixte DENIZET
- *
+ * 
  */
 public class GeoGebraContext extends ViewContext {
-    
-    /**
-     * TOKENS : A Map which contains the names of keywords
-     */
-    public static final Map<String, Integer> TOKENS = new HashMap(14);
-    
-    static {
-        TOKENS.put("Default", GeoGebraLexerConstants.DEFAULT);
-        TOKENS.put("Operator", GeoGebraLexerConstants.OPERATOR);
-        TOKENS.put("Constante", GeoGebraLexerConstants.CONSTANTE);
-        TOKENS.put("Number", GeoGebraLexerConstants.NUMBER);
-        TOKENS.put("OpenClose", GeoGebraLexerConstants.OPENCLOSE);
-        TOKENS.put("String", GeoGebraLexerConstants.STRING);
-        TOKENS.put("Built-in function", GeoGebraLexerConstants.BUILTINFUNCTION);
-        TOKENS.put("Function", GeoGebraLexerConstants.FUNCTION);
-        TOKENS.put("Command", GeoGebraLexerConstants.COMMAND);
-        TOKENS.put("Unknown", GeoGebraLexerConstants.UNKNOWN);
-        TOKENS.put("Variable", GeoGebraLexerConstants.VARIABLE);
-        TOKENS.put("White", GeoGebraLexerConstants.WHITE);
-        TOKENS.put("Tabulation", GeoGebraLexerConstants.TAB);        
-    }
 
-    private View view;
-    private Application app;
-    private List<Integer> typeToDefault = new ArrayList<Integer>();
+	/**
+	 * TOKENS : A Map which contains the names of keywords
+	 */
+	public static final Map<String, Integer> TOKENS = new HashMap(14);
 
-    private static final Map<String, Color> colorMap = new HashMap<String, Color>();
-    static {
-    	colorMap.put("Default", Color.decode("#000000"));
-        colorMap.put("Operator", Color.decode("#01a801"));
-        colorMap.put("Constante", Color.decode("#da70d6"));
-        colorMap.put("Number", Color.decode("#9f06ef"));
-        colorMap.put("OpenClose", Color.decode("#4a55db"));
-        colorMap.put("String", Color.decode("#bc8f8f"));
-        colorMap.put("Built-in function", Color.decode("#32b9b9"));
-        colorMap.put("Function", Color.decode("#ffb9b9"));
-        colorMap.put("Unknown", Color.decode("#ff5409"));
-        colorMap.put("Command", Color.decode("#ae5cb0"));
-        colorMap.put("Variable", Color.decode("#834310"));
-        colorMap.put("White", Color.decode("#dcdcdc"));
-        colorMap.put("Tabulation", Color.decode("#dcdcdc"));
-    }
+	static {
+		TOKENS.put("Default", GeoGebraLexerConstants.DEFAULT);
+		TOKENS.put("Operator", GeoGebraLexerConstants.OPERATOR);
+		TOKENS.put("Constante", GeoGebraLexerConstants.CONSTANTE);
+		TOKENS.put("Number", GeoGebraLexerConstants.NUMBER);
+		TOKENS.put("OpenClose", GeoGebraLexerConstants.OPENCLOSE);
+		TOKENS.put("String", GeoGebraLexerConstants.STRING);
+		TOKENS.put("Built-in function", GeoGebraLexerConstants.BUILTINFUNCTION);
+		TOKENS.put("Function", GeoGebraLexerConstants.FUNCTION);
+		TOKENS.put("Command", GeoGebraLexerConstants.COMMAND);
+		TOKENS.put("Unknown", GeoGebraLexerConstants.UNKNOWN);
+		TOKENS.put("Variable", GeoGebraLexerConstants.VARIABLE);
+		TOKENS.put("White", GeoGebraLexerConstants.WHITE);
+		TOKENS.put("Tabulation", GeoGebraLexerConstants.TAB);
+	}
 
-    private static final Map<String, Integer> attribMap = new HashMap<String, Integer>();
-    static {
-    	attribMap.put("Default", 0);
-        attribMap.put("Operator", 0);
-        attribMap.put("Constante", 0);
-        attribMap.put("Number", 0);
-        attribMap.put("OpenClose", 0);
-        attribMap.put("String", 0);
-        attribMap.put("Built-in function", 1);
-        attribMap.put("Function", 1);
-        attribMap.put("Command", 1);
-        attribMap.put("Unknown", 0);
-        attribMap.put("Variable", 0);
-        attribMap.put("White", 0);
-        attribMap.put("Tabulation", 0);
-    }
-    
-    /**
-     * The constructor
-     * @param app the Application where this context is needed
-     */
-    public GeoGebraContext(Application app) {
-        super();
-        this.app = app;
-        tokenFont = app.getPlainFont();
-        genColors();
-        genAttributes();
-    }
+	private View view;
+	private Application app;
+	private List<Integer> typeToDefault = new ArrayList<Integer>();
 
-    /**
-     * Generate an attribute for a type of keyword
-     * @param keyword the name can be found in scinotesConfiguration.xml
-     * @param type the type to use
-     */
-    public void genAttribute(String keyword, int type) {
-        tokenAttrib[TOKENS.get(keyword)] = type;
-        if (TOKENS.get(keyword) == GeoGebraLexerConstants.DEFAULT) {
-            for (Integer i : typeToDefault) {
-                tokenAttrib[i] = tokenAttrib[0];
-            }
-        }
-    }
+	private static final Map<String, Color> colorMap = new HashMap<String, Color>();
+	static {
+		colorMap.put("Default", Color.decode("#000000"));
+		colorMap.put("Operator", Color.decode("#01a801"));
+		colorMap.put("Constante", Color.decode("#da70d6"));
+		colorMap.put("Number", Color.decode("#9f06ef"));
+		colorMap.put("OpenClose", Color.decode("#4a55db"));
+		colorMap.put("String", Color.decode("#bc8f8f"));
+		colorMap.put("Built-in function", Color.decode("#32b9b9"));
+		colorMap.put("Function", Color.decode("#ffb9b9"));
+		colorMap.put("Unknown", Color.decode("#ff5409"));
+		colorMap.put("Command", Color.decode("#ae5cb0"));
+		colorMap.put("Variable", Color.decode("#834310"));
+		colorMap.put("White", Color.decode("#dcdcdc"));
+		colorMap.put("Tabulation", Color.decode("#dcdcdc"));
+	}
 
-    /**
-     * Generate attributes to use to render the document
-     */
-    public void genAttributes() {
-        tokenAttrib = new int[GeoGebraLexerConstants.NUMBEROFTOKENS];
-        Map<String, Integer> map = attribMap;
-        Iterator<String> it = map.keySet().iterator();
-        while (it.hasNext()) {
-            String tokenType = it.next();
-            tokenAttrib[TOKENS.get(tokenType)] = map.get(tokenType).intValue();
-        }
+	private static final Map<String, Integer> attribMap = new HashMap<String, Integer>();
+	static {
+		attribMap.put("Default", 0);
+		attribMap.put("Operator", 0);
+		attribMap.put("Constante", 0);
+		attribMap.put("Number", 0);
+		attribMap.put("OpenClose", 0);
+		attribMap.put("String", 0);
+		attribMap.put("Built-in function", 0);
+		attribMap.put("Function", 0);
+		attribMap.put("Command", 0);
+		attribMap.put("Unknown", 0);
+		attribMap.put("Variable", 0);
+		attribMap.put("White", 0);
+		attribMap.put("Tabulation", 0);
+	}
 
-        for (Integer i : typeToDefault) {
-            tokenAttrib[i] = tokenAttrib[0];
-        }
-    }
+	/**
+	 * The constructor
+	 * 
+	 * @param app
+	 *            the Application where this context is needed
+	 */
+	public GeoGebraContext(Application app) {
+		super();
+		this.app = app;
+		tokenFont = app.getPlainFont();
+		genColors();
+		genAttributes();
+	}
 
-    /**
-     * Generate the colors to use to render the document
-     */
-    public void genColors() {
-        tokenColors = new Color[GeoGebraLexerConstants.NUMBEROFTOKENS];
-        Map<String, Color> map = colorMap;
-        Iterator<String> it = map.keySet().iterator();
-        while (it.hasNext()) {
-            String tokenType = it.next();
-            tokenColors[TOKENS.get(tokenType)] = map.get(tokenType);
-        }
+	/**
+	 * Generate an attribute for a type of keyword
+	 * 
+	 * @param keyword
+	 *            the name can be found in scinotesConfiguration.xml
+	 * @param type
+	 *            the type to use
+	 */
+	public void genAttribute(String keyword, int type) {
+		tokenAttrib[TOKENS.get(keyword)] = type;
+		if (TOKENS.get(keyword) == GeoGebraLexerConstants.DEFAULT) {
+			for (Integer i : typeToDefault) {
+				tokenAttrib[i] = tokenAttrib[0];
+			}
+		}
+	}
 
-        typeToDefault.clear();
-        for (int i = 0; i < tokenColors.length; i++) {
-            if (tokenColors[i] == null) {
-                tokenColors[i] = tokenColors[0];
-                typeToDefault.add(i);
-            }
-        }
-    }
+	/**
+	 * Generate attributes to use to render the document
+	 */
+	public void genAttributes() {
+		tokenAttrib = new int[GeoGebraLexerConstants.NUMBEROFTOKENS];
+		Map<String, Integer> map = attribMap;
+		Iterator<String> it = map.keySet().iterator();
+		while (it.hasNext()) {
+			String tokenType = it.next();
+			tokenAttrib[TOKENS.get(tokenType)] = map.get(tokenType).intValue();
+		}
 
-    /**
-     * Generate a color for a type of keyword
-     * @param name the name can be found in scinotesConfiguration.xml
-     * @param color the color to use
-     */
-    public void genColors(String name, Color color) {
-        if (tokenColors == null) {
-            genColors();
-        }
+		for (Integer i : typeToDefault) {
+			tokenAttrib[i] = tokenAttrib[0];
+		}
+	}
 
-        tokenColors[TOKENS.get(name)] = color;
+	/**
+	 * Generate the colors to use to render the document
+	 */
+	public void genColors() {
+		tokenColors = new Color[GeoGebraLexerConstants.NUMBEROFTOKENS];
+		Map<String, Color> map = colorMap;
+		Iterator<String> it = map.keySet().iterator();
+		while (it.hasNext()) {
+			String tokenType = it.next();
+			tokenColors[TOKENS.get(tokenType)] = map.get(tokenType);
+		}
 
-        if (TOKENS.get(name) == GeoGebraLexerConstants.DEFAULT) {
-            for (Integer i : typeToDefault) {
-                tokenColors[i] = tokenColors[0];
-            }
-        }
-    }
+		typeToDefault.clear();
+		for (int i = 0; i < tokenColors.length; i++) {
+			if (tokenColors[i] == null) {
+				tokenColors[i] = tokenColors[0];
+				typeToDefault.add(i);
+			}
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public View getCurrentView() {
-        return view;
-    }
+	/**
+	 * Generate a color for a type of keyword
+	 * 
+	 * @param name
+	 *            the name can be found in scinotesConfiguration.xml
+	 * @param color
+	 *            the color to use
+	 */
+	public void genColors(String name, Color color) {
+		if (tokenColors == null) {
+			genColors();
+		}
 
-    /**
-     * {@inheritDoc}
-     */
-    public View create(Element elem) {
-    	view = new GeoGebraView(elem, new GeoGebraLexer(app), this);
-        return view;
-    }	
+		tokenColors[TOKENS.get(name)] = color;
+
+		if (TOKENS.get(name) == GeoGebraLexerConstants.DEFAULT) {
+			for (Integer i : typeToDefault) {
+				tokenColors[i] = tokenColors[0];
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public View getCurrentView() {
+		return view;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public View create(Element elem) {
+		view = new GeoGebraView(elem, new GeoGebraLexer(app), this);
+		return view;
+	}
 }
