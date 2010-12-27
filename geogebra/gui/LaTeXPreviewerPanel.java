cd93299c841512b3905b9e822938dea204c007f4
calixte <calixte@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-12-27 19:29:02 +0000

    Problem when the string is just "$".
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@7900 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/LaTeXPreviewerPanel.java
+++ geogebra/gui/LaTeXPreviewerPanel.java
@@ -8,7 +8,7 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by 
 the Free Software Foundation.
 
-*/
+ */
 package geogebra.gui;
 
 import geogebra.kernel.GeoText;
@@ -32,75 +32,79 @@ import org.scilab.forge.jlatexmath.TeXFormula;
  */
 public class LaTeXPreviewerPanel extends JPanel {
 	private static final int INSET = 3;
-    private static final Rectangle NULLRECT = new Rectangle(0, 0, 0, 0);
+	private static final Rectangle NULLRECT = new Rectangle(0, 0, 0, 0);
 
-    private static final int defaultSize = 15;
-    
-    private Icon icon;
-    private int width;
-    private int height;
-    
-    /**
-     * Default constructor
-     */
-    public LaTeXPreviewerPanel() {
-        // I disable the double-buffering, it's useless here
-        super(false);
-    }
+	private static final int defaultSize = 15;
 
-    public void setLaTeX(Application app, String str) {
-    	
-		if (str.indexOf('"') > -1) {	
-			
+	private Icon icon;
+	private int width;
+	private int height;
+
+	/**
+	 * Default constructor
+	 */
+	public LaTeXPreviewerPanel() {
+		// I disable the double-buffering, it's useless here
+		super(false);
+	}
+
+	public void setLaTeX(Application app, String str) {
+
+		if (str.indexOf('"') > -1) {
+
 			boolean errorsActive = app.isErrorDialogsActive();
 			app.setErrorDialogsActive(false);
-			GeoText text = app.getKernel().getAlgebraProcessor().evaluateToText(str, false);
+			GeoText text = app.getKernel().getAlgebraProcessor()
+					.evaluateToText(str, false);
 			app.setErrorDialogsActive(errorsActive);
-			
+
 			if (text != null) {
 				text.setLaTeX(true, false);
 				str = text.getTextString();
 			} else {
 				// bad syntax, remove all quotes and use raw string
-				while (str.indexOf('"') > -1) str = str.replace('"', ' ');
-				//latexPreview.setLaTeX(str);
+				while (str.indexOf('"') > -1)
+					str = str.replace('"', ' ');
+				// latexPreview.setLaTeX(str);
 			}
 		} else {
-			//latexPreview.setLaTeX(str);
+			// latexPreview.setLaTeX(str);
 
 		}
-    	
-    	String f = str.trim();
-    	if (f.startsWith("$") && f.endsWith("$")) {
-            f = f.substring(1, f.length() - 1);
-        }
 
-        icon = TeXFormula.getPartialTeXFormula(f).createTeXIcon(TeXConstants.STYLE_DISPLAY, defaultSize);
-        if (icon == null) {
-            icon = TeXFormula.getPartialTeXFormula("").createTeXIcon(TeXConstants.STYLE_DISPLAY, defaultSize);
-        }
+		String f = str.trim();
+		if (f.length() >= 2 && f.startsWith("$") && f.endsWith("$")) {
+			f = f.substring(1, f.length() - 1);
+		}
 
-        width = icon.getIconWidth();
-        height = icon.getIconHeight();
-        Dimension dim = new Dimension(width + 2 * INSET, height + 2 * INSET);
-        setPreferredSize(dim);
-        setSize(dim);
-        setLocation(0, 0);
-        setVisible(true);
-        repaint();   
-    }
+		icon = TeXFormula.getPartialTeXFormula(f).createTeXIcon(
+				TeXConstants.STYLE_DISPLAY, defaultSize);
+		if (icon == null) {
+			icon = TeXFormula.getPartialTeXFormula("").createTeXIcon(
+					TeXConstants.STYLE_DISPLAY, defaultSize);
+		}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void paintComponent(Graphics g) {
-    	super.paintComponent(g);
-        if (icon != null) {
-	        g.setColor(Color.WHITE);
-	        g.fillRect(0, 0, width + 2 * INSET, height + 2 * INSET);
-	      //  g.setColor(Color.BLACK);
-	      //  g.drawRect(0, 0, width + 2 * INSET - 1, height + 2 * INSET - 1);
-        	icon.paintIcon(this, g, INSET, INSET);
-        }
-    }
+		width = icon.getIconWidth();
+		height = icon.getIconHeight();
+		Dimension dim = new Dimension(width + 2 * INSET, height + 2 * INSET);
+		setPreferredSize(dim);
+		setSize(dim);
+		setLocation(0, 0);
+		setVisible(true);
+		repaint();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if (icon != null) {
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, width + 2 * INSET, height + 2 * INSET);
+			// g.setColor(Color.BLACK);
+			// g.drawRect(0, 0, width + 2 * INSET - 1, height + 2 * INSET - 1);
+			icon.paintIcon(this, g, INSET, INSET);
+		}
+	}
 }
