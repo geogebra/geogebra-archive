e5872578fbb2d73c7b46a74f8230e1f27d186f18
andre <andre@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-01-01 19:25:42 +0000

    Switched back to the old curve rendering method. DrawCurve3D.useOldCurves controls which method is used.
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@7995 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra3D/euclidian3D/DrawCurve3D.java
+++ geogebra3D/euclidian3D/DrawCurve3D.java
@@ -1,10 +1,10 @@
 package geogebra3D.euclidian3D;
 
 import geogebra.Matrix.GgbVector;
-import geogebra.main.Application;
 import geogebra3D.euclidian3D.opengl.PlotterBrush;
 import geogebra3D.euclidian3D.opengl.Renderer;
 import geogebra3D.euclidian3D.plots.CurveMesh;
+import geogebra3D.euclidian3D.plots.CurveTree;
 import geogebra3D.kernel3D.GeoCurveCartesian3D;
 
 /**
@@ -14,7 +14,10 @@ import geogebra3D.kernel3D.GeoCurveCartesian3D;
  *
  */
 public class DrawCurve3D extends Drawable3DCurves {
+	private final boolean useOldCurves = true;
+
 	private CurveMesh mesh;
+	private CurveTree tree;
 	
 	
 	/** handle to the curve */
@@ -33,9 +36,12 @@ public class DrawCurve3D extends Drawable3DCurves {
 	public DrawCurve3D(EuclidianView3D a_view3d, GeoCurveCartesian3D curve) {
 		super(a_view3d,curve);
 		this.curve=curve;
-		double rad = currentRadius();
-		savedRadius=rad;
-		mesh = new CurveMesh(curve, rad);
+		if(useOldCurves)
+			tree = new CurveTree(curve, a_view3d);
+		else {
+			updateRadius();
+			mesh = new CurveMesh(curve, savedRadius);
+		}
 	}
 	
 
@@ -61,6 +67,37 @@ public class DrawCurve3D extends Drawable3DCurves {
 	}
 	
 	/** gets the viewing radius based on the viewing frustum 
+	 */
+	private void updateRadius() {
+		EuclidianView3D view = getView3D();
+		Renderer temp = view.getRenderer();
+		double x1 = temp.getLeft();
+		double x2 = temp.getRight();
+		double y1 = temp.getTop();
+		double y2 = temp.getBottom();
+		double z1 = temp.getFront(true);
+		double z2 = temp.getBack(true);
+		GgbVector [] v = new GgbVector[8];
+		v[0] = new GgbVector(x1,y1,z1);
+		v[1] = new GgbVector(x1,y2,z1);
+		v[2] = new GgbVector(x1,y1,z2);
+		v[3] = new GgbVector(x1,y2,z2);
+		v[4] = new GgbVector(x2,y1,z1);
+		v[5] = new GgbVector(x2,y2,z1);
+		v[6] = new GgbVector(x2,y1,z2);
+		v[7] = new GgbVector(x2,y2,z2);
+
+		savedRadius=0;
+		double norm;
+		for(int i = 0; i < 8; i++){
+			view.toSceneCoords3D(v[i]);
+			norm = v[i].norm();
+			if(norm>savedRadius)
+				savedRadius=norm;
+		}
+	}
+	
+		/** gets the viewing radius based on the viewing frustum 
 	 */
 	private double currentRadius() {
 		EuclidianView3D view = getView3D();
@@ -93,21 +130,45 @@ public class DrawCurve3D extends Drawable3DCurves {
 	
 	@Override
 	protected void realtimeUpdate(){
+		if(!useOldCurves){		
+			Renderer renderer = getView3D().getRenderer();
+			mesh.setRadius(savedRadius);
+			mesh.optimize();
 		
-		Renderer renderer = getView3D().getRenderer();
-		mesh.setRadius(savedRadius);
-		mesh.optimize();
-		
-		PlotterBrush brush = renderer.getGeometryManager().getBrush();
-		brush.start(8);
-		brush.draw(mesh,savedRadius);
+			PlotterBrush brush = renderer.getGeometryManager().getBrush();
+			brush.start(8);
+			brush.draw(mesh,savedRadius);
 
-		setGeometryIndex(brush.end());
-		
+			setGeometryIndex(brush.end());
+		}
 	}
 	
 	protected boolean updateForItSelf(){
+		if(useOldCurves){
+			Renderer renderer = getView3D().getRenderer();
+		
+			if (!curve.isEuclidianVisible() || !curve.isDefined()){
+				setGeometryIndex(-1);
+			
+			//} else if(needRedraw()){
+			}else{
+			
+				needRedraw();
+				tree = new CurveTree(curve, getView3D());
+			
+				PlotterBrush brush = renderer.getGeometryManager().getBrush();
 
+				brush.setThickness(getGeoElement().getLineThickness(),(float) getView3D().getScale());
+
+				brush.start(8);
+			
+				brush.draw(tree,savedRadius);
+
+				setGeometryIndex(brush.end());
+			}
+		} else {
+			updateRadius();
+		}
 		return true;
 	}
 	
