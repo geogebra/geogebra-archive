118b5cd5c434fe7bd05d6d1dfe48c2f715a25853
zoltan <zoltan@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-01-04 20:10:43 +0000

    Christian Schött's contribution for creating Linux portable versions
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@8046 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ scripts/autobuild/create_geogebra_portable.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This script creates the portable version for Linux (32bit & 64bit).
+# @author Christian Schött <schoett@gmx.de>
+
+# argument 1: version of GeoGebra (eg. 3.2.44.0)
+# argument 2: path of executable jre installation file (eg. jre-6u21-linux-x64.bin)
+# argument 3: path of directory containing unpacked geogebra files
+# argument 4: path of directory containing unsigned geogebra files
+# argument 5: path of geogebra icon (eg. geogebra.png)
+# argument 6: path of destination directory
+
+import os, shutil, sys, tarfile, tempfile
+if len(sys.argv) != 7:
+	print("Error: Six arguments are expected.")
+	sys.exit(1)
+if not os.path.exists(sys.argv[2]):
+	print("Error: "+sys.argv[2]+" does not exist.")
+	sys.exit(1)
+if not os.path.exists(sys.argv[3]):
+	print("Error: "+sys.argv[3]+" does not exist.")
+	sys.exit(1)
+if not os.path.exists(sys.argv[4]):
+	print("Error: "+sys.argv[4]+" does not exist.")
+	sys.exit(1)
+if not os.path.exists(sys.argv[5]):
+	print("Error: "+sys.argv[5]+" does not exist.")
+	sys.exit(1)
+geogebra_version = sys.argv[1].replace(".", "-")
+java_path = os.path.abspath(sys.argv[2])
+unpacked_path = os.path.abspath(sys.argv[3])
+unsigned_path = os.path.abspath(sys.argv[4])
+icon_path = os.path.abspath(sys.argv[5])
+destination_path = os.path.abspath(sys.argv[6])
+if not os.path.isfile(java_path):
+	print("Error: "+java_path+" is not a file.")
+	sys.exit(1)
+if not os.access(java_path,os.X_OK):
+	print("Error: "+java_path+" is not executable.")
+	sys.exit(1)
+if not os.path.isdir(unpacked_path):
+	print("Error: "+unpacked_path+" is not a directory.")
+	sys.exit(1)
+if not os.path.isdir(unsigned_path):
+	print("Error: "+unsigned_path+" is not a directory.")
+	sys.exit(1)
+if not os.path.isfile(icon_path):
+	print("Error: "+icon_path+" is not a file.")
+	sys.exit(1)
+if os.path.splitext(icon_path)[1] != ".png":
+	print("Error: "+icon_path+" has not the ending \".png\".")
+	sys.exit(1)
+if not os.access(icon_path,os.R_OK):
+	print("Error: "+icon_path+" is not readable.")
+	sys.exit(1)
+if not os.path.isdir(destination_path):
+	print("Error: "+destination_path+" is not a directory.")
+	sys.exit(1)
+java_filename = os.path.basename(java_path)
+icon_filename = os.path.basename(icon_path)
+arch = ""
+if "linux-x64" in java_filename:
+	arch = "64bit-"
+elif not "linux-i586" in java_filename:
+	print("Error: Architecture can not be determined.")
+	sys.exit(1)
+temp_dir = tempfile.mkdtemp()
+try:
+	shutil.copytree(unpacked_path, temp_dir+"/geogebra")
+	os.chdir(temp_dir+"/geogebra")
+	shutil.copytree(unsigned_path, "unsigned")
+	shutil.copy(icon_path, ".")
+	if not icon_filename == "geogebra.png":
+		os.rename(icon_filename, "geogebra.png")
+	os.chmod("geogebra.png",0o444)
+	with open("geogebra.sh", "w") as sh_file:
+		sh_file_lines = ["#!/bin/sh\n", "jre/bin/java -jar -Xms32m -Xmx512m geogebra.jar \"$@\"\n"]
+		sh_file.writelines(sh_file_lines)
+	os.chmod("geogebra.sh",0o755)
+	os.system("sh "+java_path)
+	content = os.listdir(os.getcwd())
+	jre_dir = ""
+	for name in content:
+		if "jre" in name:
+			jre_dir = name
+			break
+	if jre_dir == "":
+		print("Error: "+java_filename+" does not create expected folder.")
+		sys.exit(1)
+	os.rename(jre_dir, "jre")
+	os.chdir("../")
+	tar_gz_file = tarfile.open(destination_path+"/GeoGebra-Linux-"+arch+"Portable-"+geogebra_version+".tar.gz", "w:gz")
+	try:
+		tar_gz_file.add("geogebra")
+	finally:
+		tar_gz_file.close()
+finally:
+	shutil.rmtree(temp_dir)
+sys.exit(0)
