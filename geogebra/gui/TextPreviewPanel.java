3187628f6fd97034db45df0fc980f638cbc2250b
george <george@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-01-09 22:45:30 +0000

    improved text preview error handling
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@8116 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/TextPreviewPanel.java
+++ geogebra/gui/TextPreviewPanel.java
@@ -4,107 +4,84 @@ import geogebra.euclidian.EuclidianController;
 import geogebra.euclidian.EuclidianView;
 import geogebra.kernel.AlgoDependentText;
 import geogebra.kernel.Construction;
+import geogebra.kernel.GeoElement;
 import geogebra.kernel.GeoText;
 import geogebra.kernel.Kernel;
 import geogebra.kernel.arithmetic.ExpressionNode;
-import geogebra.kernel.parser.TokenMgrError;
 
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Font;
 
 /**
  * 
- * Extension of EuclidianView that can be used to preview GeoText while editing. 
+ * Extension of EuclidianView that can be used to preview GeoText while editing.
+ * 
+ * The class maintains two hidden geos (previewGeoIndependent and
+ * previewGeoDependent) that are used to preview the two possible types of
+ * GeoText, independent and dependent.
+ * 
+ * TODO: adjust the size of the panel when the bounding box of the preview geo
+ * grows larger than the enclosing JScrollpane viewport. Currently the preview
+ * geo uses absolute screen coords, so we can't easily get the bounding box
+ * dimensions.
  * 
  * @author gsturr 2010-6-30
- *
+ * 
  */
 public class TextPreviewPanel extends EuclidianView {
 
 	private EuclidianController ec;
 	private static boolean[] showAxes = { false, false };
 	private static boolean showGrid = false;
-	private GeoText previewGeoText;
+	private GeoText previewGeoIndependent, previewGeoDependent;
 	private boolean enableUpdateSize = true; // needed ?
-	
-	private AlgoDependentText textAlgo;
-	Construction cons ;
-	//private boolean isIndependent;
 
-	
+	private AlgoDependentText textAlgo;
+	private Construction cons ;
+	private boolean isIndependent;
+
+
 	public TextPreviewPanel(Kernel kernel) {
-		
+
 		super(new EuclidianController(kernel), showAxes, showGrid);
 		this.ec = this.getEuclidianController();
 
 		this.cons = kernel.getConstruction();
 
-		// set display properties 
+		// set EV display properties 
 		setAntialiasing(true);
 		setAllowShowMouseCoords(false);
 		setAxesCornerCoordsVisible(false);
 		updateFonts();
 		updateSize();
-		
-		// remove mouse listeners
+
+		// remove EV mouse listeners
 		removeMouseListener(ec);
 		removeMouseMotionListener(ec);
 		removeMouseWheelListener(ec);
-		
-	}
-
-	
-	/**
-	 * creates the preview GeoText, hides it from EV and puts it into
-	 * this view
-	 */
-	private void createPreviewGeo(){
-
-		// create and label the preview geo
-		previewGeoText = new GeoText(kernel.getConstruction());
-		previewGeoText.setLabel(null);
-
-		// add the geo to this view and remove it from EV	
-		previewGeoText.addView(this);
-		this.add(previewGeoText);
-		previewGeoText.removeView(app.getEuclidianView());
-		app.getEuclidianView().remove(previewGeoText);
-		previewGeoText.setEuclidianVisible(true);
-
-		// position the text using absolute screen coordinates
-		previewGeoText.setAbsoluteScreenLocActive(true);
-		locateTextGeo();
 
 	}
 
-	
+
+
+
 	/**
-	 * positions the preview geo in the upper left corner of the panel
-	 * two settings are needed to account for differences in the way
-	 * LaTeX and standard text is drawn
-	 */
-	private void locateTextGeo() {
-		int xInset = 4;
-		int yInset = previewGeoText.isLaTeX() ? 4 : 4 + app.getFontSize();
-		previewGeoText.setAbsoluteScreenLocActive(true);
-		previewGeoText.setAbsoluteScreenLoc( xInset, yInset);
-	}
-	
-	
-	/**
-	 * removes the preview geo
+	 * Removes the preview geos
 	 */
 	public void removePreviewGeoText() {
-		if(previewGeoText != null)
-			previewGeoText.remove();
+		if(previewGeoIndependent != null)
+			previewGeoIndependent.remove();
+		if(previewGeoDependent != null){
+			previewGeoDependent.remove();
+			textAlgo.remove();
+		}
 	}
 
-	
-	
-	
+
+
 	/**
-	 * updates the preview geo by redefining it from the string inputValue;
-	 * also copies visual style from targetGeo 
+	 * Updates the preview geos and creates new geos if needed.
+	 * Changes are determined by the inputValue string and the visual style of the targetGeo. 
 	 *  
 	 * @param targetGeo
 	 * @param inputValue
@@ -112,98 +89,186 @@ public class TextPreviewPanel extends EuclidianView {
 	 */
 	public void updatePreviewText(GeoText targetGeo, String inputValue, boolean isLaTeX) {
 
-		if(textAlgo != null)
-			textAlgo.remove();
-		
-		if(previewGeoText != null)
-			previewGeoText.remove();
+		// initialize variables
+		ExpressionNode exp = null;
+		String formattedInput = formatInputValue(inputValue);		
+		boolean hasParseError = false;
+		boolean showErrorMessage = false;
+		isIndependent = false;
 
-		String formattedInput = formatInputValue(inputValue);
-		
-
-		try {
-			
-			boolean independent = false;
-			
-			try
-			{
-				ExpressionNode exp = (ExpressionNode) kernel.getParser().parseGeoGebraExpression(formattedInput);
-				exp.resolveVariables();
-				
-				if (!exp.isConstant()) {
-					System.out.println("is not independent");
-					textAlgo = new AlgoDependentText(cons, exp);
-					cons.removeFromConstructionList(textAlgo);
-				} else {
-					independent = true;
-				}
-				
-				previewGeoText = textAlgo.getGeoText();
-			} catch (Exception e) {
-				independent = true;
-			} catch (Error e) {
-				independent = true;
-			}
-			
-			if (independent) {
-				// independent text
-				System.out.println("is independent");
-
-				
-				// reuse previewGeoText if we can
-				if (previewGeoText == null || !previewGeoText.isIndependent())
-					previewGeoText = new GeoText(kernel.getConstruction());
-				
-				previewGeoText.setTextString(formattedInput);
-			}
-			
-
-			// update the display style
-			if(targetGeo != null){
-				previewGeoText.setVisualStyle(targetGeo);
-			} else {
-				if (isLaTeX) previewGeoText.setSerifFont(true);
-				previewGeoText.setObjColor(Color.black);
-			}
-			previewGeoText.setLaTeX(isLaTeX, true);
-
-			locateTextGeo();
-			previewGeoText.setEuclidianVisible(true);
-			previewGeoText.addView(this);
-			add(previewGeoText);
-
-			
-		} catch (Throwable t) {
-			t.printStackTrace();
+		// create previewGeoIndependent 
+		if (previewGeoIndependent == null){
+			previewGeoIndependent = new GeoText(kernel.getConstruction());
+			previewGeoIndependent.addView(this);
+			add(previewGeoIndependent);
 		}
 
+
+		// parse the input text 
+		try{
+			exp = (ExpressionNode) kernel.getParser().parseGeoGebraExpression(formattedInput);
+		}
+
+		catch (Exception e) {
+			isIndependent = true;
+			hasParseError = true;  	
+			if(formattedInput.length() > 0) // no error message if we have an empty string
+				showErrorMessage = true;
+			//Application.debug("parse exception");
+		} 
+		catch (Error e) {
+			isIndependent = true;
+			hasParseError = true;  // odd numbers of quotes give parse errors 
+			showErrorMessage = true;
+			//Application.debug("parse error");
+		}
+
+
+
+		// resolve variables
+		if(!(hasParseError)){
+			try
+			{
+				exp.resolveVariables();
+				isIndependent = exp.isConstant();
+			} 
+			catch (Error e) {
+				// if an error is thrown here then we must have independent text
+				// this is not considered an error, so don't show an error message
+				isIndependent = true;
+				//Application.debug("resolve error");
+			} 
+			catch (Exception e) {
+				showErrorMessage = true;
+				isIndependent = true;
+				//Application.debug("resolve exception");
+			}
+		}
+
+
+		// create the previewGeoText
+
+		// case1: independent text based on string only, including error messages
+		if (isIndependent) 
+		{
+			//System.out.println("is independent");
+
+			// remove quote characters
+			formattedInput = formattedInput. replaceAll("\"", "");
+
+			// set the text
+			if(showErrorMessage)
+				previewGeoIndependent.setTextString(app.getError("InvalidInput"));
+			else
+				previewGeoIndependent.setTextString(formattedInput);
+
+			// update the display style
+			updateVisualProperties(previewGeoIndependent, targetGeo, isLaTeX, showErrorMessage);
+		} 
+
+
+		// case 2: dependent GeoText, needs AlgoDependentText	
+		else
+		{
+			//System.out.println("is not independent");
+
+			if(previewGeoDependent != null){
+				previewGeoDependent.remove();
+				textAlgo.remove();
+			}
+
+			// create new  previewGeoDependent 
+			textAlgo = new AlgoDependentText(cons, exp);
+			cons.removeFromConstructionList(textAlgo);
+			previewGeoDependent = textAlgo.getGeoText();
+			previewGeoDependent.addView(this);
+			add(previewGeoDependent);
+
+			// set the display style
+			updateVisualProperties(previewGeoDependent, targetGeo, isLaTeX, showErrorMessage);
+		}
+
+		// hide/show the preview geos  
+		previewGeoIndependent.setEuclidianVisible(isIndependent);
+		previewGeoIndependent.updateRepaint();
+		if(previewGeoDependent !=null){
+			previewGeoDependent.setEuclidianVisible(!isIndependent);
+			previewGeoDependent.updateRepaint();
+		}
+
+	}
+
+	/**
+	 * Sets the visual properties of a preview geo
+	 */
+	private void updateVisualProperties(GeoText geo, GeoText targetGeo, boolean isLaTeX, boolean isErrorMessage ){
+
+		// set error message style
+		if(isErrorMessage)
+		{
+			geo.setVisualStyle(cons.getConstructionDefaults().getDefaultGeo(GeoElement.GEO_CLASS_TEXT));
+			geo.setObjColor(Color.red);
+			geo.setBackgroundColor(Color.white);
+			//geo.setFontSize(app.getFontSize());
+			geo.setFontStyle(Font.ITALIC);
+			geo.setLaTeX(false, true);
+		}
+
+		// set text style
+		else
+		{	
+			if(targetGeo != null){
+				geo.setVisualStyle(targetGeo);
+			} else {
+				if (isLaTeX) geo.setSerifFont(true);
+				geo.setObjColor(Color.black);
+			}
+			geo.setLaTeX(isLaTeX, true);
+		}
+
+		// set geo position in upper left corner (it might need changing after isLaTeX change)
+		locateTextGeo(geo);
 
 	}
 
 
 
+	/**
+	 * Positions the preview geo in the upper left corner of the panel
+	 * two settings are needed to account for differences in the way
+	 * LaTeX and standard text is drawn
+	 */
+	private void locateTextGeo(GeoText geo) {
+		int xInset = 4;
+		int yInset = geo.isLaTeX() ? 4 : 4 + app.getFontSize();
+		geo.setAbsoluteScreenLocActive(true);
+		geo.setAbsoluteScreenLoc( xInset, yInset);
+	}
+
+
+	/**
+	 * Prepares the inputValue string for the parser
+	 * 
+	 *  TODO: not sure this is really needed, review the code
+	 */
 	private String formatInputValue(String inputValue){
-
-		//isIndependent = true;
-
-		// Fit quote characters to the inputValue string so that the algebra processor 
-		// can use it to redefine the geo.
 
 		// if inputValue is null then use the current definition
 		if(inputValue == null){
-			if(previewGeoText.isIndependent()){ 
-				inputValue = previewGeoText.getTextString();
-				if(previewGeoText.getKernel().lookupLabel(inputValue) != null)
+			System.out.println("=== null input === ");
+			if(previewGeoIndependent.isIndependent()){ 
+				inputValue = previewGeoIndependent.getTextString();
+				if(previewGeoIndependent.getKernel().lookupLabel(inputValue) != null)
 					inputValue = "\"" + inputValue + "\"";            		 		
 			}
 			else{
-				inputValue = previewGeoText.getCommandDescription(); 
-				//isIndependent = false;
+				inputValue = previewGeoIndependent.getCommandDescription(); 
 			}
 
-		// inputValue is not null (code copied from TextInputHandler ... inner class in TextInputDialog)		
+
+			// inputValue is not null (code copied from TextInputHandler ... inner class in TextInputDialog)		
 		} else {
-			System.out.println("process input: " + inputValue);
+			//System.out.println("process input: " + inputValue);
 			// no quotes?
 			if (inputValue.indexOf('"') < 0) {
 				// this should become either
@@ -235,148 +300,6 @@ public class TextPreviewPanel extends EuclidianView {
 
 		return inputValue;
 	}
-
-
-
-
-
-
-	//=======================================================
-	// old code
-
- 
-	public void updatePreviewTextOLD(GeoText targetGeo, String inputValue, boolean isLaTeX) {
-
-		// create a new preview geo if needed
-		if(previewGeoText == null)
-			createPreviewGeo();
-
-		// update the display style
-		if(targetGeo != null){
-			previewGeoText.setVisualStyle(targetGeo);
-		} else {
-			if (isLaTeX) previewGeoText.setSerifFont(true);
-		}
-
-		// Fit quote characters to the inputValue string so that the algebra processor 
-		// can use it to redefine the geo.
-		
-		// if inputValue is null then use the current definition
-		if(inputValue == null){
-			if(previewGeoText.isIndependent()){ 
-				inputValue = previewGeoText.getTextString();
-				if(previewGeoText.getKernel().lookupLabel(inputValue) != null)
-					inputValue = "\"" + inputValue + "\"";            		 		
-			}
-			else
-				inputValue = previewGeoText.getCommandDescription(); 
-
-		// inputValue is not null (code copied from TextInputHandler ... inner class in TextInputDialog)		
-		} else {
-
-			// no quotes?
-			if (inputValue.indexOf('"') < 0) {
-				// this should become either
-				// (1) a + "" where a is an object label or
-				// (2) "text", a plain text 
-
-				// ad (1) OBJECT LABEL 
-				// add empty string to end to make sure
-				// that this will become a text object
-				if (kernel.lookupLabel(inputValue.trim()) != null) {
-					inputValue = "(" + inputValue + ") + \"\"";
-				} 
-				// ad (2) PLAIN TEXT
-				// add quotes to string
-				else {
-					inputValue = "\"" + inputValue + "\"";
-				}        			
-			} 
-			else {
-				// replace \n\" by \"\n, this is useful for e.g.:
-				//    "a = " + a + 
-				//	"b = " + b 
-				inputValue = inputValue.replaceAll("\n\"", "\"\n");
-			}
-		}
-
-		
-		// redefine the preview geo
-		GeoText newText;
-		try {
-			newText = (GeoText) kernel.getAlgebraProcessor()
-			.changeGeoElementNoExceptionHandling(previewGeoText,
-					inputValue, true, false);
-
-			if(newText != null)
-				previewGeoText = newText;
-			
-			// make sure the correct LaTeX setting is used
-			previewGeoText.setLaTeX(isLaTeX, true);
-			
-			// put the geo in the correct location (depends on LaTeX setting) 
-			locateTextGeo();
-			previewGeoText.setObjColor(Color.black);
-			previewGeoText.updateRepaint();
-
-			// make sure the geo is displayed in this view and not EV
-			// this needs to be done here in case the geo was redefined as indepenedent
-			// TODO: why does this kind of redefinition change the view settings?
-			previewGeoText.addView(this);
-			this.add(previewGeoText);
-			previewGeoText.removeView(app.getEuclidianView());
-			app.getEuclidianView().remove(previewGeoText);
-
-		} catch (Exception e) {
-			// reset the preview geo to show an error message
-			previewGeoText.setTextString(app.getError("InvalidInput"));
-			previewGeoText.setObjColor(Color.red);
-			previewGeoText.updateRepaint();
-			//e.printStackTrace();
-		}                
-
-
-		
-		//TODO: adjust the size of the panel when the bounding box of the preview geo
-		// grows larger than the enclosing scrollpane viewport.
-		// Currently the preview geo uses absolute screen coords, so we can't get the 
-		// bounding box dimensions.
-
-
-		//this.setRealWorldCoordSystem(0, 1, -1, 0);
-		//updateSize();
-		//this.repaint();
-
-		enableUpdateSize = true;
-
-		Dimension d = this.getMinimumSize();
-
-		if(d.height < getBounds().height)
-			d.height = getBounds().height;
-		if(d.width < getBounds().width)
-			d.width = getBounds().width;
-
-		//setSize(d);
-		//updateSize();
-		//enableUpdateSize = false;
-
-		//System.out.println("bounding box: " + getBounds().toString());
-		//this.setPreferredSize(new Dimension((int)r.getWidth(), (int)r.getHeight()));
-
-	}
-
-	
-	public void updateSize() {
-
-		// override updateSize so we can control the size of the panel?
-		
-		//	Application.printStacktrace("update size");
-		//if(enableUpdateSize)
-		super.updateSize();
-	}
-
-	
-	
 
 
 }
