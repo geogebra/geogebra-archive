386e3b682e262a43d02371c737edcf5ed7cacb1e
hans-petter <hans-petter@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-02-09 22:22:18 +0000

    New commands: LeftSum, RectangleSum
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@8503 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CommandProcessor.java
+++ geogebra/kernel/commands/CommandProcessor.java
@@ -3931,6 +3931,94 @@ class CmdLowerSum extends CommandProcessor {
 }
 
 /**
+ * LeftSum[ <GeoFunction>, <Number>, <Number>, <Number> ]
+ */
+class CmdLeftSum extends CommandProcessor {
+
+	/**
+	* Create new command processor
+	* @param kernel kernel
+	*/
+	public CmdLeftSum (Kernel kernel) {
+		super(kernel);
+	}
+
+	final public GeoElement[] process(Command c) throws MyError {
+		int n = c.getArgumentNumber();
+		boolean[] ok = new boolean[n];
+		GeoElement[] arg;
+
+		switch (n) {
+		case 4 :
+			arg = resArgs(c);
+			if ((ok[0] = (arg[0] .isGeoFunctionable()))
+					&& (ok[1] = (arg[1] .isNumberValue()))
+					&& (ok[2] = (arg[2] .isNumberValue()))
+					&& (ok[3] = (arg[3] .isNumberValue()))) {
+				GeoElement[] ret =
+				{
+						kernel.LeftSum(
+								c.getLabel(),
+								((GeoFunctionable) arg[0]).getGeoFunction(),
+								(NumberValue) arg[1],
+								(NumberValue) arg[2],
+								(NumberValue) arg[3])};
+				return ret;
+			} else
+				throw argErr(app, "LeftSum", null);
+
+		default :
+			throw argNumErr(app, "LeftSum", n);
+		}
+	}
+}//CmdLeftSum
+
+/**
+ * RectangleSum[ <GeoFunction>, <Number>, <Number>, <Number> ]
+ */
+class CmdRectangleSum extends CommandProcessor {
+
+	/**
+	* Create new command processor
+	* @param kernel kernel
+	*/
+	public CmdRectangleSum (Kernel kernel) {
+		super(kernel);
+	}
+
+	final public GeoElement[] process(Command c) throws MyError {
+		int n = c.getArgumentNumber();
+		boolean[] ok = new boolean[n];
+		GeoElement[] arg;
+
+		switch (n) {
+		case 5 :
+			arg = resArgs(c);
+			if ((ok[0] = (arg[0] .isGeoFunctionable()))
+					&& (ok[1] = (arg[1] .isNumberValue()))
+					&& (ok[2] = (arg[2] .isNumberValue()))
+					&& (ok[3] = (arg[3] .isNumberValue()))					
+					&& (ok[4] = (arg[4] .isNumberValue()))) {
+				GeoElement[] ret =
+				{
+						kernel.RectangleSum(
+								c.getLabel(),
+								((GeoFunctionable) arg[0]).getGeoFunction(),
+								(NumberValue) arg[1],
+								(NumberValue) arg[2],
+								(NumberValue) arg[3],								
+								(NumberValue) arg[4])};
+				return ret;
+			} else
+				throw argErr(app, "RectangleSum", null);
+
+		default :
+			throw argNumErr(app, "RectangleSum", n);
+		}
+	}
+}//CmdRectangleSum
+
+/**
  * LowerSum[ <GeoFunction>, <Number>, <Number>, <Number> ]
  */
 class CmdTrapezoidalSum extends CommandProcessor {
