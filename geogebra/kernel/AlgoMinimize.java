911cf68e5d565051fdd3a368158a639c9d94fbfe
hans-petter <hans-petter@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-02-20 16:55:42 +0000

    For Maximize/Minimize
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@8638 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ geogebra/kernel/AlgoMinimize.java
@@ -0,0 +1,50 @@
+/* 
+GeoGebra - Dynamic Mathematics for Everyone
+http://www.geogebra.org
+
+This file is part of GeoGebra.
+
+This program is free software; you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by 
+the Free Software Foundation.
+
+*/
+
+package geogebra.kernel;
+
+import geogebra.kernel.arithmetic.NumberValue;
+import geogebra.kernel.optimization.ExtremumFinder;
+import geogebra.main.Application;
+import geogebra.kernel.optimization.RealRootFunctionVariable;
+
+/**
+ * AlgoMinimize
+ * Command Minimize[ <dependent variable>, <independent variable> ] (and Minimize[] )
+ * which searches for the independent variable which gives the 
+ * smallest result for the dependent variable.
+ * 
+ *  Extends abstract class AlgoOptimize
+ *  
+ * @author 	Hans-Petter Ulven
+ * @version 20.02.2011
+ * 
+ */
+
+public class AlgoMinimize extends AlgoOptimize{
+
+	private static final long serialVersionUID = 1L;
+	
+	/** Constructor for Maximize*/
+	public AlgoMinimize(Construction cons,String label,GeoElement dep,GeoNumeric indep){
+		super(cons,label,dep,indep,AlgoOptimize.MINIMIZE);
+		//cons.registerEuclididanViewAlgo(this);
+	}//Constructor for Maximize
+	
+
+    public String getClassName() {
+    	return "AlgoMinimize";
+    }//getClassName()    
+
+ 
+
+}//class AlgoMinimize
