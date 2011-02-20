4d32c30aa1d39b0eb6e9d7c1a089f33e29e1731d
hans-petter <hans-petter@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-02-20 16:49:14 +0000

    For Maximize/Minimize
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@8631 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/optimization/RealRootFunctionVariable.java
+++ geogebra/kernel/optimization/RealRootFunctionVariable.java
@@ -16,6 +16,7 @@ import geogebra.kernel.GeoElement;
 import geogebra.kernel.GeoNumeric;
 import geogebra.kernel.GeoPolygon;
 import geogebra.kernel.GeoSegment;
+import geogebra.kernel.GeoAngle;
 
 
 /**
@@ -26,11 +27,11 @@ import geogebra.kernel.GeoSegment;
  * the value of the independent variable when the dependent variable is 
  * maximum or minimum.
  * 
- * Used by the command searchMaximum[ <dependent variable>, <independent variable> ]
- * in kernel.AlgoSearchMaximum.  (Also a minimum version...)
+ * Used by the command Maximize[ <dependent variable>, <independent variable> ]
+ * in kernel.AlgoMaximize  (Also a minimum version...)
  * 
  * @author  	Hans-Petter Ulven
- * @version 	10.02.2011
+ * @version 	20.02.2011
  */
 public class RealRootFunctionVariable implements RealRootFunction {
 	
@@ -49,18 +50,22 @@ public class RealRootFunctionVariable implements RealRootFunction {
 	
 
 	public double evaluate(double x) {
+		double result=0.0;
 		if( (geodep!=null) && (geoindep!=null) ){
 			geoindep.setValue(x);
 			geoindep.updateCascade();
 			if(geodep.isGeoNumeric()){
-				return ((GeoNumeric)geodep).getDouble();
+				result=((GeoNumeric)geodep).getDouble();
 			}else if(geodep.isGeoPolygon()){
-				return ((GeoPolygon)geodep).getDouble();
+				result = ((GeoPolygon)geodep).getDouble();
 			}else if(geodep.isGeoSegment()){
-				return ((GeoSegment) geodep).getDouble();
+				result = ((GeoSegment) geodep).getDouble();
+			}else if(geodep.isGeoAngle()){
+				result = ((GeoAngle) geodep).getDouble();
 			}else{
-				return Double.NaN;
+				result = Double.NaN;
 			}//if type
+			return result;
 		}else{
 			return Double.NaN;
 		}//if variables are ok
