bbf057945ce6747e2745eb2d0c73257fc2aec7c4
zbynek <zbynek@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-03-10 01:22:41 +0000

    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@8925 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/AlgoName.java
+++ geogebra/kernel/AlgoName.java
@@ -8,7 +8,7 @@ This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by 
 the Free Software Foundation.
 
-*/
+ */
 
 /*
  * AlgoDependentNumber.java
@@ -20,68 +20,74 @@ package geogebra.kernel;
 
 import geogebra.kernel.arithmetic.ExpressionNode;
 
-
 /**
  * Returns the name of a GeoElement as a GeoText.
- * @author  Markus
- * @version 
+ * 
+ * @author Markus
+ * @version
  */
 public class AlgoName extends AlgoElement {
 
 	private static final long serialVersionUID = 1L;
-	private GeoElement geo;  // input
-    private GeoText text;     // output              
-    
-    /**
-     * Creates text containing name of the geo
-     * @param cons Construction
-     * @param label Label of the resulting text
-     * @param geo Element whose name shoul be used
-     */
-    public AlgoName(Construction cons, String label, GeoElement geo) {
-    	super(cons);
-        this.geo = geo;  
-        
-       text = new GeoText(cons);
+	private GeoElement geo; // input
+	private GeoText text; // output
+
+	/**
+	 * Creates text containing name of the geo
+	 * 
+	 * @param cons
+	 *            Construction
+	 * @param label
+	 *            Label of the resulting text
+	 * @param geo
+	 *            Element whose name shoul be used
+	 */
+	public AlgoName(Construction cons, String label, GeoElement geo) {
+		super(cons);
+		this.geo = geo;
+
+		text = new GeoText(cons);
 		text.setIsTextCommand(true); // stop editing as text
 		setInputOutput(); // for AlgoElement
-        
-        // compute value of dependent number
-        compute();      
-        text.setLabel(label);
-    }   
-    
+
+		// compute value of dependent number
+		compute();
+		text.setLabel(label);
+	}
+
 	public String getClassName() {
 		return "AlgoName";
 	}
-    
-    // for AlgoElement
+
+	// for AlgoElement
 	protected void setInputOutput() {
-        input = new GeoElement[1];
-        input[0] = geo;
-        
-        setOutputLength(1);        
-        setOutput(0,text);        
-        setDependencies(); // done by AlgoElement
-    }    
-    
+		input = new GeoElement[1];
+		input[0] = geo;
+
+		setOutputLength(1);
+		setOutput(0, text);
+		setDependencies(); // done by AlgoElement
+	}
+
 	/**
 	 * Returns the text element
+	 * 
 	 * @return text element containing the name
 	 */
-    public GeoText getGeoText() { return text; }
-    
-    // calc the current value of the arithmetic tree
-    protected final void compute() {    	
-    	if(geo.getParentAlgorithm() == null) {
-    		String returnLabel = geo.getRealLabel();
-    		if(returnLabel==null)
-    			geo.setLabel(returnLabel);
-    		text.setTextString(geo.getRealLabel());
-    		}	    
-    	else {
-    		// eg Name[a+3]
-    		text.setTextString(geo.getRealFormulaString(ExpressionNode.STRING_TYPE_GEOGEBRA	, false));
-    	}
-    }         
+	public GeoText getGeoText() {
+		return text;
+	}
+
+	// calc the current value of the arithmetic tree
+	protected final void compute() {
+
+		String returnLabel = geo.getRealLabel();
+		if (returnLabel != null) {
+			text.setTextString(returnLabel);
+		} else {
+			// eg Name[a+3]
+			text.setTextString(geo.getRealFormulaString(
+					ExpressionNode.STRING_TYPE_GEOGEBRA, false));
+		}
+	}
 }
