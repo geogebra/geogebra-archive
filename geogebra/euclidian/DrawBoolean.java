e93fe6f96acf55a1d779635b27a9fe6468d23601
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-04-08 18:34:14 +0000

    removed dead code
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@9344 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/euclidian/DrawBoolean.java
+++ geogebra/euclidian/DrawBoolean.java
@@ -194,36 +194,13 @@ public final class DrawBoolean extends Drawable {
 	public static class CheckBoxIcon {
 		
 		// Michael Borcherds 2008-05-11
-		// adapted from http://www.java2s.com/Open-Source/Java-Document/6.0-JDK-Modules-com.sun.java/swing/com/sun/java/swing/plaf/windows/WindowsIconFactory.java.htm
+		// used this as an outline only:
+		// http://www.java2s.com/Open-Source/Java-Document/6.0-JDK-Modules-com.sun.java/swing/com/sun/java/swing/plaf/windows/WindowsIconFactory.java.htm
 		// references to XPStyle removed
 		// option for double-size added
 		// replaced UIManager.getColor() with numbers from:
 		// http://www.java2s.com/Tutorial/Java/0240__Swing/ListingUIDefaultProperties.htm
 		
-		/*
-         * Copyright 1998-2006 Sun Microsystems, Inc.  All Rights Reserved.
-         * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-         *
-         * This code is free software; you can redistribute it and/or modify it
-         * under the terms of the GNU General Public License version 2 only, as
-         * published by the Free Software Foundation.  Sun designates this
-         * particular file as subject to the "Classpath" exception as provided
-         * by Sun in the LICENSE file that accompanied this code.
-         *
-         * This code is distributed in the hope that it will be useful, but WITHOUT
-         * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-         * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-         * version 2 for more details (a copy is included in the LICENSE file that
-         * accompanied this code).
-         *
-         * You should have received a copy of the GNU General Public License version
-         * 2 along with this work; if not, write to the Free Software Foundation,
-         * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-         *
-         * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
-         * CA 95054 USA or visit www.sun.com if you need additional information or
-         * have any questions.
-         */
 		//int csize = 13;
 		
 		EuclidianView ev;
@@ -244,60 +221,35 @@ public final class DrawBoolean extends Drawable {
                 // outer bevel
                 if (true) {
                     // Outer top/left
-                    //g.setColor(UIManager.getColor("CheckBox.shadow"));
                     g.setColor(new Color(128,128,128));
                     g.drawLine(x, y, x + (csize-2), y);
                     g.drawLine(x, y + 1, x, y + (csize-2));
 
                     // Outer bottom/right
-                    //g.setColor(UIManager.getColor("CheckBox.highlight"));
                     g.setColor(Color.white);
                     g.drawLine(x + (csize-1), y, x + (csize-1), y + (csize-1));
                     g.drawLine(x, y + (csize-1), x + (csize-2), y + (csize-1));
 
                     // Inner top.left
-                    //g.setColor(UIManager.getColor("CheckBox.darkShadow"));
                     g.setColor(new Color(64,64,64));
                     g.drawLine(x + 1, y + 1, x + (csize-3), y + 1);
                     g.drawLine(x + 1, y + 2, x + 1, y + (csize-3));
 
                     // Inner bottom/right
-                    //g.setColor(UIManager.getColor("CheckBox.light"));
                     g.setColor(new Color(212,208,200));
                     g.drawLine(x + 1, y + (csize-2), x + (csize-2), y + (csize-2));
                     g.drawLine(x + (csize-2), y + 1, x + (csize-2), y + (csize-3));
 
                     // inside box 
                     if (highlighted) {
-                        //g.setColor(UIManager.getColor("CheckBox.background"));
                         g.setColor(highlightBackground);
                     } else {
-                        //g.setColor(UIManager.getColor("CheckBox.interiorBackground"));
                         g.setColor(Color.white);
                     }
                     g.fillRect(x + 2, y + 2, csize - 4, csize - 4);
-                } else {
-                    //g.setColor(UIManager.getColor("CheckBox.shadow"));
-                    g.setColor(new Color(128,128,128));
-                    g.drawRect(x + 1, y + 1, csize - 3, csize - 3);
+                } 
 
-                    if (true) {
-                        //g.setColor(UIManager.getColor("CheckBox.background"));
-                        g.setColor(highlightBackground);
-                    } else {
-                        //g.setColor(UIManager.getColor("CheckBox.interiorBackground"));
-                        g.setColor(Color.white);
-                    }
-                    g.fillRect(x + 2, y + 2, csize - 4, csize - 4);
-                }
-
-                if (true) {
-                    //g.setColor(UIManager.getColor("CheckBox.foreground"));
-                    g.setColor(new Color(0,0,0));
-                } else {
-                    //g.setColor(UIManager.getColor("CheckBox.shadow"));
-                    g.setColor(new Color(128,128,128));
-                }
+                g.setColor(new Color(0,0,0));
 
                 // paint check
                 
@@ -310,16 +262,6 @@ public final class DrawBoolean extends Drawable {
                 	  
                 	  for (int i=3 ; i<=4 ; i++)
                           g.drawLine(x+i, y+i+2, x+i, y+i+4);
-                	  /*
-                	g.drawLine(x + 9, y + 3, x + 9, y + 3);
-                    g.drawLine(x + 8, y + 4, x + 9, y + 4);
-                    g.drawLine(x + 7, y + 5, x + 9, y + 5);
-                    g.drawLine(x + 6, y + 6, x + 8, y + 6);
-                    g.drawLine(x + 3, y + 7, x + 7, y + 7);
-                    g.drawLine(x + 4, y + 8, x + 6, y + 8);
-                    g.drawLine(x + 5, y + 9, x + 5, y + 9);
-                    g.drawLine(x + 3, y + 5, x + 3, y + 5);
-                    g.drawLine(x + 3, y + 6, x + 4, y + 6);*/
                     
                   }
                   else
@@ -331,36 +273,6 @@ public final class DrawBoolean extends Drawable {
                 	  for (int i=5 ; i<=9 ; i++)
                           g.drawLine(x+i, y+i+4, x+i, y+i+9);
                 		  
-                		  
-                	/*	  
-                    g.drawLine(x + 18, y +  6, x + 18, y +  6);
-                    g.drawLine(x + 17, y +  7, x + 18, y +  7);
-
-                    g.drawLine(x + 16, y +  8, x + 18, y +  8);
-                    g.drawLine(x + 15, y +  9, x + 18, y +  9);
-
-                    g.drawLine(x + 14, y + 10, x + 18, y + 10);
-                    g.drawLine(x + 13, y + 11, x + 18, y + 11);
-                    
-                    g.drawLine(x + 12, y + 12, x + 17, y + 12);
-                    g.drawLine(x + 11, y + 13, x + 16, y + 13);
-                    
-                    g.drawLine(x +  5, y + 14, x + 15, y + 14);
-                    g.drawLine(x +  6, y + 15, x + 14, y + 15);
-                    
-                    g.drawLine(x +  7, y + 16, x + 13, y + 16);
-                    g.drawLine(x +  8, y + 17, x + 12, y + 17);
-                    
-                    g.drawLine(x + 9, y + 18, x + 11, y + 18);
-                    g.drawLine(x + 10, y + 19, x + 10, y + 19);
-                    
-                    g.drawLine(x +  5, y + 9, x +  5, y + 9);
-                    g.drawLine(x +  5, y + 10, x +  6, y + 10);
-                    
-                    g.drawLine(x +  5, y + 11, x +  7, y + 11);
-                    g.drawLine(x +  5, y + 12, x +  8, y + 12);
-                    
-                    g.drawLine(x +  5, y + 13, x +  9, y + 13);*/
                   }
                 }
             }
