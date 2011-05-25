4140745c0bcf9f2802a48cd175c4f16e3e27f3af
george <george@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-05-25 04:25:27 +0000

    LaTeX for AlgebraView (testing)
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@9740 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/view/algebra/MyRenderer.java
+++ geogebra/gui/view/algebra/MyRenderer.java
@@ -1,14 +1,18 @@
 package geogebra.gui.view.algebra;
 
+import geogebra.euclidian.Drawable;
 import geogebra.kernel.GeoElement;
 import geogebra.kernel.Kernel;
+import geogebra.kernel.arithmetic.ExpressionNode;
 import geogebra.main.Application;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -27,7 +31,16 @@ public class MyRenderer extends DefaultTreeCellRenderer {
 	protected Application app;
 	private Kernel kernel;
 	private ImageIcon iconShown, iconHidden;
-		
+	
+
+	
+	private boolean useLaTeX = false;  //<============== flag for testing 
+	
+	private ImageIcon latexIcon;
+	private String latexStr = null;
+	
+	
+	
 	public MyRenderer(Application app) {
 		setOpaque(true);		
 		this.app = app;
@@ -38,6 +51,9 @@ public class MyRenderer extends DefaultTreeCellRenderer {
 		
 		setOpenIcon(app.getImageIcon("tree-close.png"));
 		setClosedIcon(app.getImageIcon("tree-open.png"));
+		
+		latexIcon = new ImageIcon();
+		String laTextStr;
 	}
 	
 	public Component getTreeCellRendererComponent(
@@ -91,7 +107,15 @@ public class MyRenderer extends DefaultTreeCellRenderer {
 			} else {
 				setIcon(iconHidden);
 			}
-			
+
+			if(useLaTeX  && kernel.getAlgebraStyle() == Kernel.ALGEBRA_STYLE_VALUE){
+				latexStr  = "\\," +  geo.getLabel() + "\\,=\\," +
+					geo.getFormulaString(ExpressionNode.STRING_TYPE_LATEX, true);				
+				drawLatexImageIcon(latexIcon, latexStr, app.getPlainFont(), false, Color.black, this.getBackground() );
+				setIcon(joinIcons((ImageIcon) getIcon(),latexIcon));
+				setText("");
+			}
+
 			// sometimes objects do not identify themselves as GeoElement for a second,
 			// causing the else-part to give them a border (because they have no children)
 			// we have to remove this border to prevent an unnecessary indent
@@ -126,6 +150,8 @@ public class MyRenderer extends DefaultTreeCellRenderer {
 			
 			// make sure we use a font that can display the text
 			setFont(app.getFontCanDisplay(str));
+			
+						
 		}		
 		
 		return this;
@@ -138,6 +164,62 @@ public class MyRenderer extends DefaultTreeCellRenderer {
 	 */
 	protected String getAlgebraDescriptionTextOrHTML(GeoElement geo){
 		return geo.getAlgebraDescriptionTextOrHTML();
+	}
+	
+	
+	/**
+	 * Draw a LaTeX image in the cell icon. Drawing is done twice. First draw gives 
+	 * the needed size of the image. Second draw renders the image with the correct
+	 * dimensions.
+	 */
+	private void drawLatexImageIcon(ImageIcon latexIcon, String latex, Font font, boolean serif, Color fgColor, Color bgColor) {
+		
+		// Create image with dummy size, then draw into it to get the correct size
+		BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2image = image.createGraphics();
+		g2image.setBackground(bgColor);
+		g2image.clearRect(0, 0, image.getWidth(), image.getHeight());
+		g2image.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2image.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		Dimension d = new Dimension();
+		d = Drawable.drawEquation(app, null, g2image, 0, 0, latex, font, serif, fgColor,
+				bgColor);
+
+		// Now use this size and draw again to get the final image
+		image = new BufferedImage(d.width, d.height, BufferedImage.TYPE_INT_ARGB);
+		g2image = image.createGraphics();
+		g2image.setBackground(bgColor);
+		g2image.clearRect(0, 0, image.getWidth(), image.getHeight());
+		g2image.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2image.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		d = Drawable.drawEquation(app, null, g2image, 0, 0, latex, font, serif, fgColor,
+				bgColor);
+
+		latexIcon.setImage(image);
+		
+	}
+	
+	public ImageIcon joinIcons(ImageIcon leftIcon, ImageIcon rightIcon){
+		
+		int w1 = leftIcon.getIconWidth();
+		int w2 = rightIcon.getIconWidth();
+		int h1 = leftIcon.getIconHeight();
+		int h2 = rightIcon.getIconHeight();
+		int h = Math.max(h1, h2);
+		int mid = h/2;
+		BufferedImage image = new BufferedImage(w1+w2, h, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = (Graphics2D) image.getGraphics();
+		g2.drawImage(leftIcon.getImage(), 0, mid - h1/2, null);
+		g2.drawImage(rightIcon.getImage(), w1,  mid - h2/2, null);
+		g2.dispose(); 
+		
+		ImageIcon ic = new ImageIcon(image);
+		return ic;
 	}
 	
 	
