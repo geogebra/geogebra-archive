8df3ef6327a2e1ca10b4112c0d25d07864dc1a00
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-06-16 21:38:28 +0000

    finished TDistribution
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10075 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CommandProcessor.java
+++ geogebra/kernel/commands/CommandProcessor.java
@@ -5910,24 +5910,53 @@ class CmdTDistribution extends CommandProcessor {
 		int n = c.getArgumentNumber();
 		boolean[] ok = new boolean[n];
 		GeoElement[] arg;
+		
+		arg = resArgs(c);
 
+		boolean cumulative = false; // default for n=2
 		switch (n) {
-		case 2:
-			arg = resArgs(c);
-			if ((ok[0] = arg[0].isNumberValue())
-					&& (ok[1] = arg[1].isNumberValue())) {
-				GeoElement[] ret = { kernel.TDistribution(c.getLabel(),
-						(NumberValue) arg[0], (NumberValue) arg[1]) };
-				return ret;
-
+		case 3:
+			if (arg[2].isGeoBoolean()) {
+				cumulative = ((GeoBoolean)arg[2]).getBoolean();
 			} else
-				throw argErr(app, c.getName(), arg[0]);
+				throw argErr(app, c.getName(), arg[2]);
 
-		default:
-			throw argNumErr(app, c.getName(), n);
+			// fall through
+		case 2:			
+			if ((ok[0] = arg[0].isNumberValue()) ) {
+				if (arg[1].isGeoFunction() && ((GeoFunction)arg[1]).toString().equals("x")) {
+
+					String v = arg[0].getLabel();
+					String command;
+					
+					if (cumulative) {
+						command = "0.5+sign(x)/2*(betaRegularized(("+v+")/2,0.5,1)-betaRegularized(("+v+")/2,0.5,("+v+")/("+v+"+x^2)))";
+					} else {
+						command = "gamma(("+v+"+1)/2)*(1+x^2/("+v+"))^(-(("+v+"+1)/2))/(gamma(("+v+")/2)*sqrt(pi*("+v+")))";
+					}
+					
+					
+					GeoElement[] ret = kernel.getAlgebraProcessor().processAlgebraCommand(command, true);
+					return ret;
+
+
+				} else if (arg[1].isNumberValue()) {
+					GeoElement[] ret = { kernel.TDistribution(c.getLabel(),
+							(NumberValue) arg[0], (NumberValue) arg[1]) };
+					return ret;
+				} else
+					throw argErr(app, c.getName(), arg[1]);
+
+				} else 
+					throw argErr(app, c.getName(), arg[0]);
+				
+
+			default:
+				throw argNumErr(app, c.getName(), n);
+			}
 		}
 	}
-}
+
 
 /**
  *InverseTDistribution
