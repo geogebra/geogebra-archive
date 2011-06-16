42127a654c1659027573f0d58fa6ad088df6c389
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-06-16 22:03:02 +0000

    finished Cauchy[ ]
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10077 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CommandProcessor.java
+++ geogebra/kernel/commands/CommandProcessor.java
@@ -6386,7 +6386,7 @@ class CmdInverseGamma extends CommandProcessor {
 }
 
 /**
- *Cauchy
+ *Cauchy Distribution
  */
 class CmdCauchy extends CommandProcessor {
 
@@ -6404,31 +6404,56 @@ class CmdCauchy extends CommandProcessor {
 		int n = c.getArgumentNumber();
 		boolean[] ok = new boolean[n];
 		GeoElement[] arg;
+		
+		arg = resArgs(c);
 
+		boolean cumulative = false; // default for n=3
 		switch (n) {
-		case 3:
-			arg = resArgs(c);
-			if ((ok[0] = arg[0].isNumberValue())
-					&& (ok[1] = arg[1].isNumberValue())
-					&& (ok[2] = arg[2].isNumberValue())) {
-				GeoElement[] ret = { kernel.Cauchy(c.getLabel(),
-						(NumberValue) arg[0], (NumberValue) arg[1],
-						(NumberValue) arg[2]) };
-				return ret;
+		case 4:
+			if (arg[3].isGeoBoolean()) {
+				cumulative = ((GeoBoolean)arg[3]).getBoolean();
+			} else
+				throw argErr(app, c.getName(), arg[3]);
 
-			} else if (!ok[0])
-				throw argErr(app, c.getName(), arg[0]);
-			else if (!ok[1])
-				throw argErr(app, c.getName(), arg[1]);
-			else
-				throw argErr(app, c.getName(), arg[2]);
+			// fall through
+		case 3:			
+			if ((ok[0] = arg[0].isNumberValue()) && (ok[1] = arg[1].isNumberValue())) {
+				if (arg[2].isGeoFunction() && ((GeoFunction)arg[2]).toString().equals("x")) {
 
-		default:
-			throw argNumErr(app, c.getName(), n);
+					String x0 = arg[0].getLabel();
+					String g = arg[1].getLabel();
+					String command;
+					
+					if (cumulative) {
+						command = "1/pi atan((x-("+x0+"))/abs("+g+"))+0.5";
+					} else {
+						command = "1/pi abs("+g+")/(("+g+")^2+(x-("+x0+"))^2)";
+					}
+					
+					
+					GeoElement[] ret = kernel.getAlgebraProcessor().processAlgebraCommand(command, true);
+					return ret;
+
+
+				} else if (arg[2].isNumberValue()) {
+					GeoElement[] ret = { kernel.Cauchy(c.getLabel(),
+							(NumberValue) arg[0], (NumberValue) arg[1],
+							(NumberValue) arg[2]) };
+					return ret;
+				} else
+					throw argErr(app, c.getName(), arg[2]);
+
+				} else if (!ok[0])
+					throw argErr(app, c.getName(), arg[0]);
+				else if (!ok[1])
+					throw argErr(app, c.getName(), arg[1]);
+				
+
+			default:
+				throw argNumErr(app, c.getName(), n);
+			}
 		}
 	}
-}
-
 /**
  *InverseCauchy
  */
