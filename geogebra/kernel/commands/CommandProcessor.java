1e5e784e2fae7b85b8a94f689999e5793daf0737
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-06-16 21:48:16 +0000

    finished Gamma[ ]
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10076 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CommandProcessor.java
+++ geogebra/kernel/commands/CommandProcessor.java
@@ -6271,7 +6271,7 @@ class CmdInverseFDistribution extends CommandProcessor {
 }
 
 /**
- *Gamma
+ *Gamma Distribution
  */
 class CmdGamma extends CommandProcessor {
 
@@ -6289,30 +6289,56 @@ class CmdGamma extends CommandProcessor {
 		int n = c.getArgumentNumber();
 		boolean[] ok = new boolean[n];
 		GeoElement[] arg;
+		
+		arg = resArgs(c);
 
+		boolean cumulative = false; // default for n=3
 		switch (n) {
-		case 3:
-			arg = resArgs(c);
-			if ((ok[0] = arg[0].isNumberValue())
-					&& (ok[1] = arg[1].isNumberValue())
-					&& (ok[2] = arg[2].isNumberValue())) {
-				GeoElement[] ret = { kernel.Gamma(c.getLabel(),
-						(NumberValue) arg[0], (NumberValue) arg[1],
-						(NumberValue) arg[2]) };
-				return ret;
+		case 4:
+			if (arg[3].isGeoBoolean()) {
+				cumulative = ((GeoBoolean)arg[3]).getBoolean();
+			} else
+				throw argErr(app, c.getName(), arg[3]);
 
-			} else if (!ok[0])
-				throw argErr(app, c.getName(), arg[0]);
-			else if (!ok[1])
-				throw argErr(app, c.getName(), arg[1]);
-			else
-				throw argErr(app, c.getName(), arg[2]);
+			// fall through
+		case 3:			
+			if ((ok[0] = arg[0].isNumberValue()) && (ok[1] = arg[1].isNumberValue())) {
+				if (arg[2].isGeoFunction() && ((GeoFunction)arg[2]).toString().equals("x")) {
 
-		default:
-			throw argNumErr(app, c.getName(), n);
+					String k = arg[0].getLabel();
+					String t = arg[1].getLabel();
+					String command;
+					
+					if (cumulative) {
+						command = "If[x<0,0,gamma("+k+",x/("+t+"))/gamma("+k+")]";
+					} else {
+						command = "If[x<0,0,x^("+k+"-1) exp(-x/("+t+"))/(gamma("+k+")("+t+")^("+k+"))]";
+					}
+					
+					
+					GeoElement[] ret = kernel.getAlgebraProcessor().processAlgebraCommand(command, true);
+					return ret;
+
+
+				} else if (arg[2].isNumberValue()) {
+					GeoElement[] ret = { kernel.Gamma(c.getLabel(),
+							(NumberValue) arg[0], (NumberValue) arg[1],
+							(NumberValue) arg[2]) };
+					return ret;
+				} else
+					throw argErr(app, c.getName(), arg[2]);
+
+				} else if (!ok[0])
+					throw argErr(app, c.getName(), arg[0]);
+				else if (!ok[1])
+					throw argErr(app, c.getName(), arg[1]);
+				
+
+			default:
+				throw argNumErr(app, c.getName(), n);
+			}
 		}
 	}
-}
 
 /**
  *InverseGamma
