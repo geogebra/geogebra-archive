800111bea7dfbf2075cfe4b7df12a12f041ee0a1
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-06-17 21:38:03 +0000

    finished Exponential[ ]
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10087 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/commands/CommandProcessor.java
+++ geogebra/kernel/commands/CommandProcessor.java
@@ -6104,7 +6104,7 @@ class CmdInverseChiSquared extends CommandProcessor {
 }
 
 /**
- *Exponential
+ *Exponential distribution
  */
 class CmdExponential extends CommandProcessor {
 
@@ -6122,26 +6122,53 @@ class CmdExponential extends CommandProcessor {
 		int n = c.getArgumentNumber();
 		boolean[] ok = new boolean[n];
 		GeoElement[] arg;
+		
+		arg = resArgs(c);
 
+		boolean cumulative = false; // default for n=2
 		switch (n) {
-		case 2:
-			arg = resArgs(c);
-			if ((ok[0] = arg[0].isNumberValue())
-					&& (ok[1] = arg[1].isNumberValue())) {
-				GeoElement[] ret = { kernel.Exponential(c.getLabel(),
-						(NumberValue) arg[0], (NumberValue) arg[1]) };
-				return ret;
+		case 3:
+			if (arg[2].isGeoBoolean()) {
+				cumulative = ((GeoBoolean)arg[2]).getBoolean();
+			} else
+				throw argErr(app, c.getName(), arg[2]);
 
-			} else if (!ok[0])
-				throw argErr(app, c.getName(), arg[0]);
-			else
-				throw argErr(app, c.getName(), arg[1]);
+			// fall through
+		case 2:			
+			if ((ok[0] = arg[0].isNumberValue()) ) {
+				if (arg[1].isGeoFunction() && ((GeoFunction)arg[1]).toString().equals("x")) {
 
-		default:
-			throw argNumErr(app, c.getName(), n);
+					String l = arg[0].getLabel();
+					String command = null;
+					
+					if (cumulative) {
+						command="If[x<0,0,1-exp(-("+l+")x)]";
+					} else {
+						command="If[x<0,0,("+l+")exp(-("+l+")x)]";
+					}
+					
+					
+					GeoElement[] ret = kernel.getAlgebraProcessor().processAlgebraCommand(command, true);
+					return ret;
+
+
+				} else if (arg[1].isNumberValue()) {
+					GeoElement[] ret = { kernel.Exponential(c.getLabel(),
+							(NumberValue) arg[0], (NumberValue) arg[1]) };
+					return ret;
+				} else
+					throw argErr(app, c.getName(), arg[1]);
+
+				} else 
+					throw argErr(app, c.getName(), arg[0]);
+				
+
+			default:
+				throw argNumErr(app, c.getName(), n);
+			}
 		}
 	}
-}
+
 
 /**
  *InverseExponential
