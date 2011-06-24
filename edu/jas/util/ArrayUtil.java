f68c7394d0f03d506bb339f062cba047bfb9ef21
julian <julian@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-06-24 18:10:33 +0000

    Deleted broken and unused implementation of ArrayUtil.copyOf() That is what libraries (apache commons, guava) are good for!
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10184 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- edu/jas/util/ArrayUtil.java
+++ edu/jas/util/ArrayUtil.java
@@ -4,54 +4,29 @@
 
 package edu.jas.util;
 
-
-//import org.apache.log4j.Logger;
-
 import edu.jas.structure.Complex;
-
 
 /**
  * Array utilities.
- * For example copyOf from Java 6.
+ * 
  * @author Heinz Kredel
  */
 
 public class ArrayUtil {
 
+	/**
+	 * * Copy the specified array.
+	 * 
+	 * @param original
+	 *            array.
+	 * @param newLength
+	 *            new array length.
+	 * @return copy of this.
+	 */
+	public static Complex[] copyOfComplex(Complex[] original, int newLength) {
+		Complex[] copy = new Complex[newLength];
+		System.arraycopy(original, 0, copy, 0, Math.min(original.length, newLength));
+		return copy;
+	}
 
-    //private static final Logger logger = Logger.getLogger(ArrayUtil.class);
-    // private static boolean debug = logger.isDebugEnabled();
-
-
-    /**                                                                                                                            * Copy the specified array.
-     * @param original array.
-     * @param newLength new array length.
-     * @return copy of this.
-     */
-    public static <T> T[] copyOf(T[] original, int newLength) {
-        T[] copy = (T[]) new Object[newLength];
-        System.arraycopy(original, 0, copy, 0, Math.min(original.length, newLength));
-        return copy;
-    }
-
-
-    /**                                                                                                                            * Copy the specified array.
-     * @param original array.
-     * @param newLength new array length.
-     * @return copy of this.
-     */
-    public static Complex[] copyOfComplex(Complex[] original, int newLength) {
-        Complex[] copy = new Complex[newLength];
-        System.arraycopy(original, 0, copy, 0, Math.min(original.length, newLength));
-        return copy;
-    }
-
-
-    /**                                                                                                                            * Copy the specified array.
-     * @param original array.
-     * @return copy of this.
-     */
-    public static <T> T[] copyOf(T[] original) {
-        return (T[]) copyOf(original,original.length);
-    }
 }
