347a32ecf5a27adf7b500c0fcfa969a9970e2ba4
markus <markus@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-06-25 18:18:52 +0000

    updated MathPiper, version .81f
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10206 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- edu/jas/util/ArrayUtil.java
+++ edu/jas/util/ArrayUtil.java
@@ -4,22 +4,54 @@
 
 package edu.jas.util;
 
+
+//import org.apache.log4j.Logger;
+
 import edu.jas.structure.Complex;
-import edu.jas.structure.RingElem;
+
 
 /**
  * Array utilities.
- * 
+ * For example copyOf from Java 6.
  * @author Heinz Kredel
  */
 
-public class ArrayUtil {	
-	
-	public static <C extends RingElem<C>> Complex<C>[] copyOfComplex(Complex<C>[] original, int newLength) {
-		@SuppressWarnings("unchecked")
-		Complex<C>[] copy = new Complex[newLength];
-		System.arraycopy(original, 0, copy, 0, Math.min(original.length, newLength));
-		return copy;
-	}
+public class ArrayUtil {
 
+
+    //private static final Logger logger = Logger.getLogger(ArrayUtil.class);
+    // private static boolean debug = logger.isDebugEnabled();
+
+
+    /**                                                                                                                            * Copy the specified array.
+     * @param original array.
+     * @param newLength new array length.
+     * @return copy of this.
+     */
+    public static <T> T[] copyOf(T[] original, int newLength) {
+        T[] copy = (T[]) new Object[newLength];
+        System.arraycopy(original, 0, copy, 0, Math.min(original.length, newLength));
+        return copy;
+    }
+
+
+    /**                                                                                                                            * Copy the specified array.
+     * @param original array.
+     * @param newLength new array length.
+     * @return copy of this.
+     */
+    public static Complex[] copyOfComplex(Complex[] original, int newLength) {
+        Complex[] copy = new Complex[newLength];
+        System.arraycopy(original, 0, copy, 0, Math.min(original.length, newLength));
+        return copy;
+    }
+
+
+    /**                                                                                                                            * Copy the specified array.
+     * @param original array.
+     * @return copy of this.
+     */
+    public static <T> T[] copyOf(T[] original) {
+        return (T[]) copyOf(original,original.length);
+    }
 }
