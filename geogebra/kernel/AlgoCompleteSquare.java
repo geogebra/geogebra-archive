d969e13238c5fc387350bebe7ce8916cdfc50538
zbynek <zbynek@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-06-30 08:47:37 +0000

    Fixed check of function validity in CompleteSquare
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10335 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/AlgoCompleteSquare.java
+++ geogebra/kernel/AlgoCompleteSquare.java
@@ -50,21 +50,22 @@ public class AlgoCompleteSquare extends AlgoElement {
 					.power(new MyDouble(kernel,2)).multiply(a).plus(k);
 			square.getFunction().setExpression(squareE);
 		}
-		
+		lastDeg = degInt;
 		fv.setVarString(f.getVarString());
 		//px^2+qx+r; p+q+r=s;		
 		double r = f.evaluate(0);
 		double s = f.evaluate(1);
 		//          p*2^d+q*(2^d/2)  => p*2^(d/2)+q  => p*(2^(d/2)-1)  => p
 		double p = ((f.evaluate(2)-r)*Math.pow(2, -deg/2) - (s-r))  / (Math.pow(2, deg/2)-1);
-		double q = s-p-r;
+		double q = s-p-r;		
 		
 		boolean isQuadratic = !f.isGeoFunctionConditional();
 		double[] checkpoints = {Math.pow(10000,2/degInt),-Math.pow(10000,2/degInt),Math.PI,Math.E};
 		for(int i=0;i<checkpoints.length;i++){
 			double x=checkpoints[i];
-			if(!Kernel.isEqual(p*Math.pow(x,deg)+q*Math.pow(x,deg/2)+r, f.evaluate(x)))
-				isQuadratic = false;
+			double dif = p*Math.pow(x,deg)+q*Math.pow(x,deg/2)+r- f.evaluate(x);
+			if(!Kernel.isZero(dif*dif))				
+				isQuadratic = false;			
 		}
 		if(!isQuadratic){
 			square.setUndefined();
