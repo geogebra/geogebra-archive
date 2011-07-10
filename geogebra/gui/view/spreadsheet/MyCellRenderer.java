ae210a369fb5e0c6d748cbc16000947a05812c4d
george <george@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-07-10 15:16:35 +0000

    code cleanup for spreadsheet cell renderer
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10503 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/view/spreadsheet/MyCellRenderer.java
+++ geogebra/gui/view/spreadsheet/MyCellRenderer.java
@@ -35,20 +35,23 @@ import javax.swing.table.DefaultTableCellRenderer;
 public class MyCellRenderer extends DefaultTableCellRenderer
 {
 	private static final long serialVersionUID = 1L;
+
+	// ggb fields
 	private Application app;
 	private Kernel kernel;
 	private SpreadsheetView view;
-	
+
 	// LaTeX
 	private ImageIcon latexIcon, emptyIcon; 
 	private String latexStr = new String();
-	
+
 	// Cell formats
 	private CellFormat formatHandler;
 	private Point cellPoint;
 	private Integer alignment = -1;
 	private Integer traceBorder = -1;
-	
+	private Integer fontStyle;
+
 	// Borders (not implemented yet)
 	private Border cellPadding = BorderFactory.createEmptyBorder(2, 5, 2, 5);
 	private Border bTop = BorderFactory.createMatteBorder(1, 0, 0, 0, Color.RED);
@@ -63,95 +66,97 @@ public class MyCellRenderer extends DefaultTableCellRenderer
 	private JComboBox comboBox;
 	private DefaultComboBoxModel cbModel;
 	private Color bgColor;
-	private Integer fontStyle;
+
+
+	// Cell geo
+	private GeoElement geo;
+
 	
 	
-	
+	/*********************************************************
+	 * Constructor
+	 * @param app
+	 * @param view
+	 * @param formatHandler
+	 */
 	public MyCellRenderer(Application app, SpreadsheetView view, CellFormat formatHandler) {
-		
+
 		this.app = app;		
 		this.kernel = app.getKernel();
 		this.formatHandler =  formatHandler;
 		this.view = view;
-		
+
 		//Add horizontal padding
 		setBorder(BorderFactory.createEmptyBorder(2, 5, 2, 5));
-		
+
 
 		// The cell renderer extends JLabel...its icon is used to display LaTeX.
 		latexIcon = new ImageIcon();
 		emptyIcon = new ImageIcon();
-		
+
 		cellPoint = new Point(); // used for cell format calls
-		
+
 		// Rendering for booleans, buttons and lists
 		checkBox = new JCheckBox();
 		button = new JButton();
 		comboBox = new JComboBox();
 		comboBox.setRenderer(new MyListCellRenderer());
-		
+
 		cbModel = new DefaultComboBoxModel();
 		comboBox.setModel(cbModel);
 	}
-	
-	
-	
+
+
+
 	public Component getTableCellRendererComponent(JTable table, Object value,
 			boolean isSelected, boolean hasFocus, int row, int column) 
 	{	
-		
+
+		setBorder(cellPadding);		
 		cellPoint.setLocation(column, row);
 		setIcon(emptyIcon);
 		setIconTextGap(0);
-		
+
+
+		// Set visible formats ... do this before exit with null geo
+		// ==================================================
 		// set default background color (adjust later if geo exists)
 		bgColor = (Color) formatHandler.getCellFormat(cellPoint, 
 				CellFormat.FORMAT_BGCOLOR);	
 		if(bgColor == null) 
 			bgColor = table.getBackground();
 		setBackground(bgColor);
-		
-		fontStyle = (Integer) formatHandler.getCellFormat(cellPoint, 
-				CellFormat.FORMAT_FONTSTYLE);
-		if(fontStyle == null)
-			fontStyle = Font.PLAIN;
-		
-			
-		
-		
-		//TODO: Other formats should be set here ... before exit with null geo
-		
-		
-		// exit if no geo to display
-		if (value == null) {		
+
+
+
+		// Get the cell geo, exit if null
+		// ==================================================
+		if (value != null) {
+			geo = (GeoElement)value;
+		}else{
 			setText("");
 			return this;
 		}
-				
-		
-		// set cell content
-		GeoElement geo = (GeoElement)value;
-		
-		
-		//=======================================================
+
+
+
 		// use special rendering for buttons, booleans and lists
-		
+		//=======================================================
+
 		if(view.allowSpecialEditor() && kernel.getAlgebraStyle()==Kernel.ALGEBRA_STYLE_VALUE){
 
 			if(geo.isGeoBoolean()){
 				checkBox.setBackground(table.getBackground());
 				checkBox.setHorizontalAlignment(CENTER);
 				checkBox.setEnabled(geo.isIndependent());
-				
-				if(geo.isLabelVisible()){
-					
+
+				if(geo.isLabelVisible()){					
 					//checkBox.setText(geo.getCaption());
 				}
 				checkBox.setSelected(((GeoBoolean)geo).getBoolean());
 
 				return checkBox;
 			}
-
 
 			if(geo.isGeoButton()){
 				//button.setBackground(table.getBackground());
@@ -173,51 +178,64 @@ public class MyCellRenderer extends DefaultTableCellRenderer
 			}
 		}
 
-		// end special rendering
-		//========================================================
+		//===============================================
+		// (end special rendering)
 
-		
+
+
 		// Set text according to algebra style
+		//===============================================
 		String text = null;
 		if (geo.isIndependent()) {
 			text = geo.toValueString();
 		} else {
 			switch (kernel.getAlgebraStyle()) {
-				case Kernel.ALGEBRA_STYLE_VALUE:
-					text = geo.toValueString();
-					break;
-					
-				case Kernel.ALGEBRA_STYLE_DEFINITION:
-					text = GeoElement.convertIndicesToHTML(geo.getDefinitionDescription());
-					break;
-					
-				case Kernel.ALGEBRA_STYLE_COMMAND:
-					text = GeoElement.convertIndicesToHTML(geo.getCommandDescription());
-					break;
+			case Kernel.ALGEBRA_STYLE_VALUE:
+				text = geo.toValueString();
+				break;
+
+			case Kernel.ALGEBRA_STYLE_DEFINITION:
+				text = GeoElement.convertIndicesToHTML(geo.getDefinitionDescription());
+				break;
+
+			case Kernel.ALGEBRA_STYLE_COMMAND:
+				text = GeoElement.convertIndicesToHTML(geo.getCommandDescription());
+				break;
 			}	
 		}
 
-		
+
 		// Set font
+		//===============================================
+		fontStyle = (Integer) formatHandler.getCellFormat(cellPoint, 
+				CellFormat.FORMAT_FONTSTYLE);
+		if(fontStyle == null)
+			fontStyle = Font.PLAIN;
+
 		setText(text);
 		setFont(app.getFontCanDisplay(text, fontStyle));
-		
+
+
+
+
 		// Set foreground and background color
-		
+		//===============================================
 		if (geo.getBackgroundColor() != null) {
 			bgColor = geo.getBackgroundColor();
 		}
-		
+
 		if (geo.doHighlighting()) {
+
 			bgColor = MyTable.SELECTED_BACKGROUND_COLOR;
 		}
 		setBackground(bgColor);
 		setForeground(geo.getAlgebraColor());
-		
-		
-		
-		
+
+
+
+
 		// Set horizontal alignment
+		//===============================================
 		alignment = (Integer) formatHandler.getCellFormat(cellPoint,
 				CellFormat.FORMAT_ALIGN);
 		if (alignment != null) {
@@ -227,55 +245,21 @@ public class MyCellRenderer extends DefaultTableCellRenderer
 		} else {
 			setHorizontalAlignment(JLabel.RIGHT);
 		}	
-		
-	
-		// Set border
-		// (not finished ... border cell formats need coding)
-		traceBorder = (Integer) formatHandler.getCellFormat(cellPoint,
-				CellFormat.FORMAT_TRACING);
-		
-		if (traceBorder != null){
-			
-			switch (traceBorder){
-			case CellFormat.BORDER_STYLE_ALL:
-				setBorder(BorderFactory.createCompoundBorder(bAll, cellPadding));
-			break;
-			case CellFormat.BORDER_STYLE_TOP:
-				setBorder(BorderFactory.createCompoundBorder(bTop, cellPadding));
-			break;
-			case CellFormat.BORDER_STYLE_LEFT:
-				setBorder(BorderFactory.createCompoundBorder(bLeft, cellPadding));
-			break;
-			case CellFormat.BORDER_STYLE_BOTTOM:
-				setBorder(BorderFactory.createCompoundBorder(bBottom, cellPadding));
-			break;
-			case CellFormat.BORDER_STYLE_RIGHT:
-				setBorder(BorderFactory.createCompoundBorder(bRight, cellPadding));
-			break;
-			
-			}
-			
-		}else{
-			setBorder(cellPadding);	
-		}
-		
-		
-		
-		
-		// set icons for LaTeX and images
-		// TODO: LaTeX slows things down, need a better test for which geos 
-		// get LaTex and maybe a toggle switch 
 
+
+
+		// set icons for LaTeX and images
+		// TODO: LaTeX slows things down, need a better test for which geos get LaTex?  
+		//===============================================
 		if(geo.isGeoImage()){		
 			latexIcon.setImage(((GeoImage) geo).getFillImage());
 			setIcon(latexIcon);
 			setHorizontalAlignment(this.CENTER);
 			setText("");
-	
+
 		}else{
 
 			boolean isSerif = false;
-
 			if (geo.isDefined() && kernel.getAlgebraStyle() == Kernel.ALGEBRA_STYLE_VALUE) {
 
 				latexStr = geo.getFormulaString(ExpressionNode.STRING_TYPE_LATEX, true);
@@ -300,22 +284,21 @@ public class MyCellRenderer extends DefaultTableCellRenderer
 			}
 
 		}
-		
-		
 
-		
 		return this;
 	}
 
-	
-	
+
+
+
+
 	/**
 	 * Draw a LaTeX image in the cell icon. Drawing is done twice. First draw gives 
 	 * the needed size of the image. Second draw renders the image with the correct
 	 * dimensions.
 	 */
 	private void drawLatexImageIcon(ImageIcon latexIcon, String latex, Font font, boolean serif, Color fgColor, Color bgColor) {
-		
+
 		// Create image with dummy size, then draw into it to get the correct size
 		BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2image = image.createGraphics();
@@ -325,7 +308,7 @@ public class MyCellRenderer extends DefaultTableCellRenderer
 				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		g2image.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-		
+
 		FormulaDimension d = new FormulaDimension();
 		d = Drawable.drawEquation(app, null, g2image, 0, 0, latex, font, serif, fgColor,
 				bgColor);
@@ -343,30 +326,30 @@ public class MyCellRenderer extends DefaultTableCellRenderer
 				bgColor);
 
 		latexIcon.setImage(image);
-		
+
 	}
-	
-	
-	
-	
+
+
+
+
 
 	//======================================================
 	//         ComboBox Cell Renderer 
 	//======================================================
-	
+
 	/**
 	 * Custom cell renderer that displays GeoElement descriptions.
 	 */
 	private class MyListCellRenderer extends DefaultListCellRenderer {
-		
+
 		@Override
 		public Component getListCellRendererComponent(JList list, Object value,
 				int index, boolean isSelected, boolean hasFocus) {
 
 			setBackground(Color.WHITE);
 			JLabel lbl = (JLabel)super.getListCellRendererComponent(
-	                list, value, index, isSelected, hasFocus);
-	        lbl.setHorizontalAlignment(LEFT);
+					list, value, index, isSelected, hasFocus);
+			lbl.setHorizontalAlignment(LEFT);
 
 			if (value != null) {
 				GeoElement geo = (GeoElement) value;
@@ -376,11 +359,48 @@ public class MyCellRenderer extends DefaultTableCellRenderer
 					setText(geo.getLabel());
 			} else
 				setText(" ");
-			
+
 			return lbl;
 		}
 
 	}
+
+
+
+	/*
+	// Set border
+	// (not finished ... border cell formats need coding)
+//	traceBorder = (Integer) formatHandler.getCellFormat(cellPoint,
+		//	CellFormat.FORMAT_TRACING);
+
+	if (traceBorder != null){
+
+		switch (traceBorder){
+		case CellFormat.BORDER_STYLE_ALL:
+			setBorder(BorderFactory.createCompoundBorder(bAll, cellPadding));
+		break;
+		case CellFormat.BORDER_STYLE_TOP:
+			setBorder(BorderFactory.createCompoundBorder(bTop, cellPadding));
+		break;
+		case CellFormat.BORDER_STYLE_LEFT:
+			setBorder(BorderFactory.createCompoundBorder(bLeft, cellPadding));
+		break;
+		case CellFormat.BORDER_STYLE_BOTTOM:
+			setBorder(BorderFactory.createCompoundBorder(bBottom, cellPadding));
+		break;
+		case CellFormat.BORDER_STYLE_RIGHT:
+			setBorder(BorderFactory.createCompoundBorder(bRight, cellPadding));
+		break;
+
+		}
+
+	}else{
+		setBorder(cellPadding);	
+	}
+
+	 */
+
+
 
 
 }
