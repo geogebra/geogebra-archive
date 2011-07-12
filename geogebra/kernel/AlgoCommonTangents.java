5d312f380c9e5f478ca644e3d063e9764fb93c17
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-07-12 06:29:25 +0000

    continuity fixes for Common Tangents
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@10570 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/AlgoCommonTangents.java
+++ geogebra/kernel/AlgoCommonTangents.java
@@ -20,52 +20,78 @@ package geogebra.kernel;
 import geogebra.euclidian.EuclidianConstants;
 
 /**
- * Common tangents of two circles
+ * Two tangents through point P to conic section c
  */
 public class AlgoCommonTangents extends AlgoElement {
 
     private static final long serialVersionUID = 1L;
-
-    private GeoConic c1; // input
-    private GeoConic c2; // input
+    private GeoPoint P, P2; // tmp
+    private GeoConic c, c2; // input
     private GeoLine[] tangents; // output  
 
-    private GeoPoint P; // temp
-
-    private GeoLine polar;
-    private AlgoIntersectLineConic algoIntersect;
-    private GeoPoint[] tangentPoints;
-    private boolean equalLines = false;
-
-    /*  
-        AlgoCommonTangents(Construction cons, String label, GeoPoint P, GeoConic c) {
-        this(cons, P, c);
-        GeoElement.setLabels(label, tangents);            
-        }*/
+    private GeoLine polar, polar2;
+    private AlgoIntersectLineConic algoIntersect, algoIntersect2;
+    private GeoPoint[] tangentPoints, tangentPoints2;
+    private boolean equalLines = false, equalLines2 = false;
 
     AlgoCommonTangents(
                        Construction cons,
                        String[] labels,
-                       GeoConic c1,
+                       GeoConic c,
                        GeoConic c2) {
-        this(cons, c1, c2);
+        this(cons, c, c2);
         GeoElement.setLabels(labels, tangents);
     }
 
-    AlgoCommonTangents(Construction cons, GeoConic c1, GeoConic c2) {
+    AlgoCommonTangents(Construction cons, GeoConic c, GeoConic c2) {
         super(cons);
-        this.c1 = c1;
+        this.c = c;
         this.c2 = c2;
 
-        P = new GeoPoint(cons);
-        c1 = new GeoConic(cons);
+        double r = c.getCircleRadius();
+        double r2 = c2.getCircleRadius();
 
-        tangents = new GeoLine[4];
+        // outer
+        P = new GeoPoint(cons);
+        if( Math.abs(r2-r) > Kernel.MIN_PRECISION) {
+            P.setCoords((c.b.x*r2-c2.b.x*r)/(r2-r),
+                        (c.b.y*r2-c2.b.y*r)/(r2-r), 1.0d);
+        } else {
+            P.setCoords((c.b.x*r2-c2.b.x*r),
+                        (c.b.y*r2-c2.b.y*r), 0.0d);
+        }
+        // the tangents are computed by intersecting the
+        // polar line of P with c
+        polar = new GeoLine(cons);
+        c.polarLine(P, polar);
+        algoIntersect = new AlgoIntersectLineConic(cons, polar, c);
+        //  this is only an internal Algorithm that shouldn't be in the construction list
+        cons.removeFromConstructionList(algoIntersect);
+        tangentPoints = algoIntersect.getIntersectionPoints();
+
+        // inner
+        P2 = new GeoPoint(cons);
+        P2.setCoords((c.b.x*r2+c2.b.x*r)/(r2+r),
+                     (c.b.y*r2+c2.b.y*r)/(r2+r), 1.0d);
+        // the tangents are computed by intersecting the
+        // polar line of P with c
+        polar2 = new GeoLine(cons);
+        c2.polarLine(P2, polar2);
+        algoIntersect2 = new AlgoIntersectLineConic(cons, polar2, c2);
+        //  this is only an internal Algorithm that shouldn't be in the construction list
+        cons.removeFromConstructionList(algoIntersect2);
+        tangentPoints2 = algoIntersect2.getIntersectionPoints();
+
+        tangents = new GeoLine[2+2];
         tangents[0] = new GeoLine(cons);
         tangents[1] = new GeoLine(cons);
-        tangents[2] = new GeoLine(cons);
-        tangents[3] = new GeoLine(cons);
-        polar = new GeoLine(cons);
+        tangents[0].setStartPoint(P);
+        tangents[1].setStartPoint(P);
+
+        tangents[0+2] = new GeoLine(cons);
+        tangents[1+2] = new GeoLine(cons);
+        tangents[0+2].setStartPoint(P2);
+        tangents[1+2].setStartPoint(P2);
 
         setInputOutput(); // for AlgoElement
 
@@ -80,8 +106,10 @@ public class AlgoCommonTangents extends AlgoElement {
             tangents[1].setUndefined();
             tangentPoints[1].setUndefined();
         }
-        if( tangents[2].isEqual(tangents[3]) ) {
-        	tangents[3].setUndefined();
+        equalLines2 = tangents[0+1].isEqual(tangents[1+2]);
+        if (equalLines2) {        
+            tangents[1+2].setUndefined();
+            tangentPoints2[1].setUndefined();
         }
     }
 
@@ -92,12 +120,13 @@ public class AlgoCommonTangents extends AlgoElement {
     public int getRelatedModeID() {
         return EuclidianConstants.MODE_TANGENTS;
     }
-
+    
     // for AlgoElement
     protected void setInputOutput() {
         input = new GeoElement[2];
-        input[0] = c1;
+        input[0] = c;
         input[1] = c2;
+
         output = tangents;
         setDependencies(); // done by AlgoElement
     }
@@ -106,92 +135,66 @@ public class AlgoCommonTangents extends AlgoElement {
         return tangents;
     }
 
-    // GeoPoint getPoint() {
-    //     return P;
-    // }
-    // GeoConic getConic() { 
-    //     return c2; // ???
-    // }       
-
-    // /**
-    //  * return intersection point of tangent line and conic c.
-    //  * return null if line is not defined as tangent of conic c.
-    //  */
-    // GeoPoint getTangentPoint(GeoConic conic, GeoLine line) {
-    //     if (conic != c2) // ???
-    //         return null;
-    // 
-    //     if (line == tangents[0])
-    //         return tangentPoints[0];
-    //     else if (line == tangents[1])
-    //         return tangentPoints[1];
-    //     else
-    //         return null;
-    // }
-    
-    // /**
-    //  * Inits the helping interesection algorithm to take
-    //  * the current position of the lines into account.
-    //  * This is important so the the tangent lines are not
-    //  * switched after loading a file
-    //  */
-    // public void initForNearToRelationship() {
-    //     // if first tangent point is not on first tangent,
-    //     // we switch the intersection points
-    //     if (!tangents[0].isOnFullLine(tangentPoints[0], Kernel.MIN_PRECISION)) {
-    //         algoIntersect.initForNearToRelationship();
-    //         
-    //         // remember first point
-    //         double px = tangentPoints[0].x;
-    //         double py = tangentPoints[0].y;
-    //         double pz = tangentPoints[0].z;
-    //         
-    //         // first = second
-    //         algoIntersect.setIntersectionPoint(0, tangentPoints[1]);
-    //         
-    //         // second = first
-    //         tangentPoints[1].setCoords(px, py, pz);
-    //         algoIntersect.setIntersectionPoint(1, tangentPoints[1]);
-    //     }                    
-    // }
+    /**
+     * Inits the helping interesection algorithm to take
+     * the current position of the lines into account.
+     * This is important so the the tangent lines are not
+     * switched after loading a file
+     */
+    public void initForNearToRelationship() {
+        // if first tangent point is not on first tangent,
+        // we switch the intersection points
+        if (!tangents[0].isOnFullLine(tangentPoints[0], Kernel.MIN_PRECISION)) {
+            algoIntersect.initForNearToRelationship();
+            // remember first point
+            double px = tangentPoints[0].x;
+            double py = tangentPoints[0].y;
+            double pz = tangentPoints[0].z;
+            // first = second
+            algoIntersect.setIntersectionPoint(0, tangentPoints[1]);
+            // second = first
+            tangentPoints[1].setCoords(px, py, pz);
+            algoIntersect.setIntersectionPoint(1, tangentPoints[1]);
+        }
+        if (!tangents[0+2].isOnFullLine(tangentPoints2[0], Kernel.MIN_PRECISION)) {
+            algoIntersect2.initForNearToRelationship();
+            // remember first point
+            double px = tangentPoints2[0].x;
+            double py = tangentPoints2[0].y;
+            double pz = tangentPoints2[0].z;
+            // first = second
+            algoIntersect2.setIntersectionPoint(0, tangentPoints2[1]);
+            // second = first
+            tangentPoints2[1].setCoords(px, py, pz);
+            algoIntersect2.setIntersectionPoint(1, tangentPoints2[1]);
+        }
+    }
 
     // calc tangents
     protected final void compute() {
 
-        if( !c1.isCircle() || !c2.isCircle() ) {
+        if( !c.isCircle() || !c2.isCircle() ) {
             for(int i=0; i<4; i++) {
                 tangents[i].setUndefined();
             }
             return;
         }
 
-        double r1 = c1.getCircleRadius();
+        double r = c.getCircleRadius();
         double r2 = c2.getCircleRadius();
-        
-        // outer tangents
-        if( Math.abs(r1-r2) > Kernel.MIN_PRECISION) {
-            P.setCoords((c1.b.x*r2-c2.b.x*r1)/(r2-r1),
-                        (c1.b.y*r2-c2.b.y*r1)/(r2-r1), 1.0d);
+
+        // outer
+        if( Math.abs(r2-r) > Kernel.MIN_PRECISION) {
+            P.setCoords((c.b.x*r2-c2.b.x*r)/(r2-r),
+                        (c.b.y*r2-c2.b.y*r)/(r2-r), 1.0d);
         } else {
-            P.setCoords((c1.b.x*r2-c2.b.x*r1),
-                        (c1.b.y*r2-c2.b.y*r1), 0.0d); // infinity
-        }
-        algoIntersect = new AlgoIntersectLineConic(cons, polar, c1);
-        // this is only an internal Algorithm that shouldn't be in the construction list
-        // cons.removeFromConstructionList(algoIntersect);
-        tangentPoints = algoIntersect.getIntersectionPoints();
-        tangents[0].setStartPoint(P);
-        tangents[1].setStartPoint(P);
-        // degenerates should not have any tangents
-        if (c1.isDegenerate()) {
-            tangents[0].setUndefined();
-            tangents[1].setUndefined();
-            return;
+            P.setCoords((c.b.x*r2-c2.b.x*r),
+                        (c.b.y*r2-c2.b.y*r), 0.0d);
         }
         // update polar line
-        c1.polarLine(P, polar);
+        c.polarLine(P, polar);
         // if P lies on the conic, the polar is a tangent        
-        if (c1.isIntersectionPointIncident(P, Kernel.MIN_PRECISION)) {
+        if (c.isIntersectionPointIncident(P, Kernel.MIN_PRECISION)) {
             tangents[0].setCoords(polar);
             tangentPoints[0].setCoords(P);
             // check if we had equal lines at the beginning
@@ -215,58 +218,44 @@ public class AlgoCommonTangents extends AlgoElement {
             // we no longer have equal lines (if we ever had them)
             equalLines = false;
         }
-        // end of outer tangents
 
-        // inner tangents
-        P.setCoords((c1.b.x*r2+c2.b.x*r1)/(r2+r1),
-                    (c1.b.y*r2+c2.b.y*r1)/(r2+r1), 1.0d);
-        algoIntersect = new AlgoIntersectLineConic(cons, polar, c1);
-        //  this is only an internal Algorithm that shouldn't be in the construction list
-        // cons.removeFromConstructionList(algoIntersect);
-        tangentPoints = algoIntersect.getIntersectionPoints();
-        tangents[0+2].setStartPoint(P);
-        tangents[1+2].setStartPoint(P);
-        // degenerates should not have any tangents
-        if (c1.isDegenerate()) {
-            tangents[0+2].setUndefined();
-            tangents[1+2].setUndefined();
-            return;
-        }
+        // inner
+        P2.setCoords((c.b.x*r2+c2.b.x*r)/(r2+r),
+                     (c.b.y*r2+c2.b.y*r)/(r2+r), 1.0d);
         // update polar line
-        c1.polarLine(P, polar);
+        c2.polarLine(P2, polar2);
         // if P lies on the conic, the polar is a tangent        
-        if (c1.isIntersectionPointIncident(P, Kernel.MIN_PRECISION)) {
-            tangents[0+2].setCoords(polar);
-            tangentPoints[0].setCoords(P);
+        if (c2.isIntersectionPointIncident(P2, Kernel.MIN_PRECISION)) {
+            tangents[0+2].setCoords(polar2);
+            tangentPoints2[0].setCoords(P2);
             // check if we had equal lines at the beginning
             // if so we still don't want to see the second line
-            if (equalLines) {
+            if (equalLines2) {
                 tangents[1+2].setUndefined();
-                tangentPoints[1].setUndefined();
+                tangentPoints2[1].setUndefined();
             } else {
                 tangents[1+2].setCoords(polar);
-                tangentPoints[1].setCoords(P);
+                tangentPoints2[1].setCoords(P);
             }
         }
         // if P is not on the conic, the tangents pass through
         // the intersection points of polar and conic
         else {
             // intersect polar line with conic -> tangentPoints
-            algoIntersect.update();
+            algoIntersect2.update();
             // calc tangents through tangentPoints
-            GeoVec3D.lineThroughPoints(P, tangentPoints[0], tangents[0+2]);
-            GeoVec3D.lineThroughPoints(P, tangentPoints[1], tangents[1+2]);
+            GeoVec3D.lineThroughPoints(P2, tangentPoints2[0], tangents[0+2]);
+            GeoVec3D.lineThroughPoints(P2, tangentPoints2[1], tangents[1+2]);
             // we no longer have equal lines (if we ever had them)
-            equalLines = false;
+            equalLines2 = false;
         }
-        // end of inner tangents
-        
+
     } // end of compute
 
     public final String toString() {
         // Michael Borcherds 2008-03-30
         // simplified to allow better Chinese translation
-        return app.getPlain("CommonTangentOfCirclesAandB", c1.getLabel(), c2.getLabel());
+        return app.getPlain("CommonTangentOfCirclesAandB", c.getLabel(), c2.getLabel());
     }
 }
 
