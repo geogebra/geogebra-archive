a13e6ba33e7d125aa92d7cea4435eedb095fe286
zbynek <zbynek@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-08-01 17:23:19 +0000

    Fix for LCM[{1,2,0,3}] (check for zeros so that we don't divide by zero GCDs
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@11258 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/AlgoListLCM.java
+++ geogebra/kernel/AlgoListLCM.java
@@ -24,6 +24,7 @@ import java.math.BigInteger;
 public class AlgoListLCM extends AlgoElement {
 
 	private static final long serialVersionUID = 1L;
+	private static final BigInteger bigZero = BigInteger.valueOf(0);
 	private GeoList geoList; //input
     private GeoNumeric num; //output	
 
@@ -54,7 +55,7 @@ public class AlgoListLCM extends AlgoElement {
     GeoNumeric getLCM() {
         return num;
     }
-
+    
     protected final void compute() {
     	int size = geoList.size();
     	if (!geoList.isDefined() ||  size == 0) {
@@ -75,10 +76,14 @@ public class AlgoListLCM extends AlgoElement {
     		if(!kernel.isInteger(nd) || Math.abs(lcm.doubleValue())>1e15){
     			num.setUndefined();
     			return;
-    		}
+    		}    		
         	BigInteger n = BigInteger.valueOf((long)nd);
-        	BigInteger product = n.multiply(lcm);
-    		lcm =  product.divide(lcm.gcd(n));
+        	if(n.compareTo(bigZero)==0){
+        		lcm = bigZero;
+        	}else{
+        		BigInteger product = n.multiply(lcm);
+        		lcm =  product.divide(lcm.gcd(n));
+        	}
     	}
     	
     	double resultD = Math.abs(lcm.doubleValue());
