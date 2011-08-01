20967fe8a01c122cb30767c96fefb71d4937402c
george <george@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-08-01 20:02:11 +0000

    PlotPanel right-click handling, started copy/export
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@11264 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/gui/view/spreadsheet/statdialog/PlotPanelEuclidianView.java
+++ geogebra/gui/view/spreadsheet/statdialog/PlotPanelEuclidianView.java
@@ -2,19 +2,19 @@ package geogebra.gui.view.spreadsheet.statdialog;
 
 import geogebra.euclidian.EuclidianController;
 import geogebra.euclidian.EuclidianView;
-import geogebra.euclidian.Hits;
-import geogebra.kernel.GeoList;
 import geogebra.kernel.Kernel;
 import geogebra.main.Application;
 
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-import javax.swing.JPanel;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 
 /**
  * 
@@ -25,16 +25,20 @@ import javax.swing.JPanel;
  *
  */
 public class PlotPanelEuclidianView extends EuclidianView implements ComponentListener {
-	
-	
+
+
 	private EuclidianController ec;
-	
+
 	private static boolean[] showAxes = { true, true };
 	private static boolean showGrid = false;
-	
+
 	private double xMinData, xMaxData, yMinData, yMaxData;
-	
+
 	private PlotSettings plotSettings;
+
+	private MyMouseListener myMouseListener;
+
+	private boolean enableContextMenu;
 	public PlotSettings getPlotSettings() {
 		return plotSettings;
 	}
@@ -42,41 +46,44 @@ public class PlotPanelEuclidianView extends EuclidianView implements ComponentLi
 		this.plotSettings = plotSettings;
 		this.setEVParams();
 	}
-	
 
-	
-	
+
+
+
 	/*************************************************
 	 * Construct the panel
 	 */
 	public PlotPanelEuclidianView(Kernel kernel) {
-		super(new EuclidianController(kernel), showAxes, showGrid);
-		
+		super(new PlotPanelEuclidianController(kernel), showAxes, showGrid);
+
 		this.ec = this.getEuclidianController();
-		
+
 		setMouseEnabled(false);
 		setMouseMotionEnabled(false);
 		setMouseWheelEnabled(false);
 		setAllowShowMouseCoords(false);
 		setAxesCornerCoordsVisible(false);
-		
-		
+		setContextMenuEnabled(true);
+
+
 		setAntialiasing(true);
 		updateFonts();
 		setPreferredSize(new Dimension(300,200));
 		setSize(new Dimension(300,200));
 		updateSize();
-			
+
 		plotSettings = new PlotSettings();
-		
+
 		addComponentListener(this);
-		
+
 	}
 
 	public void setMouseEnabled(boolean enableMouse){
+		
 		removeMouseListener(ec);
 		if(enableMouse)
 			addMouseListener(ec);
+		setContextMenuEnabled(enableContextMenu);
 	}
 
 	public void setMouseMotionEnabled(boolean enableMouseMotion){
@@ -91,67 +98,74 @@ public class PlotPanelEuclidianView extends EuclidianView implements ComponentLi
 			addMouseWheelListener(ec);
 	}
 
+	public void setContextMenuEnabled(boolean enableContextMenu){
+		this.enableContextMenu = enableContextMenu;
+		if(myMouseListener == null)
+			myMouseListener = new MyMouseListener();
+		removeMouseListener(myMouseListener);
+		if(enableContextMenu)
+			addMouseListener(myMouseListener);;
+	}
+
+
 	
-	
-	
-	
-	
+
 	/**
 	 * Override UpdateSize() so that our plots stay centered and scaled in a
 	 * resized window.
 	 */
 	@Override
 	public void updateSize(){
-		
+
 		// record the old coord system
 		double xminTemp = getXmin();
 		double xmaxTemp = getXmax();
 		double yminTemp = getYmin();
 		double ymaxTemp = getYmax();	
-		
+
 		// standard update: change the coord system to match new window dimensions
 		// with the upper left corner fixed and the other bounds adjusted.  
 		super.updateSize();		
-		
+
 		// now reset the coord system so that our view dimensions are restored 
 		// using the new scaling factors. 
 		setRealWorldCoordSystem(xminTemp, xmaxTemp, yminTemp, ymaxTemp);
 	}	
 
-	
+
 	@Override
 	public void setMode(int mode) {
 		// .... do nothing
 	}
-	
+
 
 	public void setEVParams(){
-		
+
 		showGrid(plotSettings.showGrid);
 		setShowAxis(EuclidianView.AXIS_Y, plotSettings.showYAxis, false);
-		
+
 		setAutomaticGridDistance(plotSettings.gridIntervalAuto);
 		if(!plotSettings.gridIntervalAuto){
 			this.setGridDistances(plotSettings.gridInterval);
 		}
-		
-		
+
+
 		if(plotSettings.showArrows){
 			setAxesLineStyle(EuclidianView.AXES_LINE_TYPE_ARROW);
 		}else{
 			setAxesLineStyle(EuclidianView.AXES_LINE_TYPE_FULL);
 		}
-		
+
 		setDrawBorderAxes(plotSettings.isEdgeAxis);
 		if(!plotSettings.isEdgeAxis[0])
 			setAxisCross(0,0);
 		if(!plotSettings.isEdgeAxis[1])
 			setAxisCross(1,0);
-		
-		
+
+
 		setPositiveAxes(plotSettings.isPositiveOnly);
-		
-		
+
+
 		if(plotSettings.forceXAxisBuffer){
 			// ensure that the axis labels are shown
 			// by forcing a fixed pixel height below the x-axis	
@@ -160,10 +174,10 @@ public class PlotPanelEuclidianView extends EuclidianView implements ComponentLi
 			plotSettings.yMin = - pixelOffset * plotSettings.yMax / (pixelHeight + pixelOffset);
 		}
 
-		
+
 		setAxesCornerCoordsVisible(false);
-		
-		
+
+
 		this.setAutomaticAxesNumberingDistance(plotSettings.xAxesIntervalAuto, 0);
 		this.setAutomaticAxesNumberingDistance(plotSettings.yAxesIntervalAuto, 1);
 		if(!plotSettings.xAxesIntervalAuto){
@@ -172,32 +186,22 @@ public class PlotPanelEuclidianView extends EuclidianView implements ComponentLi
 		if(!plotSettings.yAxesIntervalAuto){
 			setAxesNumberingDistance(plotSettings.yAxesInterval, 1);
 		}
-		
+
 		setPointCapturing(plotSettings.pointCaptureStyle);
 
 		// do this last ?
 		setRealWorldCoordSystem(plotSettings.xMin, plotSettings.xMax, plotSettings.yMin, plotSettings.yMax);
-		
+
 		repaint();
 	}
-	
-	
-	
-	
-	protected void processMouseEvent(MouseEvent e){
-		if(e.getClickCount() > 1 || Application.isRightClick(e)){
-			e.consume();
-			return;
-		}else{
-			super.processMouseEvent(e);
-		}	
-	}
-	
-	
+
+
+
+
 	//==================================================
 	//       Component Listener  (for resizing our EV)
 	//=================================================
-	
+
 	public void componentHidden(ComponentEvent arg0) {	
 	}
 	public void componentMoved(ComponentEvent arg0) {
@@ -208,7 +212,94 @@ public class PlotPanelEuclidianView extends EuclidianView implements ComponentLi
 	}
 	public void componentShown(ComponentEvent arg0) {
 	}
+
+
+
+
+	//==================================================
+	//       Mouse Handlers
+	//=================================================
+
+
+	protected void processMouseEvent(MouseEvent e){
+		if(e.getClickCount() > 1){
+			e.consume();
+			return;}
+
+		else if(Application.isRightClick(e)){
+			if(e.getID()==MouseEvent.MOUSE_RELEASED){
+				ContextMenu contextMenu = new ContextMenu();
+				contextMenu.show(e.getComponent(), e.getX(),e.getY());
+				e.consume();
+			}
+			return;	}
+
+		else
+			super.processMouseEvent(e);
+	}
+
+
+	class MyMouseListener implements MouseListener{
+
+		public void mouseClicked(MouseEvent e) {
+			Object ob = e.getSource();	
+			System.out.println("sdsadasdasdasdsadsadsd");
+			// right click shows context menu
+			if (Application.isRightClick(e)) {
+				System.out.println("popup time!");
+				e.consume();
+				//app.getGuiManager().showPopupMenu(temp, table, origin);
+			}
+		}
+
+		public void mouseEntered(MouseEvent e) {}
+
+		public void mouseExited(MouseEvent e) {}
+
+		public void mousePressed(MouseEvent e) {}
+
+		public void mouseReleased(MouseEvent e) {}
+
+	}
+
 	
+	//=============================================
+	//      Context Menu
+	//=============================================
+
+	private class ContextMenu extends JPopupMenu{
+
+		JMenuItem menuItem;
+
+		public  ContextMenu(){
+			this.setOpaque(true);
+			setBackground(bgColor);	
+			setFont(app.getPlainFont());
+
+			menuItem = new JMenuItem(app.getMenu("CopyToGraphics") + "...", app.getImageIcon("edit-copy.png"));
+			menuItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					
+					
+				}
+			});
+			add(menuItem);
+			menuItem.setBackground(bgColor);
+
+			menuItem = new JMenuItem(app.getPlain("ExportAsPicture") + "...");
+			menuItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+
+				}
+			});
+			add(menuItem);
+			menuItem.setBackground(bgColor);
+			
+		
+		}
+	}
 
 
+	
 }
