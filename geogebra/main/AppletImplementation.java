a22f797ee5526ab63837f7380568eae14e315f7e
michael <michael@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-08-20 17:16:00 +0000

    removed stacktrace when ggbOnInit() not found
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@11999 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/main/AppletImplementation.java
+++ geogebra/main/AppletImplementation.java
@@ -1345,8 +1345,8 @@ public class AppletImplementation implements AppletImplementationInterface {
 			}
 			else Application.debug("Warning: could not initialize JSObject.getWindow() for GeoGebraApplet when calling "+jsFunction);
 		} catch (Exception e) {						
-			System.err.println("Warning: Error calling JavaScript function '"+jsFunction+"'");
-			e.printStackTrace();
+			System.err.println("Warning: Error calling JavaScript function '"+jsFunction+"' ("+e.getLocalizedMessage()+")");
+			//e.printStackTrace();
 		}    
 	} 	
 	
