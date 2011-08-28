33f2a1a2b7617be8b5c252eca80b37f15d1f88c4
tam <tam@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2011-08-28 19:21:59 +0000

    a trial fix for # 1294
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra@12318 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- geogebra/kernel/AlgoConicFivePoints.java
+++ geogebra/kernel/AlgoConicFivePoints.java
@@ -33,9 +33,12 @@ public class AlgoConicFivePoints extends AlgoElement {
 	 */
 	private static final long serialVersionUID = 1L;
 	private GeoPoint[] P; // input  five points      
+	private GeoPoint[] Ppert;
+	private GeoConic conicpert; // output    
     private GeoConic conic; // output             
-
-    private double[][] A, B, C;
+    private double delta;
+    
+    private double[][] A, B, C, Cpert, Cmin;
     private double l, m;
     private GeoVec3D[] line;
     private int i, j;
@@ -59,6 +62,8 @@ public class AlgoConicFivePoints extends AlgoElement {
         A = new double[3][3];
         B = new double[3][3];
         C = new double[3][3];
+        Cpert = new double[3][3];
+        Cmin = new double[3][3];
 
         compute();
         conic.setLabel(label);
@@ -112,7 +117,97 @@ public class AlgoConicFivePoints extends AlgoElement {
         l = evalMatrix(B, P[4]);
         m = -evalMatrix(A, P[4]);
         linComb(A, B, l, m, C);
+        
+        // compute a perturbed Cpert
+    	kernel.setSilentMode(true);
+    	Ppert = new GeoPoint[5];
+    	for (int i=0; i<5; i++) {
+    		Ppert[i] = new GeoPoint(P[i]);
+    		
+    	}
+    	
+    	
+    	/*
+    	double maxDistSqr = 0;
+    	double temp;
+    	int maxDistP1 = 0;
+    	int maxDistP2 = 0;
+    	
+    	
+    	for (int i=0; i<5; i++) {
+    		temp = P[0].distanceSqr(P[i]);
+    		if ( temp > maxDistSqr) {
+    			maxDistP2 = i;
+    			maxDistSqr = temp;
+    		}
+    	}
+    	for (int i=0; i<5; i++) {
+    		temp = P[i].distanceSqr(P[maxDistP2]);
+    		if (temp > maxDistSqr) {
+    			maxDistP1 = i;
+    			maxDistSqr = temp;
+    		}
+    	}
+    	
+    	if (maxDistSqr!=0) {
+    		double t = Kernel.EPSILON/Math.sqrt(maxDistSqr);
+    		Ppert[maxDistP1].setCoords(
+    				P[maxDistP1].inhomX * (1+t) - P[maxDistP2].inhomX * t,
+    				P[maxDistP1].inhomY * (1+t) - P[maxDistP2].inhomY * t,
+    				1
+    			);
+       		Ppert[maxDistP2].setCoords(
+    				P[maxDistP2].inhomX * (1+t) - P[maxDistP1].inhomX * t,
+    				P[maxDistP2].inhomY * (1+t) - P[maxDistP1].inhomY * t,
+    				1
+    			);
+    	 */
+
+    	delta = 0;
+    	int repetition = 5;
+    	for (int m=0; m<3; m++)
+    		for (int n=0; n<3; n++)
+    			Cmin[m][n]=Double.POSITIVE_INFINITY;
+    	
+    	for (int k=0; k<repetition; k++) {
+    		for (int i=0; i<5; i++) {
+    			Ppert[i].randomizeForErrorEstimation();
+    		}
+
+            GeoVec3D.cross(Ppert[0], Ppert[1], line[0]);
+            GeoVec3D.cross(Ppert[2], Ppert[3], line[1]);
+            GeoVec3D.cross(Ppert[0], Ppert[2], line[2]);
+            GeoVec3D.cross(Ppert[1], Ppert[3], line[3]);
+            degCone(line[0], line[1], A);
+            degCone(line[2], line[3], B);
+            l = evalMatrix(B, Ppert[4]);
+            m = -evalMatrix(A, Ppert[4]);
+            linComb(A, B, l, m, Cpert);
+            
+            //calculate the estimation of error of detS
+            delta += Math.abs((C[0][0]*C[1][1] - (C[0][1]+C[1][0])*(C[0][1]+C[1][0])/4)
+            - (Cpert[0][0]*Cpert[1][1] - (Cpert[0][1]+Cpert[1][0])*(Cpert[0][1]+Cpert[1][0])/4));
+        	for (int m=0; m<3; m++)
+        		for (int n=0; n<3; n++)
+        			Cmin[m][n]=Math.min(Cmin[m][n],Math.abs(Cpert[m][n]));
+        	        
+    	}
+    
+    	delta = delta / repetition;
+    	
+        conic.errDetS = 3*delta;
+        //TODO: this is not reasonable enough.
+        if (Math.abs(C[0][0])< Math.abs(Cmin[0][0])/10)
+        	C[0][0]=0;
+        if (Math.abs(C[1][1])< Math.abs(Cmin[1][1])/10)
+    		C[1][1]=0;
+        if (Math.abs(C[0][1] + C[1][0])< Math.abs(Cmin[0][1]+Cmin[1][0])/10) {
+    		C[0][1]=0;
+    		C[1][0]=0;
+        }
         conic.setMatrix(C);
+        kernel.setSilentMode(false);
+        
     }
 
     // compute degenerate conic from lines a, b
